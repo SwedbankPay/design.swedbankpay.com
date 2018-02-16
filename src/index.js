2ff80304cb2de9f79aafccdc0a72e66abc8493b2
erikhallaraaker <erikhallaraaker@gmail.com>
2018-02-16 14:33:08 +0100

    Added polyfills for ie support

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ import "prismjs/plugins/toolbar/prism-toolbar.css";
 import "prismjs/plugins/toolbar/prism-toolbar.min.js";
 import "prismjs/plugins/copy-to-clipboard/prism-copy-to-clipboard.min.js";
 
+// Polyfills
+import "./polyfills";
+
 import "./less/px.less";
 import "./less/themes/documentation.less";
 import "./less/designguide.less";
