df29a5f0a822c317a63f89544c58387bae159c2a
erikhallaraaker <erikhallaraaker@gmail.com>
2018-03-14 15:48:00 +0100

    Built v.0.1.5

--- dist/scripts/polyfills-0.1.5.js
+++ dist/scripts/polyfills-0.1.5.js
@@ -1,2 +1,2 @@
 var payex=webpackJsonppayex(["polyfills"],{"./src/polyfills/String.includes.js":function(t,s,i){"use strict";String.prototype.includes||(String.prototype.includes=function(t,s){return"number"!=typeof s&&(s=0),!(s+t.length>this.length)&&-1!==this.indexOf(t,s)})},"./src/polyfills/String.startsWith.js":function(t,s,i){"use strict";String.prototype.startsWith||(String.prototype.startsWith=function(t,s){return this.substr(!s||s<0?0:+s,t.length)===t})},"./src/polyfills/index.js":function(t,s,i){"use strict";i("./src/polyfills/String.startsWith.js"),i("./src/polyfills/String.includes.js")}},["./src/polyfills/index.js"]);
-//# sourceMappingURL=polyfills-0.1.4.js.map?0c6cc47f018c2df13b8e
+//# sourceMappingURL=polyfills-0.1.5.js.map?141b8fe80e5354c68414
