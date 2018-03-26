fb6c9038f943373da9276381572124f00cecfebd
erikhallaraaker <erikhallaraaker@gmail.com>
2018-03-26 11:39:25 +0200

    Added tests for svg-logos and updated snapshots for header

---
+++ src/App/AppHeader/HeaderIcons.test.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { shallow } from "enzyme";
+
+import { GithubLogo, SlackLogo } from "./HeaderIcons";
+
+describe("AppHeader: HeaderIcons", () => {
+    describe("GithubLogo", () => {
+        it("is defined", () => {
+            expect(GithubLogo).toBeDefined();
+        });
+
+        it("renders as expected", () => {
+            const wrapper = shallow(
+                <GithubLogo />
+            );
+
+            expect(wrapper).toMatchSnapshot();
+        });
+    });
+
+    describe("SlackLogo", () => {
+        it("is defined", () => {
+            expect(SlackLogo).toBeDefined();
+        });
+
+        it("renders as expected", () => {
+            const wrapper = shallow(
+                <SlackLogo />
+            );
+
+            expect(wrapper).toMatchSnapshot();
+        });
+    });
+});
