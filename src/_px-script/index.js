7bc5573bf90a6901edd9107f3ecc9cafaf559baf
erikhallaraaker <erikhallaraaker@gmail.com>
2018-04-06 14:27:56 +0200

    Moved old px-script to folder "_px-script"

---
+++ src/_px-script/index.js
@@ -0,0 +1,38 @@
+import alert from "./alert";
+import button from "./button";
+import loader from "./loader";
+import modal from "./modal";
+import rangeslider from "./rangeslider";
+import utils from "./utils";
+import script from "./script";
+
+const px = {
+    alert,
+    button,
+    loader,
+    modal,
+    rangeslider,
+    utils,
+    script
+};
+
+if (!window.stopPx) {
+    document.addEventListener("DOMContentLoaded", () => { script.initAll(px); }, false);
+}
+
+window.px = px;
+export default px;
+export {
+    alert,
+    button,
+    loader,
+    modal,
+    rangeslider,
+    utils,
+    script
+};
+
+/* istanbul ignore if|else */
+if (module.hot) {
+    module.hot.accept();
+}
