5ff3905c2d55786700cf8b956684c89bd8e5de35
erikhallaraaker <erikhallaraaker@gmail.com>
2018-04-18 16:49:04 +0200

    Readded /core

---
+++ src/App/Documentation/core/Color/index.js
@@ -0,0 +1,71 @@
+import React from "react";
+
+import ComponentPreview from "../../utils/ComponentPreview";
+import DocToc from "../../utils/DocToc";
+import ColorPreview from "./ColorPreview";
+
+const ColorPalette = () => (
+    <>
+        <h2 id="color-palette">Color palette</h2>
+        {/* <a href="#" className=""><i className="material-icons color-warning icon-link">check</i></a> */}
+        <p>The PayEx DesignGuide color palette...</p>
+        <ComponentPreview language="html" showCasePanel={true}>
+            <div className="row">
+                <div className="col-xs-12 col-sm-6 col-md-4 col-lg-2">
+                    <ColorPreview name="brand" hex="#2da944" />
+                </div>
+                <div className="col-xs-12 col-sm-6 col-md-4 col-lg-2">
+                    <ColorPreview name="default" hex="#3c3c3c" />
+                </div>
+                <div className="col-xs-12 col-sm-6 col-md-4 col-lg-2">
+                    <ColorPreview name="neutral" hex="#266c9a" />
+                </div>
+                <div className="col-xs-12 col-sm-6 col-md-4 col-lg-2">
+                    <ColorPreview name="success" hex="#2da944" />
+                </div>
+                <div className="col-xs-12 col-sm-6 col-md-4 col-lg-2">
+                    <ColorPreview name="warning" hex="#ff9f00" />
+                </div>
+                <div className="col-xs-12 col-sm-6 col-md-4 col-lg-2">
+                    <ColorPreview name="danger" hex="#cd2e00" />
+                </div>
+            </div>
+        </ComponentPreview>
+    </>
+);
+
+const Usage = () => (
+    <>
+        <h2 id="usage">Usage</h2>
+        <p>To use a color simply prepend <code className="token property">color-</code> to the desired color and add it to the element class attribute.</p>
+        <ComponentPreview language="html" showCasePanel={true} codeFigure={true}>
+            <p className="color-brand">This text is color-brand.</p>
+            <p className="color-default">This text is color-default.</p>
+            <p className="color-neutral">This text is color-neutral.</p>
+            <p className="color-success">This text is color-success.</p>
+            <p className="color-warning">This text is color-warning.</p>
+            <p className="color-danger">This text is color-danger.</p>
+        </ComponentPreview>
+    </>
+);
+
+const ColorText = () => (
+    <div className="col-md-12 col-lg-10 doc-body">
+        <p className="lead">Here there be colors...</p>
+        <ColorPalette />
+        <Usage />
+    </div>
+);
+
+const Color = () => {
+    return (
+        <div className="doc-container">
+            <div className="row">
+                <ColorText />
+                <DocToc component={ColorText} />
+            </div>
+        </div>
+    );
+};
+
+export default Color;
---
+++ src/App/Documentation/core/Typography/index.test.js
@@ -0,0 +1,154 @@
+import React from "react";
+import { shallow } from "enzyme";
+
+import Typography, { Fonts, Headings, Small, Lead, Inline, TextUtilities, Abbreviations, Blockquotes, TypographyText } from "./index";
+
+describe("Core: Typography", () => {
+    it("is defined", () => {
+        expect(Typography).toBeDefined();
+    });
+
+    it("renders", () => {
+        const wrapper = shallow(
+            <Typography />
+        );
+
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    describe("Fonts", () => {
+        it("is defined", () => {
+            expect(Fonts).toBeDefined();
+        });
+
+        it("renders", () => {
+            const wrapper = shallow(
+                <Fonts />
+            );
+
+            expect(wrapper).toMatchSnapshot();
+        });
+    });
+
+    describe("Headings", () => {
+        it("is defined", () => {
+            expect(Headings).toBeDefined();
+        });
+
+        it("renders", () => {
+            const wrapper = shallow(
+                <Headings />
+            );
+
+            expect(wrapper).toMatchSnapshot();
+        });
+    });
+
+    describe("Small", () => {
+        it("is defined", () => {
+            expect(Small).toBeDefined();
+        });
+
+        it("renders", () => {
+            const wrapper = shallow(
+                <Small />
+            );
+
+            expect(wrapper).toMatchSnapshot();
+        });
+    });
+
+    describe("Lead", () => {
+        it("is defined", () => {
+            expect(Lead).toBeDefined();
+        });
+
+        it("renders", () => {
+            const wrapper = shallow(
+                <Lead />
+            );
+
+            expect(wrapper).toMatchSnapshot();
+        });
+    });
+
+    describe("Inline", () => {
+        it("is defined", () => {
+            expect(Inline).toBeDefined();
+        });
+
+        it("renders", () => {
+            const wrapper = shallow(
+                <Inline />
+            );
+
+            expect(wrapper).toMatchSnapshot();
+        });
+    });
+
+    describe("Text Utilities", () => {
+        it("is defined", () => {
+            expect(TextUtilities).toBeDefined();
+        });
+
+        it("renders", () => {
+            const wrapper = shallow(
+                <TextUtilities />
+            );
+
+            expect(wrapper).toMatchSnapshot();
+        });
+    });
+
+    describe("Abbreviations", () => {
+        it("is defined", () => {
+            expect(Abbreviations).toBeDefined();
+        });
+
+        it("renders", () => {
+            const wrapper = shallow(
+                <Abbreviations />
+            );
+
+            expect(wrapper).toMatchSnapshot();
+        });
+    });
+
+    describe("Blockquotes", () => {
+        it("is defined", () => {
+            expect(Blockquotes).toBeDefined();
+        });
+
+        it("renders", () => {
+            const wrapper = shallow(
+                <Blockquotes />
+            );
+
+            if (Blockquotes().props.children) {
+                Blockquotes().props.children.map(Child => {
+                    const wrapper = shallow(
+                        <Child.type />
+                    );
+
+                    expect(wrapper).toMatchSnapshot();
+                });
+            }
+
+            expect(wrapper).toMatchSnapshot();
+        });
+    });
+
+    describe("Typography Text", () => {
+        it("is defined", () => {
+            expect(TypographyText).toBeDefined();
+        });
+
+        it("renders", () => {
+            const wrapper = shallow(
+                <TypographyText />
+            );
+
+            expect(wrapper).toMatchSnapshot();
+        });
+    });
+});
