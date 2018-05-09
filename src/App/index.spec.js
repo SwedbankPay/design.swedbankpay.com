7140504a12901f891442d5dc6e240620dc249c57
erikhallaraaker <erikhallaraaker@gmail.com>
2018-05-09 17:02:14 +0200

    Got testcafe running locally. Also added support for reporting on teamcity

---
+++ src/App/index.spec.js
@@ -0,0 +1,13 @@
+import { Selector } from "testcafe";
+
+fixture `Getting Started`
+    .page `http://localhost:3000`;
+
+test("Test uno", async t => {
+    // Test code
+    // const articleHeader = await Selector(".panel-body").find("h2");
+    await t
+        .click("a[href='/docs']");
+
+
+});
