ba08027321bda2cea4763fbe21bb7d4c6f50dedf
erikhallaraaker <erikhallaraaker@gmail.com>
2018-05-16 14:21:15 +0200

    Made google analytics update on route changes, also slightly refactored router logic

--- src/App/index.js
+++ src/App/index.js
@@ -1,14 +1,16 @@
 import React from "react";
-import { BrowserRouter, HashRouter, Switch, Route } from "react-router-dom";
+import { Router, Switch, Route } from "react-router-dom";
+import createBrowserHistory from "history/createBrowserHistory";
 
 import routes from "./routes/root.js";
 import AppHeader from "./AppHeader";
 import ErrorPage404 from "./ErrorPage404";
 
-const Router = (process.env.NODE_ENV === "production") ? HashRouter : BrowserRouter;
+// const isProd = process.env.NODE_ENV === "production";
+const history = createBrowserHistory({ basename: "/design.payex.com" });
 
 const App = () => (
-    <Router>
+    <Router history={history}>
         <div id="px-designguide">
             <AppHeader />
             <Switch>
@@ -17,7 +19,15 @@ const App = () => (
                     const RouteComponent = component.default;
 
                     return (
-                        <Route key={i} exact={exact} path={path} render={() => <RouteComponent />} />
+                        <Route key={i} exact={exact} path={path} render={() => {
+                            history.listen(location => {
+                                window.gtag("config", "UA-3440932-20", {
+                                    "page_location": window.location.href,
+                                    "page_path": location.pathname
+                                });
+                            });
+                            return <RouteComponent />;
+                        }} />
                     );
                 })}
                 <Route component={ErrorPage404} />
