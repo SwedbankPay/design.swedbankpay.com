c66ba34063970cd8dd94142e19a410fccfb4ba24
erikhallaraaker <erikhallaraaker@gmail.com>
2018-05-28 12:07:58 +0200

    Removed icon from heading

--- src/App/Documentation/core/Icons/index.js
+++ src/App/Documentation/core/Icons/index.js
@@ -6,7 +6,7 @@ import DocToc from "../../utils/DocToc";
 
 const Usage = () => (
     <>
-        <h2 id="usage">Usage <i className="material-icons tiny">android</i></h2>
+        <h2 id="usage">Usage</h2>
         <p>To use an icon, provide the following markup: <PrismCode className="language-html">{"<i class='material-icons'>[ICON NAME]</i>"}</PrismCode>.</p>
         <ComponentPreview language="html" showCasePanel={true} codeFigure={true}>
             <i className="material-icons">android</i>{"\n"}
