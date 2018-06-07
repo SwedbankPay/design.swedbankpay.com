21ed5b4b45a6eb7384fb91007df4631422455069
Thomas Lorntzen <thomas.lorntzen@payex.com>
2018-06-07 15:37:57 +0200

    Added dynamic classes with color and valueLabelPosition props data to js.

--- src/App/components/FormComponents/Rangeslider.js
+++ src/App/components/FormComponents/Rangeslider.js
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const Rangeslider = ({ name, min, max, step, value, valueLabel, valueLabelPrefix, valueLabelPostfix }) => {
+const Rangeslider = ({ name, min, max, step, color, value, valueLabel, valueLabelPrefix, valueLabelPostfix, valueLabelPosition }) => {
     const attrs = {
         type: "range",
         name: name,
@@ -12,17 +12,17 @@ const Rangeslider = ({ name, min, max, step, value, valueLabel, valueLabelPrefix
     };
 
     const ValueLabel = () => (
-        <div className="value-label">{"\n"}
+        <output className={`value-label${valueLabelPosition ? ` value-label-position-${valueLabelPosition}` : " value-label-position-right" }`} >{"\n"}
             <p>{"\n"}
                 {valueLabelPrefix ? <span>{valueLabelPrefix}</span> : null}{valueLabelPrefix ? "\n" : null}
                 <span data-px-rangeslider-value>{value}</span>{"\n"}
                 {valueLabelPostfix ? <span>{valueLabelPostfix}</span> : null}{valueLabelPostfix ? "\n" : null}
             </p>{"\n"}
-        </div>
+        </output>
     );
 
     return (
-        <div className="rangeslider">{"\n"}
+        <div className={`rangeslider rangeslider-${color ? color : "brand"}`}>{"\n"}
             <input {...attrs}/>{"\n"}
             {valueLabel ? <ValueLabel /> : null}
         </div>
@@ -34,10 +34,12 @@ Rangeslider.propTypes = {
     min: PropTypes.number,
     max: PropTypes.number,
     step: PropTypes.number,
+    color: PropTypes.oneOf(["brand", "neutral", "default"]),
     value: PropTypes.number,
     valueLabel: PropTypes.bool,
     valueLabelPrefix: PropTypes.string,
-    valueLabelPostfix: PropTypes.string
+    valueLabelPostfix: PropTypes.string,
+    valueLabelPosition: PropTypes.string
 };
 
 export default Rangeslider;
