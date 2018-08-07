796ee82cb5af7ea4773c7cfc2934fc9d92281bb3
erikhallaraaker <erikhallaraaker@gmail.com>
2018-08-07 16:37:38 +0200

    Added styling for alternative icon anchorpoints. Updated documentation and tests.

--- src/App/Documentation/components/ActionList/index.js
+++ src/App/Documentation/components/ActionList/index.js
@@ -21,10 +21,57 @@ const Overview = () => (
     </>
 );
 
+const Anchorpoints = () => (
+    <>
+        <h2 id="anchorpoints">Anchorpoints</h2>
+        <p>Customize anchorpoint... Top right is default.</p>
+        <h3>Top left</h3>
+        <p>Use class <code className="token property">.anchor-top-left</code> to anchor action-list button to the top left corner of the menu.</p>
+        <ComponentPreview language="html" showCasePanel={true} codeFigure={true}>
+            <div className="action-list anchor-top-left">{"\n"}
+                <Icon icon="more_vert" />{"\n"}
+                <div className="action-menu">{"\n"}
+                    <a href="#" onClick={e => e.preventDefault()}><Icon icon="bookmark"/>Add bookmark</a>{"\n"}
+                    <a href="#" onClick={e => e.preventDefault()}><Icon icon="business_center"/>Add client</a>{"\n"}
+                    <a href="#" onClick={e => e.preventDefault()}><Icon icon="add_circle"/>Add document</a>{"\n"}
+                    <a href="#" onClick={e => e.preventDefault()}><Icon icon="person_add"/>Add user</a>{"\n"}
+                </div>
+            </div>
+        </ComponentPreview>
+        <h3>Bottom right</h3>
+        <p>Use class <code className="token property">.anchor-bottom-right</code> to anchor action-list button to the bottom right corner of the menu.</p>
+        <ComponentPreview language="html" showCasePanel={true} codeFigure={true}>
+            <div className="action-list anchor-bottom-right">{"\n"}
+                <Icon icon="more_vert" />{"\n"}
+                <div className="action-menu">{"\n"}
+                    <a href="#" onClick={e => e.preventDefault()}><Icon icon="bookmark"/>Add bookmark</a>{"\n"}
+                    <a href="#" onClick={e => e.preventDefault()}><Icon icon="business_center"/>Add client</a>{"\n"}
+                    <a href="#" onClick={e => e.preventDefault()}><Icon icon="add_circle"/>Add document</a>{"\n"}
+                    <a href="#" onClick={e => e.preventDefault()}><Icon icon="person_add"/>Add user</a>{"\n"}
+                </div>
+            </div>
+        </ComponentPreview>
+        <h3>Bottom left</h3>
+        <p>Use class <code className="token property">.anchor-bottom-left</code> to anchor action-list button to the bottom left corner of the menu.</p>
+        <ComponentPreview language="html" showCasePanel={true} codeFigure={true}>
+            <div className="action-list anchor-bottom-left">{"\n"}
+                <Icon icon="more_vert" />{"\n"}
+                <div className="action-menu">{"\n"}
+                    <a href="#" onClick={e => e.preventDefault()}><Icon icon="bookmark"/>Add bookmark</a>{"\n"}
+                    <a href="#" onClick={e => e.preventDefault()}><Icon icon="business_center"/>Add client</a>{"\n"}
+                    <a href="#" onClick={e => e.preventDefault()}><Icon icon="add_circle"/>Add document</a>{"\n"}
+                    <a href="#" onClick={e => e.preventDefault()}><Icon icon="person_add"/>Add user</a>{"\n"}
+                </div>
+            </div>
+        </ComponentPreview>
+    </>
+);
+
 const ActionListText = () => (
     <div className="col-md-12 col-lg-10 doc-body">
         <p className="lead">Action lists...</p>
         <Overview />
+        <Anchorpoints />
     </div>
 );
 
@@ -47,4 +94,4 @@ class ActionList extends Component {
 export default ActionList;
 
 /* for testing */
-export { Overview, ActionListText };
+export { Overview, Anchorpoints, ActionListText };
