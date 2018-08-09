dbb424422a358ddc9a4f1e916eb91dcea25a5cd6
erikhallaraaker <erikhallaraaker@gmail.com>
2018-08-09 14:31:02 +0200

    Added size options for media object

--- src/App/components/MediaObject/index.js
+++ src/App/components/MediaObject/index.js
@@ -1,22 +1,36 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const MediaObject = ({ imgUrl, icon, heading, text, mediaRight }) => (
-    <div className={`media${mediaRight ? " media-right" : ""}`}>
-        {(imgUrl || icon) ?
-            <div className="media-img">{"\n"}
-                {imgUrl ? <img src={imgUrl} /> : null}{imgUrl ? "\n" : null}
-                {icon ? <i className="material-icons">{icon}</i> : null}{icon ? "\n" : null}
+const MediaObject = ({ size, imgUrl, icon, heading, text, mediaRight }) => {
+    const mediaHeading = () => {
+        switch (size) {
+            case "sm":
+                return <h4>{heading}</h4>;
+            case "lg":
+                return <h2>{heading}</h2>;
+            default:
+                return <h3>{heading}</h3>;
+        }
+    };
+
+    return (
+        <div className={`media${mediaRight ? " media-right" : ""}${size ? ` media-${size}` : ""}`}>
+            {(imgUrl || icon) ?
+                <div className="media-img">{"\n"}
+                    {imgUrl ? <img src={imgUrl} /> : null}{imgUrl ? "\n" : null}
+                    {icon ? <i className="material-icons">{icon}</i> : null}{icon ? "\n" : null}
+                </div>
+                : null}
+            <div className="media-body">
+                {mediaHeading()}
+                <p>{text}</p>
             </div>
-            : null}
-        <div className="media-body">
-            <h2>{heading}</h2>
-            <p>{text}</p>
         </div>
-    </div>
-);
+    );
+};
 
 MediaObject.propTypes = {
+    size: PropTypes.oneOf(["sm", "lg"]),
     imgUrl: PropTypes.string,
     icon: PropTypes.string,
     heading: PropTypes.string.isRequired,
