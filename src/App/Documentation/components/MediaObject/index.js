4217b8700845f9e2cd419bacd87c9a42b69c101d
erikhallaraaker <erikhallaraaker@gmail.com>
2018-08-09 14:31:02 +0200

    Added size options for media object

--- src/App/Documentation/components/MediaObject/index.js
+++ src/App/Documentation/components/MediaObject/index.js
@@ -8,8 +8,27 @@ const Overview = () => (
         <h2 id="overview">Overview</h2>
         <p>Use the <code className="token property">.media</code> class on an object to style it as a small container that feature a left- or right-aligned image/icon alongside textual content.</p>
         <ComponentPreview language="html" showCasePanel={true} codeFigure={true}>
-            <MediaObjectComponent imgUrl="http://via.placeholder.com/64x64" heading="Bob Corlsan" text="bob.corlsan@payex.com" />
+            <MediaObjectComponent imgUrl="http://via.placeholder.com/48x48" heading="Bob Corlsan" text="bob.corlsan@payex.com" />
             <MediaObjectComponent icon="account_circle" heading="Bob Corlsan" text="bob.corlsan@payex.com" />
+        </ComponentPreview>
+    </>
+);
+
+const Sizes = () => (
+    <>
+        <h2 id="sizes">Sizes</h2>
+        <p>Sizes for media object...</p>
+        <h3>Small</h3>
+        <p>Small media object <code className="token property">.media-sm</code>...</p>
+        <ComponentPreview language="html" showCasePanel={true} codeFigure={true}>
+            <MediaObjectComponent size="sm" imgUrl="http://via.placeholder.com/40x40" heading="Bob Corlsan" text="bob.corlsan@payex.com" />
+            <MediaObjectComponent size="sm" icon="account_circle" heading="Bob Corlsan" text="bob.corlsan@payex.com" />
+        </ComponentPreview>
+        <h3>Large</h3>
+        <p>Large media object <code className="token property">.media-lg</code>...</p>
+        <ComponentPreview language="html" showCasePanel={true} codeFigure={true}>
+            <MediaObjectComponent size="lg" imgUrl="http://via.placeholder.com/56x56" heading="Bob Corlsan" text="bob.corlsan@payex.com" />
+            <MediaObjectComponent size="lg" icon="account_circle" heading="Bob Corlsan" text="bob.corlsan@payex.com" />
         </ComponentPreview>
     </>
 );
@@ -19,7 +38,7 @@ const MediaAlignment = () => (
         <h2 id="media-alignment">Media alignment</h2>
         <p>Use the <code className="token property">.media-right</code> class to align the media content to the right.</p>
         <ComponentPreview language="html" showCasePanel={true} codeFigure={true}>
-            <MediaObjectComponent mediaRight imgUrl="http://via.placeholder.com/64x64" heading="Bob Corlsan" text="bob.corlsan@payex.com" />
+            <MediaObjectComponent mediaRight imgUrl="http://via.placeholder.com/48x48" heading="Bob Corlsan" text="bob.corlsan@payex.com" />
             <MediaObjectComponent mediaRight icon="account_circle" heading="Bob Corlsan" text="bob.corlsan@payex.com" />
         </ComponentPreview>
     </>
@@ -27,11 +46,9 @@ const MediaAlignment = () => (
 
 const MediaObjectText = () => (
     <div className="col-md-12 col-lg-10 doc-body">
-        <div className="alert alert-danger in">
-            This component is under development and will probably change.
-        </div>
         <p className="lead">Media objects...</p>
         <Overview />
+        <Sizes />
         <MediaAlignment />
     </div>
 );
