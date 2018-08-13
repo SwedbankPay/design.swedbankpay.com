a82754fa1c2e31668c512b15559ac105f8b1cce1
erikhallaraaker <erikhallaraaker@gmail.com>
2018-08-13 15:22:54 +0200

    Finished prototype for toast component

--- src/App/Documentation/components/Toast/index.js
+++ src/App/Documentation/components/Toast/index.js
@@ -8,13 +8,16 @@ const Overview = () => (
         <h2 id="overview">Overview</h2>
         <p>You want jam on that toast?</p>
         <ComponentPreview language="html" showCasePanel={true} codeFigure={true}>
-            <button className="btn btn-brand" type="button" onClick={() => toast()}>Click for toast!</button>
+            <button className="btn btn-brand" type="button" onClick={() => toast({ html: "<div><h3>LOLOLOL</h3><p>Lol saved</p></div><button class='btn toast-action'>Dismiss</button>", classes: ["asd", "fasad"] })}>Click for toast!</button>
         </ComponentPreview>
     </>
 );
 
 const ToastText = () => (
     <div className="col-md-12 col-lg-10 doc-body">
+        <div className="alert alert-danger in">
+            This component is under development and will probably change.
+        </div>
         <p className="lead">Toast...</p>
         <Overview />
     </div>
