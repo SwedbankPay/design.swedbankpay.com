b65238815646ac676f5eabef03c5574e17197efd
erikhallaraaker <erikhallaraaker@gmail.com>
2018-08-30 16:42:40 +0200

    Set up basic sentry reporting

--- src/px-script/index.js
+++ src/px-script/index.js
@@ -1,3 +1,5 @@
+import { init } from "~/node_modules/@sentry/browser";
+
 import actionList from "./action-list";
 import alert from "./alert";
 import button from "./button";
@@ -10,6 +12,10 @@ import tabs from "./tabs";
 import toast from "./toast";
 import topbar from "./topbar";
 import validation from "./validation";
+
+// sentry reporter
+// TODO: put this in webpack (https://github.com/getsentry/sentry-webpack-plugin) [EH]
+init({ dsn: "https://832de6a6953642bea1d70c41c12a5ccd@sentry.io/80077" });
 
 const px = {
     actionList,
