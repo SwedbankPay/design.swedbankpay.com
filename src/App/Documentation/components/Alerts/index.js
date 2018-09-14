f2f83d53b139b1f9e315addbb4b22a5de6ef9cc7
Aksel Christoffer Wiig <aksel.christoffer.wiig@gmail.com>
2018-09-14 13:00:54 +0200

    Fixed .alert padding and removed redundant <p> tag in documentation

--- src/App/Documentation/components/Alerts/index.js
+++ src/App/Documentation/components/Alerts/index.js
@@ -61,8 +61,7 @@ const ClosingTheAlert = () => (
 const ExtendedUsage = () => (
     <>
         <h2 id="extended-usage">Extended usage</h2>
-        <p>Alerts can also contain additional HTML elements like headings, paragraphs and dividers.</p>
-        <p>Just make sure to wrap the content in a container with the class <span className="token attr-value">.alert-body</span>.</p>
+        <p>Alerts can also contain additional HTML elements like headings, paragraphs and dividers. <br /> Just make sure to wrap the content in a container with the class <span className="token attr-value">.alert-body</span>.</p>
         <ComponentPreview language="html" showCasePanel codeFigure>
             <AlertComponent type="success" close icon="check_circle">
                 <h3 className="color-success">Success!</h3>
