9d4bd2d2909b7e2e0649bbb907f39cd63ab57314
erikhallaraaker <erikhallaraaker@gmail.com>
2018-09-19 13:23:17 +0200

    Removed static folder

--- static/v/0.10.7/scripts/designguide.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonppayex=window.webpackJsonppayex||[]).push([[4],{144:function(p,n,w){}}]);
-//# sourceMappingURL=designguide.js.map?21ff30c6b04f55c7ed70
--- static/v/0.10.7/scripts/documentation.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonppayex=window.webpackJsonppayex||[]).push([[5],{142:function(p,n,w){}}]);
-//# sourceMappingURL=documentation.js.map?21ff30c6b04f55c7ed70
