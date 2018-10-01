b35707e08260a05456297973f69b5caafde255cc
Erik Hallaraaker <erikhallaraaker@gmail.com>
2018-10-01 14:18:55 +0200

    Optimized script for action list and added tests.

--- src/px-script/action-list/index.js
+++ src/px-script/action-list/index.js
@@ -2,26 +2,32 @@ import { isWithinBoundingBox } from "../utils";
 
 class ActionList {
     constructor (element) {
-        this.isOpen = false;
         this.container = element;
         this.toggleBtn = element.querySelector("i.material-icons");
         this.actionMenu = element.querySelector(".action-menu");
+        this.actionMenuLinks = this.actionMenu.querySelectorAll("a");
+        this.isOpen = this.actionMenu.classList.contains("active");
 
-        this.toggleBtn.addEventListener("click", () => {
+        this.toggleBtn.addEventListener("click", e => {
+            e.stopPropagation();
+
             if (!this.isOpen) {
-                this.openMenu();
+                this.open();
             } else {
-                this.closeMenu();
+                this.close();
             }
         });
+
+        // close menu when clicking on links
+        this.actionMenuLinks.forEach(link => link.addEventListener("click", () => this.close()));
     }
 
-    openMenu () {
+    open () {
         this.actionMenu.classList.add("active");
         this.isOpen = true;
     }
 
-    closeMenu () {
+    close () {
         this.actionMenu.classList.remove("active");
         this.isOpen = false;
     }
@@ -38,9 +44,9 @@ const actionList = (() => {
         actionLists.forEach(l => {
             const list = new ActionList(l);
 
-            document.querySelector("html").addEventListener("mousedown", e => {
+            document.querySelector("html").addEventListener("click", e => {
                 if (list.isOpen && !list.containsPoint(e.clientX, e.clientY)) {
-                    list.closeMenu();
+                    list.close();
                 }
             });
         });
--- src/px-script/action-list/index.test.js
+++ src/px-script/action-list/index.test.js
@@ -8,14 +8,12 @@ describe("px-script: action-list", () => {
 
     document.body.appendChild(div);
 
-    const ActionList = () => (
+    const ActionList = ({ active }) => (
         <div className="action-list">
             <i className="material-icons">more_vert</i>
-            <div className="action-menu">
+            <div className={`action-menu${active ? " active" : ""}`}>
                 <a href="#"><i className="material-icons">bookmark</i>Add bookmark</a>
                 <a href="#"><i className="material-icons">business_center</i>Add client</a>
-                <a href="#"><i className="material-icons">add_circle</i>Add document</a>
-                <a href="#"><i className="material-icons">person_add</i>Add user</a>
             </div>
         </div>
     );
@@ -33,10 +31,74 @@ describe("px-script: action-list", () => {
         ReactDOM.render(<ActionList />, div);
 
         const renderedActionList = document.querySelector(".action-list");
+        const toggleBtn = renderedActionList.querySelector("i.material-icons");
+        const actionMenu = renderedActionList.querySelector(".action-menu");
 
         expect(renderedActionList).toBeDefined();
+        expect(toggleBtn).toBeDefined();
+        expect(actionMenu).toBeDefined();
+        expect(actionMenu.classList).not.toContain("active");
 
-        // TODO: Continue here... [EH]
+        actionList.init();
+        toggleBtn.click();
+
+        expect(actionMenu.classList).toContain("active");
+
+        ReactDOM.unmountComponentAtNode(div);
+    });
+
+    it("closes when clicking on icon while open", () => {
+        ReactDOM.render(<ActionList active />, div);
+
+        const renderedActionList = document.querySelector(".action-list");
+        const toggleBtn = renderedActionList.querySelector("i.material-icons");
+        const actionMenu = renderedActionList.querySelector(".action-menu");
+
+        expect(renderedActionList).toBeDefined();
+        expect(toggleBtn).toBeDefined();
+        expect(actionMenu).toBeDefined();
+        expect(actionMenu.classList).toContain("active");
+
+        actionList.init();
+        toggleBtn.click();
+
+        expect(actionMenu.classList).not.toContain("active");
+
+        ReactDOM.unmountComponentAtNode(div);
+    });
+
+    it("closes open action menu when clicking on a link", () => {
+        ReactDOM.render(<ActionList active />, div);
+
+        const renderedActionList = document.querySelector(".action-list");
+        const actionMenu = renderedActionList.querySelector(".action-menu");
+
+        expect(renderedActionList).toBeDefined();
+        expect(actionMenu).toBeDefined();
+        expect(actionMenu.classList).toContain("active");
+
+        actionList.init();
+        actionMenu.querySelector("a").click();
+
+        expect(actionMenu.classList).not.toContain("active");
+
+        ReactDOM.unmountComponentAtNode(div);
+    });
+
+    it("closes when clicking outside menu while open", () => {
+        ReactDOM.render(<ActionList active />, div);
+
+        const renderedActionList = document.querySelector(".action-list");
+        const actionMenu = renderedActionList.querySelector(".action-menu");
+
+        expect(renderedActionList).toBeDefined();
+        expect(actionMenu).toBeDefined();
+        expect(actionMenu.classList).toContain("active");
+
+        actionList.init();
+        document.querySelector("html").click(); // for clicking outside action list
+
+        expect(actionMenu.classList).not.toContain("active");
 
         ReactDOM.unmountComponentAtNode(div);
     });
