7727a8c3c746fa8943d8b146d2927e67bd835ccc
Thomas Lorntzen <cyrthomas1@gmail.com>
2018-10-04 11:41:54 +0200

    Fix missing lint

--- src/App/Documentation/components/StepBar/index.test.js
+++ src/App/Documentation/components/StepBar/index.test.js
@@ -1,8 +1,6 @@
 import React from "react";
 import { shallow } from "enzyme";
-
-import StepBar from "./index";
-import { BasicStepBar, StepBarText } from "./index";
+import { StepBar, BasicStepBar, StepBarText } from "./index";
 
 describe("Documentation: StepBar", () => {
     it("is defined", () => {
