36f072b2b59ad8f4264bdbf9835012123f426e39
Erik Hallaraaker <erikhallaraaker@gmail.com>
2018-10-08 17:21:43 +0200

    Continued with validation logic

--- src/App/Documentation/components/Validation/index.js
+++ src/App/Documentation/components/Validation/index.js
@@ -8,8 +8,16 @@ const ValidationOverview = () => (
         <h2 id="validation-overview">Validation overview</h2>
         <p><code className="token attr-name">data-validate</code>..</p>
         <ComponentPreview language="html" showCasePanel codeFigure>
-            <form action="#">
-                validation
+            <form action="#" noValidate>
+                <div className="form-group has-error">
+                    <label htmlFor="name">Name</label>
+                    <div className="input-group">
+                        <span className="input-group-addon">Mr.</span>
+                        <input type="text" className="form-control" id="name" placeholder="Enter your name" autoComplete="given-name" data-validate="" required />
+                    </div>
+                    <div className="help-block" data-error="Wrong!" data-success="Right!">This one should be easy</div>
+                </div>
+                <button className="btn btn-primary" type="submit" disabled>Submit</button>
             </form>
         </ComponentPreview>
     </>
--- src/px-script/validation/index.js
+++ src/px-script/validation/index.js
@@ -1,4 +1,23 @@
 const validation = (() => {
+    const _addInputValidation = input => {
+        input.addEventListener("focus", () => {
+            input.closest(".form-group").classList.remove("has-error");
+        });
+
+        input.addEventListener("blur", () => {
+            const isValid = input.checkValidity();
+            const parentFormGroup = input.closest(".form-group");
+
+            if (isValid) {
+                parentFormGroup.classList.remove("has-error");
+                parentFormGroup.classList.add("has-success");
+            } else {
+                parentFormGroup.classList.remove("has-success");
+                parentFormGroup.classList.add("has-error");
+            }
+        });
+    };
+
     const init = () => {
         const validateEls = document.querySelectorAll("[data-validate]");
 
@@ -6,8 +25,8 @@ const validation = (() => {
             validateEls.forEach(element => {
                 if (element.tagName === "FORM") {
                     console.log("FORM!");
-                } else {
-                    console.log("NOT FORM!");
+                } else if (element.tagName === "INPUT" || element.tagName === "TEXTAREA") {
+                    _addInputValidation(element);
                 }
             });
         }
