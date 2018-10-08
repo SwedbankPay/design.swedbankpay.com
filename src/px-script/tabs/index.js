bf8c2ee6e5ae4d1fe316fd3b0bb6074186752754
Aksel Christoffer Wiig <aksel.christoffer.wiig@gmail.com>
2018-10-08 09:45:30 +0200

    massive simplification of onclick for tabs

--- src/px-script/tabs/index.js
+++ src/px-script/tabs/index.js
@@ -11,6 +11,18 @@ class Tabs {
                 this.close();
             }
         });
+
+        [...this._el.children].forEach(elem => {
+            elem.addEventListener("click", e => {
+                e.preventDefault();
+
+                if (!this.isOpen) {
+                    this.open();
+                } else {
+                    this.close();
+                }
+            })
+        });
     }
 
     open () {
@@ -27,7 +39,7 @@ class Tabs {
     }
 
     close () {
-        if (this.isOpen && this.flexDir === "column") {
+        if (this.isOpen) {
             this.isOpen = false;
             this._el.classList.remove("tabs-open");
         }
@@ -36,28 +48,7 @@ class Tabs {
 
 const tabs = (() => {
     const init = () => {
-        const tabsresponsive = [...document.querySelectorAll(".tabs")].map(tabs => new Tabs(tabs));
-
-        [...document.querySelectorAll(".tabs")].forEach(responsive => {
-            const listElement = responsive.querySelectorAll("li");
-            const id = responsive.id;
-
-            let tabresp;
-
-            tabsresponsive.forEach(t => { t.id === id ? tabresp = t : null; });
-
-            [...listElement].forEach(anchor => {
-                anchor.addEventListener("click", e => {
-                    e.preventDefault();
-
-                    if (!tabresp.isOpen) {
-                        tabresp.open();
-                    } else {
-                        tabresp.close();
-                    }
-                });
-            });
-        });
+        [...document.querySelectorAll(".tabs")].map(tabs => new Tabs(tabs));
     };
 
     return { init };
