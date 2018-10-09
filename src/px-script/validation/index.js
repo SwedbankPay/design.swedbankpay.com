713f00183f78b9b9f7319152e420b02c5de56314
Erik Hallaraaker <erikhallaraaker@gmail.com>
2018-10-09 13:28:05 +0200

    Mostly done with validation logic

--- src/px-script/validation/index.js
+++ src/px-script/validation/index.js
@@ -1,19 +1,59 @@
 const validation = (() => {
+    const _checkValidity = input => {
+        if (!input.required && !input.value) {
+            return "NEUTRAL";
+        }
+
+        return input.checkValidity() ? "SUCCESS" : "ERRROR";
+    };
+
+    const _validateInput = input => {
+        const parentFormGroup = input.closest(".form-group");
+        const state = _checkValidity(input);
+
+        switch (state) {
+            case "SUCCESS":
+                parentFormGroup.classList.add("has-success");
+                parentFormGroup.classList.remove("has-error");
+
+                break;
+            case "ERROR":
+                parentFormGroup.classList.add("has-error");
+                parentFormGroup.classList.remove("has-success");
+
+                break;
+            default:
+                parentFormGroup.classList.remove("has-error");
+                parentFormGroup.classList.remove("has-success");
+        }
+
+        return state !== "ERROR";
+    };
+
     const _addInputValidation = input => {
         input.addEventListener("focus", () => {
+            input.closest(".form-group").classList.remove("has-success");
             input.closest(".form-group").classList.remove("has-error");
         });
 
-        input.addEventListener("blur", () => {
-            const isValid = input.checkValidity();
-            const parentFormGroup = input.closest(".form-group");
+        input.addEventListener("blur", () => _validateInput(input));
+    };
 
-            if (isValid) {
-                parentFormGroup.classList.remove("has-error");
-                parentFormGroup.classList.add("has-success");
-            } else {
-                parentFormGroup.classList.remove("has-success");
-                parentFormGroup.classList.add("has-error");
+    const _addFormValidation = form => {
+        const inputs = form.querySelectorAll("input");
+        const textareas = form.querySelectorAll("textarea");
+
+        [...inputs, ...textareas].forEach(input => _addInputValidation(input));
+
+        form.addEventListener("submit", e => {
+            let validForm = true;
+
+            [...inputs, ...textareas].forEach(input => {
+                _validateInput(input) ? validForm = false : null;
+            });
+
+            if (!validForm) {
+                e.preventDefault();
             }
         });
     };
@@ -24,8 +64,8 @@ const validation = (() => {
         if (validateEls) {
             validateEls.forEach(element => {
                 if (element.tagName === "FORM") {
-                    console.log("FORM!");
-                } else if (element.tagName === "INPUT" || element.tagName === "TEXTAREA") {
+                    _addFormValidation(element);
+                } else if (element.tagName === "INPUT") {
                     _addInputValidation(element);
                 }
             });
