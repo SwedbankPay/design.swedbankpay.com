61acf5a30c8e1f24629288cbc5ecce14e26106bf
Erik Hallaraaker <erikhallaraaker@gmail.com>
2018-10-19 10:20:04 +0200

    Forgot to revert a change to the deploy script

--- build/deploy-github-pages.ps1
+++ build/deploy-github-pages.ps1
@@ -6,8 +6,6 @@ if (($Env:APPVEYOR_REPO_TAG -eq "true") -and ($Env:GitVersion_BranchName -eq "ma
     Push-AppveyorArtifact "dist/release/PayEx.DesignGuide.v$($Env:GitVersion_FullSemVer).zip"
 }
 
-$formatted_basename = ($Env:basename -replace "/","\")
-
 # Deploy to gh-pages
 git config --global credential.helper store
 git config --global user.email $Env:github_email
@@ -15,7 +13,7 @@ git config --global user.name "payex-dev"
 Add-Content "$Env:USERPROFILE\.git-credentials" "https://$($Env:access_token):x-oauth-basic@github.com`n"
 git checkout -f gh-pages
 if ($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode)  }
-xcopy dist\* $formatted_basename\ /sy
+xcopy dist\* . /sy
 ls
 if ($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode)  }
 git add .
