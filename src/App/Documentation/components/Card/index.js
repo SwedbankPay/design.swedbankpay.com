715dc48fba82a028b7c602ac1498fb8a56a8efb4
Aksel Christoffer Wiig <aksel.christoffer.wiig@gmail.com>
2018-10-30 09:34:50 +0100

    class card-horizontal added

--- src/App/Documentation/components/Card/index.js
+++ src/App/Documentation/components/Card/index.js
@@ -4,15 +4,12 @@ import { ComponentPreview, Property, DocToc, ExperimentalComponentAlert } from "
 import CardComponent from "@/Card";
 import MediaObject from "@/MediaObject";
 
-const MultipleCards = () => (
+const Overview = () => (
     <>
-        <h2 id="multiplecards">MultipleCards</h2>
-        <p>MultipleCards... <Property value=".card" /></p>
-        <p>... <Property value=".card-default" /></p>
-        <p>... <Property value=".card-brand" /></p>
-        <p>... <Property value=".card-light" /></p>
+        <h2 id="overview">Cards horizontal</h2>
+        <p>Use the <Property value=".card" /> class and <Property value=".card-default" /> class on a container object to style it as a card.</p>
         <ComponentPreview language="html" showCasePanel codeFigure>
-            <div className="cards">
+            <div className="cards cards-md-horizontal">
                 <CardComponent
                     type="default"
                     title="Card default"
@@ -27,8 +24,60 @@ const MultipleCards = () => (
                 </CardComponent>
 
                 <CardComponent
+                    type="default"
+                    title="Card default"
+                    imgSrc="https://picsum.photos/300"
+                    text="This is the cards text"
+                    smallText="And the small text"
+                    btnTxt="Button"
+                    footerTxt="This is some text that goes in the footer."
+                    footerLinkTxt="You wanna click this link?"
+                >
+                    <MediaObject size="sm" icon="account_circle" heading="Bob Corlsan" textsm="bob.corlsan@payex.com" />
+                </CardComponent>
+
+                <CardComponent
+                    type="default"
+                    title="Card default"
+                    imgSrc="https://picsum.photos/300"
+                    text="This is the cards text"
+                    smallText="And the small text"
+                    btnTxt="Button"
+                    footerTxt="This is some text that goes in the footer."
+                    footerLinkTxt="You wanna click this link?"
+                >
+                    <MediaObject size="sm" icon="verified_user" heading="75%" textsm="Complete" />
+                </CardComponent>
+            </div>
+        </ComponentPreview>
+    </>
+);
+
+const CardTypes = () => (
+    <>
+        <h2 id="card-types">Card Types</h2>
+        <p>CardTypes... <Property value=".card" /></p>
+        <p>... <Property value=".card-default" /></p>
+        <p>... <Property value=".card-brand" /></p>
+        <p>... <Property value=".card-light" /></p>
+        <ComponentPreview language="html" showCasePanel codeFigure>
+            <div className="cards cards-md-horizontal">
+                <CardComponent
                     type="brand"
                     title="Card brand"
+                    imgSrc="https://picsum.photos/300"
+                    text="This is the cards text"
+                    smallText="And the small text"
+                    btnTxt="Button"
+                    footerTxt="This is some text that goes in the footer."
+                    footerLinkTxt="You wanna click this link?"
+                >
+                    <MediaObject size="sm" icon="account_balance" heading="725 NOK" textsm="Outstanding balance" />
+                </CardComponent>
+
+                <CardComponent
+                    type="default"
+                    title="Card default"
                     imgSrc="https://picsum.photos/300"
                     text="This is the cards text"
                     smallText="And the small text"
@@ -56,34 +105,12 @@ const MultipleCards = () => (
     </>
 );
 
-const DefaultCard = () => (
-    <>
-        <h2 id="default-card">Default Card</h2>
-        <p>Use the <Property value=".card" /> class and <Property value=".card-default" /> class on a container object to style it as a card.</p>
-        <ComponentPreview language="html" showCasePanel codeFigure>
-            <CardComponent
-                type="default"
-                title="Card default"
-                imgSrc="https://picsum.photos/300"
-                icon="account_balance"
-                highlightTitle="725 NOK"
-                highlightSubTitle="Outstanding balance"
-                text="This is the cards text"
-                smallText="And the small text"
-                btnTxt="Button"
-                footerTxt="This is some text that goes in the footer."
-                footerLinkTxt="You wanna click this link?"
-            />
-        </ComponentPreview>
-    </>
-);
-
 const CardText = () => (
     <div className="col-md-12 col-lg-10 doc-body">
         <ExperimentalComponentAlert />
         <p className="lead">Card...</p>
-        {/* <DefaultCard /> */}
-        <MultipleCards />
+        <Overview />
+        <CardTypes />
     </div>
 );
 
@@ -103,4 +130,4 @@ class Card extends Component {
 export default Card;
 
 /* For testing */
-export { DefaultCard ,MultipleCards, CardText };
+export { Overview, CardTypes, CardText };
