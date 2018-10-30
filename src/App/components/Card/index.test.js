4f885bd3cb0fb5dc17e0f9553cfe57460ab0e1ee
Aksel Christoffer Wiig <aksel.christoffer.wiig@gmail.com>
2018-10-30 15:53:33 +0100

    Writing tests for App/components/cards

--- src/App/components/Card/index.test.js
+++ src/App/components/Card/index.test.js
@@ -17,58 +17,75 @@ describe("Component: Card - ", () => {
         expect(wrapper).toMatchSnapshot();
     });
 
-    it("renders with class name card if no type is specified", () => {
+    it("renders with class name card", () => {
         const wrapper = shallow(<Card />);
 
-        expect(wrapper.find(".card")).toHaveLength(1);
+        expect(wrapper.html()).toContain("card");
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    it("renders with correct class name", () => {
+        const wrapper = shallow(<Card type="brand"/>);
+
+        expect(wrapper.html()).toContain("card-brand");
+        expect(wrapper.html()).not.toContain("card-default");
+        expect(wrapper.html()).not.toContain("card-light");
         expect(wrapper).toMatchSnapshot();
     });
 
     it("renders a header", () => {
         const wrapper = shallow(<Card title="Card default"/>);
 
+        expect(wrapper.html()).toContain("header");
         expect(wrapper).toMatchSnapshot();
     });
 
     it("renders an image", () => {
         const wrapper = shallow(<Card imgSrc="url.address" />);
 
-        expect(wrapper.find(".card-img")).toHaveLength(1);
+        expect(wrapper.html()).toContain("card-img");
         expect(wrapper).toMatchSnapshot();
     });
 
     it("renders an icon", () => {
         const wrapper = shallow(<Card icon="home" />);
 
-        expect(wrapper.find(".material-icons")).toHaveLength(1);
+        expect(wrapper.html()).toContain("material-icons");
         expect(wrapper).toMatchSnapshot();
     });
 
-    it("renders a highlight title", () => {
-        const wrapper = shallow(<Card highlightTitle="Balance" />);
+    it("renders a text in card-body", () => {
+        const wrapper = shallow(<Card text="Card text" />);
 
-        expect(wrapper.find(".highlight-title")).toHaveLength(1);
+        expect(wrapper.html()).toContain("p");
         expect(wrapper).toMatchSnapshot();
     });
 
-    it("renders a highlight subtitle", () => {
-        const wrapper = shallow(<Card highlightSubTitle="remaining amount to pay" />);
+    it("renders a smallText in card-body", () => {
+        const wrapper = shallow(<Card smallText="small text" />);
 
-        expect(wrapper.find(".highlight-subtitle")).toHaveLength(1);
+        expect(wrapper.html()).toContain("small");
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    it("renders a button in the footer", () => {
+        const wrapper = shallow(<Card btnTxt="Button" />);
+
+        expect(wrapper.html()).toContain("btn");
         expect(wrapper).toMatchSnapshot();
     });
 
     it("renders footer text", () => {
         const wrapper = shallow(<Card footerTxt="Footer information" />);
 
-        expect(wrapper.find(".footer-text")).toHaveLength(1);
+        expect(wrapper.html()).toContain("footer-text");
         expect(wrapper).toMatchSnapshot();
     });
 
     it("renders footer link text", () => {
         const wrapper = shallow(<Card footerLinkTxt="Click this footer link" />);
 
-        expect(wrapper.find(".footer-link")).toHaveLength(1);
+        expect(wrapper.html()).toContain("footer-link");
         expect(wrapper).toMatchSnapshot();
     });
 });
