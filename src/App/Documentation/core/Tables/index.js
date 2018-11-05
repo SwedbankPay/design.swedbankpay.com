3d5cdc249f6fc48b1f98d912365c0dc121b2620b
Aksel Christoffer Wiig <aksel.christoffer.wiig@gmail.com>
2018-11-05 11:57:22 +0100

    Added tables-condensed and simplified tables documentation

--- src/App/Documentation/core/Tables/index.js
+++ src/App/Documentation/core/Tables/index.js
@@ -2,53 +2,57 @@ import React from "react";
 
 import { ComponentPreview, DocToc } from "#";
 
+const DocTable = props => (
+    <table className={props.tableClass}>
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">First Name</th>
+                <th scope="col">Last Name</th>
+                <th scope="col">Location</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <th scope="row">1</th>
+                <td>Sven</td>
+                <td>Svensson</td>
+                <td>Visby</td>
+            </tr>
+            <tr>
+                <th scope="row">2</th>
+                <td>Sara</td>
+                <td>Svensson</td>
+                <td>Stockholm</td>
+            </tr>
+            <tr>
+                <th scope="row">3</th>
+                <td>Ola</td>
+                <td>Nordmann</td>
+                <td>Oslo</td>
+            </tr>
+            <tr>
+                <th scope="row">4</th>
+                <td>Holger</td>
+                <td>Danske</td>
+                <td>Copenhagen</td>
+            </tr>
+            <tr>
+                <th scope="row">5</th>
+                <td>Matti</td>
+                <td>Meikäläinen</td>
+                <td>Lahti</td>
+            </tr>
+        </tbody>
+    </table>
+);
+
 const BasicTable = () => (
     <>
         <h2 id="basic-table">Basic table</h2>
         <p>Basic tables info...</p>
         <ComponentPreview language="html" showCasePanel codeFigure>
-            <table className="table">
-                <thead>
-                    <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">First Name</th>
-                        <th scope="col">Last Name</th>
-                        <th scope="col">Location</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <th scope="row">1</th>
-                        <td>Sven</td>
-                        <td>Svensson</td>
-                        <td>Visby</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">2</th>
-                        <td>Sara</td>
-                        <td>Svensson</td>
-                        <td>Stockholm</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">3</th>
-                        <td>Ola</td>
-                        <td>Nordmann</td>
-                        <td>Oslo</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">4</th>
-                        <td>Holger</td>
-                        <td>Danske</td>
-                        <td>Copenhagen</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">5</th>
-                        <td>Matti</td>
-                        <td>Meikäläinen</td>
-                        <td>Lahti</td>
-                    </tr>
-                </tbody>
-            </table>
+            <DocTable tableClass="table" />
         </ComponentPreview>
     </>
 );
@@ -58,48 +62,7 @@ const StripedTable = () => (
         <h2 id="striped-table">Striped table</h2>
         <p>Striped tables info...</p>
         <ComponentPreview language="html" showCasePanel codeFigure>
-            <table className="table table-striped">
-                <thead>
-                    <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">First Name</th>
-                        <th scope="col">Last Name</th>
-                        <th scope="col">Location</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <th scope="row">1</th>
-                        <td>Sven</td>
-                        <td>Svensson</td>
-                        <td>Visby</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">2</th>
-                        <td>Sara</td>
-                        <td>Svensson</td>
-                        <td>Stockholm</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">3</th>
-                        <td>Ola</td>
-                        <td>Nordmann</td>
-                        <td>Oslo</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">4</th>
-                        <td>Holger</td>
-                        <td>Danske</td>
-                        <td>Copenhagen</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">5</th>
-                        <td>Matti</td>
-                        <td>Meikäläinen</td>
-                        <td>Lahti</td>
-                    </tr>
-                </tbody>
-            </table>
+            <DocTable tableClass="table table-striped" />
         </ComponentPreview>
     </>
 );
@@ -109,48 +72,7 @@ const HoverTable = () => (
         <h2 id="hover-table">Hover table</h2>
         <p>Hover tables info...</p>
         <ComponentPreview language="html" showCasePanel codeFigure>
-            <table className="table table-hover">
-                <thead>
-                    <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">First Name</th>
-                        <th scope="col">Last Name</th>
-                        <th scope="col">Location</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <th scope="row">1</th>
-                        <td>Sven</td>
-                        <td>Svensson</td>
-                        <td>Visby</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">2</th>
-                        <td>Sara</td>
-                        <td>Svensson</td>
-                        <td>Stockholm</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">3</th>
-                        <td>Ola</td>
-                        <td>Nordmann</td>
-                        <td>Oslo</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">4</th>
-                        <td>Holger</td>
-                        <td>Danske</td>
-                        <td>Copenhagen</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">5</th>
-                        <td>Matti</td>
-                        <td>Meikäläinen</td>
-                        <td>Lahti</td>
-                    </tr>
-                </tbody>
-            </table>
+            <DocTable tableClass="table table-hover" />
         </ComponentPreview>
     </>
 );
@@ -160,48 +82,7 @@ const HoverStripedTable = () => (
         <h2 id="hover-striped-table">Hover striped table</h2>
         <p>Hover striped tables info...</p>
         <ComponentPreview language="html" showCasePanel codeFigure>
-            <table className="table table-hover table-striped">
-                <thead>
-                    <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">First Name</th>
-                        <th scope="col">Last Name</th>
-                        <th scope="col">Location</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <th scope="row">1</th>
-                        <td>Sven</td>
-                        <td>Svensson</td>
-                        <td>Visby</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">2</th>
-                        <td>Sara</td>
-                        <td>Svensson</td>
-                        <td>Stockholm</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">3</th>
-                        <td>Ola</td>
-                        <td>Nordmann</td>
-                        <td>Oslo</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">4</th>
-                        <td>Holger</td>
-                        <td>Danske</td>
-                        <td>Copenhagen</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">5</th>
-                        <td>Matti</td>
-                        <td>Meikäläinen</td>
-                        <td>Lahti</td>
-                    </tr>
-                </tbody>
-            </table>
+            <DocTable tableClass="table table-hover table-striped" />
         </ComponentPreview>
     </>
 );
