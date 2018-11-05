ba6464875d9eabcf030f9e387237bd5c4a51b3ae
payex-dev <dev@payex.com>
2018-11-05 10:17:52 +0000

    GitHub release 0.12.3-documentation-more-card-variants.1+212

---
+++ documentation/more-card-variants/scripts/designguide.js
@@ -0,0 +1,2 @@
+(window.webpackJsonppayex=window.webpackJsonppayex||[]).push([[7],{407:function(p,n,w){}}]);
+//# sourceMappingURL=designguide.js.map?f7770694bb897b934dd2
---
+++ documentation/more-card-variants/scripts/px.js
@@ -0,0 +1,2 @@
+(window.webpackJsonppayex=window.webpackJsonppayex||[]).push([[61],{405:function(p,n,w){}}]);
+//# sourceMappingURL=px.js.map?f7770694bb897b934dd2
