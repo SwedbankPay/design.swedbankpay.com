f4b0d140f98318d9607a0f98e2ec88defff585b7
Aksel Christoffer Wiig <aksel.christoffer.wiig@gmail.com>
2018-11-07 08:35:05 +0100

    /App/components/MediaObject tests

--- src/App/components/MediaObject/index.test.js
+++ src/App/components/MediaObject/index.test.js
@@ -23,7 +23,45 @@ describe("Component: MediaObject - ", () => {
         expect(wrapper).toMatchSnapshot();
     });
 
-    it("renders a small MediaObject", () => {
+    it("renders an icon", () => {
+        const wrapper = shallow(<MediaObject heading="render-test" icon="home" />);
+
+        expect(wrapper).toMatchSnapshot();
+        expect(wrapper.find("i")).toHaveLength(1);
+    });
+
+    it("renders an image", () => {
+        const wrapper = shallow(<MediaObject heading="render-test" imgUrl="https://via.placeholder.com/48x48" />);
+
+        expect(wrapper).toMatchSnapshot();
+        expect(wrapper.find("img")).toHaveLength(1);
+    });
+
+    it("renders MediaObject with class media-right", () => {
+        const wrapper = shallow(<MediaObject heading="render-test" icon="home" mediaRight />);
+
+        expect(wrapper).toMatchSnapshot(1);
+        expect(wrapper.hasClass("media-right")).toEqual(true);
+    });
+
+    it("renders a <small> text", () => {
+        const wrapper = shallow(<MediaObject heading="render-test" textSmall text="small text" />);
+
+        expect(wrapper).toMatchSnapshot();
+        expect(wrapper.find("small")).toHaveLength(1);
+        expect(wrapper.html()).toContain("small text");
+    });
+
+    it("renders a muted MediaObject", () => {
+        const wrapper = shallow(<MediaObject heading="render-test" icon="home" muted />);
+        const mediaHeader = wrapper.find("h3");
+
+        expect(wrapper).toMatchSnapshot();
+        expect(mediaHeader.hasClass("text-muted")).toEqual(true);
+        expect(wrapper.find("i").hasClass("text-muted")).toEqual(true);
+    });
+
+    it("renders MediaObject with class media-sm", () => {
         const wrapper = shallow(<MediaObject heading="render-test" size="sm" />);
 
         expect(wrapper).toMatchSnapshot();
@@ -31,26 +69,11 @@ describe("Component: MediaObject - ", () => {
         expect(wrapper.hasClass("media-sm")).toEqual(true);
     });
 
-    it("renders a large MediaObject", () => {
+    it("renders MediaObject with class media-lg", () => {
         const wrapper = shallow(<MediaObject heading="render-test" size="lg" />);
 
         expect(wrapper).toMatchSnapshot();
         expect(wrapper.html()).toContain("h2");
         expect(wrapper.hasClass("media-lg")).toEqual(true);
-    });
-
-    it("renders a muted MediaObject", () => {
-        const wrapper = shallow(<MediaObject heading="render-test" muted />);
-        const mediaHeader = wrapper.find("h3");
-
-        expect(wrapper).toMatchSnapshot();
-        expect(mediaHeader.hasClass("text-muted")).toEqual(true);
-    });
-
-    it("renders a MediaObject with an icon", () => {
-        const wrapper = shallow(<MediaObject heading="render-test" icon="home"/>);
-
-        expect(wrapper).toMatchSnapshot();
-        expect(wrapper.find("i")).toHaveLength(1);
     });
 });
