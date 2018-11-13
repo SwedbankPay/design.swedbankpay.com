5be143ac807e31e29d80c69bd78c4bbbb56b7f50
Aksel Christoffer Wiig <aksel.christoffer.wiig@gmail.com>
2018-11-13 09:07:24 +0100

    Topbar rework WIP

--- src/App/Documentation/components/Topbar/index.js
+++ src/App/Documentation/components/Topbar/index.js
@@ -9,85 +9,6 @@ const menu = {
         icon: "menu",
         text: "Menu"
     },
-    hierarchy: [
-        {
-            id: "1",
-            items: [
-                {
-                    groupTitle: "Group 1",
-                    items: [
-                        {
-                            title: "Item 1",
-                            target: "2"
-                        },
-                        {
-                            title: "Item 2",
-                            target: "2"
-                        }
-                    ]
-                },
-                {
-                    groupTitle: "Group 2",
-                    items: [
-                        {
-                            title: "Item 1",
-                            target: "2"
-                        }
-                    ]
-                },
-                {
-                    title: "Link 1",
-                    href: "#"
-                }
-            ]
-        },
-        {
-            id: "2",
-            items: [
-                {
-                    groupTitle: "Group 3",
-                    items: [
-                        {
-                            title: "Item 1",
-                            target: "3"
-                        }
-                    ]
-                },
-                {
-                    title: "Link 2",
-                    href: "#"
-                },
-                {
-                    title: "Link 3",
-                    href: "#"
-                }
-            ]
-        },
-        {
-            id: "3",
-            items: [
-                {
-                    title: "Link 4",
-                    href: "#"
-                },
-                {
-                    title: "Link 5",
-                    href: "#"
-                },
-                {
-                    title: "Link 6",
-                    href: "#"
-                }
-            ]
-        }
-    ]
-};
-
-const smallMenu = {
-    btn: {
-        icon: "menu",
-        text: "Menu"
-    },
     items: [
         {
             title: "Link 1",
@@ -104,14 +25,9 @@ const smallMenu = {
     ]
 };
 
-const leftMenu = {
+const menuInfo = {
     id: "topbar-nav-left",
     ...menu
-};
-
-const rightMenu = {
-    id: "topbar-nav-right",
-    ...smallMenu
 };
 
 const Overview = () => (
@@ -119,7 +35,7 @@ const Overview = () => (
         <h2 id="overview">Overview</h2>
         <p>This will only show the code for the <Property value=".topbar" /> component, at least for now. For a demonstration of the usage check the example app (coming soon), or implement it in your own project!</p>
         <ComponentPreview language="html" showCasePanel codeFigure>
-            <TopbarComponent logo="default" leftMenu={leftMenu} rightMenu={rightMenu} />
+            <TopbarComponent logo="default" menuInfo={menuInfo} />
         </ComponentPreview>
     </>
 );
--- src/App/components/Topbar/index.js
+++ src/App/components/Topbar/index.js
@@ -8,80 +8,30 @@ const TopbarBtn = ({ align, icon, text, target }) => (
     </button>
 );
 
-const LeftMenu = ({ align, menu }) => {
-    const { id, hierarchy } = menu;
-
-    const Slide = ({ slide }) => {
-        const { id, items } = slide;
-
-        const MenuItem = ({ item, last }) => {
-            const { title, target, href } = item;
-
-            return (
-                <Fragment>
-                    {href ? <a href={href} onClick={e => e.preventDefault()}>{title}</a> : <span data-target={`#nav-${align}-slide-${target}`}>{title}</span>}{!href || last ? "\n" : null}
-                </Fragment>
-            );
-        };
-
-        const GroupItem = ({ groupItem }) => {
-            const { groupTitle, items } = groupItem;
-
-            return (
-                <div className="topbar-nav-group">
-                    <div className="topbar-nav-group-heading">{groupTitle}</div>{"\n"}
-                    {items.map((item, i) => <MenuItem key={i} item={item} last={i === items.length - 1} />)}
-                </div>
-            );
-        };
-
-        return (
-            <div id={`nav-${align}-slide-${id}`} className={"topbar-nav-slide"}>
-                <div className="slides-container">
-                    {items.map((item, i) => (
-                        <Fragment key={i}>{"\n"}
-                            {item.groupTitle ? <GroupItem groupItem={item} /> : <MenuItem item={item} last={i === items.length - 1} />}
-                        </Fragment>
-                    ))}
-                </div>
-            </div>
-        );
-    };
-
-    return (
-        <nav id={id} className={`topbar-nav topbar-nav-${align}`}>
-            {hierarchy.map(slide => <Slide key={slide.id} slide={slide}></Slide>)}
-        </nav>
-    );
-};
-
-const RightMenu = ({ align, menu }) => {
+const TopbarMenu = ({ menu }) => {
     const { id, items } = menu;
 
     return (
-        <nav id={id} className={`topbar-nav topbar-nav-${align}`}>
+        <nav id={id} className={"topbar-nav"}>
             {items.map((item, i) => (
-                <Fragment key={i}>{"\n"}
-                    <a href={item.href} onClick={e => e.preventDefault()}>{item.title}</a>
+                <Fragment key={i}>
+                    <a href={item.href} onClick={e => e.preventDefault()}>{item.title}</a>{"\n"}
                 </Fragment>
             ))}{"\n"}
         </nav>
     );
 };
 
-const Topbar = ({ leftMenu, rightMenu }) => (
+const Topbar = ({ menuInfo }) => (
     <div className="topbar">{"\n"}
-        {leftMenu ? <TopbarBtn align="left" icon={leftMenu.btn.icon} text={leftMenu.btn.text} target={leftMenu.id} /> : null}{leftMenu ? "\n" : null}
+        <TopbarBtn align="left" icon={menuInfo.btn.icon} text={menuInfo.btn.text} target={menuInfo.id} />
         <a href="#" className={"topbar-logo"} onClick={e => e.preventDefault()}></a>{"\n"}
-        {rightMenu ? <TopbarBtn align="right" icon={rightMenu.btn.icon} text={rightMenu.btn.text} target={rightMenu.id} /> : null}{rightMenu ? "\n" : null}
-        {leftMenu ? <LeftMenu align="left" menu={leftMenu} /> : null}{leftMenu ? "\n" : null}
-        {rightMenu ? <RightMenu align="right" menu={rightMenu} /> : null}{rightMenu ? "\n" : null}
+        <TopbarMenu menu={menuInfo} />
     </div>
 );
 
 Topbar.propTypes = {
-    leftMenu: PropTypes.object,
-    rightMenu: PropTypes.object
+    menuInfo: PropTypes.object
 };
 
 export default Topbar;
