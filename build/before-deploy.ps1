a5e60a95d8308d8f83dc646229474d1a922e0502
Erik Hallaraaker <erikhallaraaker@gmail.com>
2018-11-13 10:16:57 +0100

    Set up (temporary?) fix for pull requests crashing build

--- build/before-deploy.ps1
+++ build/before-deploy.ps1
@@ -1,7 +1,11 @@
-# Deploy to github pages
-./build/deploy-github-pages.ps1
+# Avoid deploying PR's as they do not have access to secrets
+if ($Env:GitVersion_PreReleaseLabel -NotMatch "PullRequest") {
+    # Deploy to github pages
+    ./build/deploy-github-pages.ps1
 
-# Check response time for scripts and styles
-npm run response-time
+    # Check response time for scripts and styles
+    npm run response-time
 
-# TODO: Run functional tests etc here [EH]
+    # TODO: Run functional tests etc here [EH]
+}
+
