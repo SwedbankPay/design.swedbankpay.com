b2ee61044e55328c83ed22c3b1d206de91c062ea
Aksel Christoffer Wiig <aksel.christoffer.wiig@gmail.com>
2018-11-15 13:17:44 +0100

    updating documentation page for slabs

--- src/App/Documentation/components/Slab/index.js
+++ src/App/Documentation/components/Slab/index.js
@@ -4,11 +4,78 @@ import { ComponentPreview, DocToc, ExperimentalComponentAlert, Property } from "
 
 const DefaultSlab = () => (
     <>
-        <h2 id="default-well">Default well</h2>
-        <p>Use the <Property value=".well" /> as a simple effect on an element to give it an inset effect.</p>
+        <h2 id="default-slab">Default slab</h2>
         <ComponentPreview language="html" showCasePanel codeFigure>
-            <div className="well">
+            <div className="slab">
+                <p className="lead">Put things in me!</p> {/* Too sexual? :) */}
+                <p>Do you want to sign up for the slab mailing list?</p>
+                <form action="#" noValidate data-validate>
+                    <div className="form-group">
+                        <label htmlFor="validation-email-2">Email</label>
+                        <div className="input-group">
+                            <span className="input-group-addon"><i className="material-icons">email</i></span>
+                            <input type="email" className="form-control" id="validation-email-2" placeholder="bob.corlsan@example.com" required />
+                        </div>
+                        <div className="help-block" data-success="Right!" data-error="Wrong!">This one might be a little tricky</div>
+                    </div>
+                    <button className="btn btn-primary" type="submit">Submit</button>
+                </form>
+            </div>
+        </ComponentPreview>
+    </>
+);
+
+const SlabElevated = () => (
+    <>
+        <h2 id="elevated-slab">Elevated slab</h2>
+        <ComponentPreview language="html" showCasePanel codeFigure>
+            <div className="slab slab-elevated">
+                <p>Look ma, im elevated!</p>
+            </div>
+        </ComponentPreview>
+    </>
+);
+
+const SlabWell = () => (
+    <>
+        <h2 id="slab-well">Slab well</h2>
+        <ComponentPreview language="html" showCasePanel codeFigure>
+            <div className="slab slab-well">
                 Look ma, im in a well!
+            </div>
+        </ComponentPreview>
+    </>
+);
+
+const SlabWhite = () => (
+    <>
+        <h2 id="slab-white">Slab white</h2>
+        <ComponentPreview language="html" showCasePanel codeFigure>
+            <div className="slab slab-white">
+                <h2>This slab can fit so many tabs in it.</h2>
+                <div className="tabs tabs-horizontal-xl">
+                    <i className="material-icons">keyboard_arrow_right</i>
+                    <ul>
+                        <li>
+                            <a href="#">Kort</a>
+                        </li>
+                        <li className="active">
+                            <a href="#">Rabatter</a>
+                        </li>
+                        <li>
+                            <a href="#">Transaksjoner</a>
+                        </li>
+                        <li>
+                            <a href="#">Faktura</a>
+                        </li>
+                        <li>
+                            <a href="#">Instillinger</a>
+                        </li>
+                        <li>
+                            <a href="#">Audit trail</a>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </ComponentPreview>
     </>
@@ -17,15 +84,22 @@ const DefaultSlab = () => (
 const SlabSizes = () => (
     <>
         <h2 id="well-sizes">Well sizes</h2>
-        <p>Use the two optional <Property value=".well-sm" /> and <Property value=".well-lg" /> classes to control padding and rounded corners.</p>
         <ComponentPreview language="html" showCasePanel codeFigure>
-            <div className="well well-sm">
-                Look ma, im in a small well!
+            <div className="slab slab-sm">
+                Look ma, im on a small slab!
             </div>
         </ComponentPreview>
         <ComponentPreview language="html" showCasePanel codeFigure>
-            <div className="well well-lg">
-                Look ma, im in a large well!
+            <div className="slab slab-white slab-lg">
+                <p className="lead">Here you can easily see how far along you are in this part of the slab docs. Ain't that neat?</p>
+                <ol className="step-bar step-bar--horizontal">
+                    <li className="step-bar-item-complete">Step one</li>
+                    <li className="step-bar-item-complete">Step two</li>
+                    <li className="step-bar-item-complete">Step three</li>
+                    <li className="step-bar-item-complete">
+                        <div className="material-icons step-bar-icon">check</div>Step four
+                    </li>
+                </ol>
             </div>
         </ComponentPreview>
     </>
@@ -34,8 +108,14 @@ const SlabSizes = () => (
 const SlabText = () => (
     <div className="col-lg-10 doc-body">
         <ExperimentalComponentAlert />
-        <p className="lead">Well well well, look what the cat dragged in...</p>
+        <p className="lead">
+            Slabs are meant to be used as container components,
+            therefore it works with whatever you want to put in it.
+        </p>
         <DefaultSlab />
+        <SlabElevated />
+        <SlabWell />
+        <SlabWhite />
         <SlabSizes />
     </div>
 );
@@ -52,4 +132,4 @@ const Well = () => (
 export default Well;
 
 /* For testing */
-export { DefaultSlab, SlabSizes, SlabText };
+export { DefaultSlab, SlabElevated, SlabWell, SlabWhite, SlabSizes, SlabText };
