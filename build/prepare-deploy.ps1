479a29dfed54ea6f69ef339d1a4e529ff9f3f99e
Erik Hallaraaker <erikhallaraaker@gmail.com>
2018-11-16 11:47:31 +0100

    Tweaked deploy script to skip deploy of dependabot branches

--- build/prepare-deploy.ps1
+++ build/prepare-deploy.ps1
@@ -1,5 +1,5 @@
 # Avoid deploying PR's as they do not have access to secrets
-if ($Env:GitVersion_PreReleaseLabel -NotMatch "PullRequest") {
+if ($Env:GitVersion_PreReleaseLabel -NotMatch "PullRequest" -And $Env:GitVersion_PreReleaseLabel -NotMatch "dependabot") {
     # Deploy to github pages
     ./build/deploy-github-pages.ps1
 
@@ -8,5 +8,5 @@ if ($Env:GitVersion_PreReleaseLabel -NotMatch "PullRequest") {
 
     # TODO: Run functional tests etc here [EH]
 } else {
-    Write-Host "Pull request, skipping deploy."
+    Write-Host "Pull request or dependabot branch, skipping deploy."
 }
