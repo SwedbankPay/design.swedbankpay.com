54967964ceb39a5066e7ba98bad749ee388f084e
Erik Hallaraaker <erikhallaraaker@gmail.com>
2018-11-19 17:19:50 +0100

    Set up some simple config for bar chart, started on pie

--- src/App/Documentation/dashboard/Chart/index.js
+++ src/App/Documentation/dashboard/Chart/index.js
@@ -4,12 +4,50 @@ import PrismCode from "react-prism";
 import { ComponentPreview, DocToc } from "#";
 import { chart } from "$/px-script/dashboard";
 
-const Overview = () => (
+const Bar = () => (
     <>
-        <h2 id="quick-start">Quick start</h2>
-        <p>To quickly add the dashboard theme to your project, include the snippets below in your project.</p>
-        <ComponentPreview language="html" codeFigure>
-            <canvas id="test-chart" width={400} height={400}></canvas>
+        <h2 id="bar">Bar</h2>
+        <p>To add a simple bar chart.</p>
+        <ComponentPreview language="html" showCasePanel codeFigure >
+            <canvas id="simple-bar-chart" />
+        </ComponentPreview>
+        <ComponentPreview language="javascript" codeFigure >
+            {`
+const options = {
+    type: "bar",
+    data: {
+        labels: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+        datasets: [
+            {
+                label: "Hours of daylight",
+                data: [6.75, 9.25, 11.75, 14.5, 17, 18.75, 18, 15.5, 13, 10.25, 7.5, 6]
+            }
+        ]
+    },
+    beginAtZero: true,
+};
+px.chart("test", options);
+            `}
+        </ComponentPreview>
+        <h3>Multiple datasets</h3>
+        <p>You can add multiple datasets</p>
+        <ComponentPreview language="html" showCasePanel codeFigure >
+            <canvas id="multi-bar-chart" />
+        </ComponentPreview>
+        <h3>Stacked bar</h3>
+        <p>You can stack</p>
+        <ComponentPreview language="html" showCasePanel codeFigure >
+            <canvas id="stacked-multi-bar-chart" />
+        </ComponentPreview>
+    </>
+);
+
+const Pie = () => (
+    <>
+        <h2 id="pie">Pie</h2>
+        <p>To add a simple pie chart.</p>
+        <ComponentPreview language="html" showCasePanel codeFigure >
+            <canvas id="simple-pie-chart" />
         </ComponentPreview>
     </>
 );
@@ -17,48 +55,72 @@ const Overview = () => (
 const ChartText = () => (
     <div className="col-lg-10 doc-body">
         <p className="lead">We are using <a href="https://www.chartjs.org/" target="_blank" rel="noopener noreferrer">chart.js</a>.</p>
-        <Overview />
+        <Bar />
+        <Pie />
     </div>
 );
 
 class Chart extends Component {
     componentDidMount () {
-        chart("test-chart", {
+        const simpleBarOptions = {
             type: "bar",
             data: {
-                labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-                datasets: [{
-                    label: "# of Votes",
-                    data: [12, 19, 3, 5, 2, 3],
-                    backgroundColor: [
-                        "rgba(255, 99, 132, 0.2)",
-                        "rgba(54, 162, 235, 0.2)",
-                        "rgba(255, 206, 86, 0.2)",
-                        "rgba(75, 192, 192, 0.2)",
-                        "rgba(153, 102, 255, 0.2)",
-                        "rgba(255, 159, 64, 0.2)"
-                    ],
-                    borderColor: [
-                        "rgba(255,99,132,1)",
-                        "rgba(54, 162, 235, 1)",
-                        "rgba(255, 206, 86, 1)",
-                        "rgba(75, 192, 192, 1)",
-                        "rgba(153, 102, 255, 1)",
-                        "rgba(255, 159, 64, 1)"
-                    ],
-                    borderWidth: 1
-                }]
+                labels: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+                datasets: [
+                    {
+                        label: "Hours of daylight",
+                        data: [6.75, 9.25, 11.75, 14.5, 17, 18.75, 18, 15.5, 13, 10.25, 7.5, 6]
+                    }
+                ]
             },
-            options: {
-                scales: {
-                    yAxes: [{
-                        ticks: {
-                            beginAtZero: true
-                        }
-                    }]
-                }
+            beginAtZero: true,
+            stacked: false
+        };
+
+        chart("simple-bar-chart", simpleBarOptions);
+
+        chart("simple-pie-chart", {
+            type: "pie",
+            data: {
+                labels: ["Test1", "Test2", "Test3"],
+                datasets: [
+                    {
+                        data: [10, 20, 30]
+                    }
+                ]
             }
         });
+
+        simpleBarOptions.data.datasets = [
+            ...simpleBarOptions.data.datasets,
+            {
+                label: "Total rain (in cm)",
+                data: [4.9, 3.6, 4.7, 4.1, 5.3, 6.5, 8.1, 8.9, 9, 8.4, 7.3, 5.5]
+            },
+            {
+                label: "Average temperature (in celcius)",
+                data: [-1.7, -4.1, -2.3, 6.5, 16.1, 17.9, 22.2, 16.3, 12.5, 7.4, 1.6, -1.5]
+            }
+        ];
+
+        chart("multi-bar-chart", simpleBarOptions);
+        simpleBarOptions.data.datasets = [
+            {
+                label: "Paid with cash",
+                data: [19, 23, 29, 24, 28, 33, 21, 24, 20, 17, 23, 24]
+            },
+            {
+                label: "Paid with credit card",
+                data: [31, 29, 32, 17, 25, 32, 28, 30, 24, 22, 20, 37]
+            },
+            {
+                label: "Paid with credit Vipps",
+                data: [11, 9, 7, 12, 4, 13, 8, 10, 4, 1, 13, 16]
+            }
+        ];
+        simpleBarOptions.stacked = true;
+
+        chart("stacked-multi-bar-chart", simpleBarOptions);
     }
 
     render () {
@@ -76,4 +138,4 @@ class Chart extends Component {
 export default Chart;
 
 /* For testing */
-export { Overview, ChartText };
+export { Bar, ChartText };
--- src/px-script/dashboard/chart/index.js
+++ src/px-script/dashboard/chart/index.js
@@ -1,14 +1,111 @@
 import Chart from "chart.js";
 
-const chart = (id, options) => {
+const _colorPool = [
+    "0, 67, 0", // darkened 40%
+    "0, 93, 0", // darkened 30%
+    "0, 118, 17", // darkened 20%
+    "20, 144, 43", // darkened 10%
+    "45, 169, 68", // original
+    "71, 195, 94", // lightened 10%
+    "96, 220, 119", // lightened 20%
+    "122, 246, 145", // lightened 30%
+    "147, 255, 170" // lightened 40%
+];
+
+const _prepareDataset = (dataset, index) => {
+    const { label, data, type } = dataset;
+
+    const preparedDataset = {
+        label,
+        data,
+        type,
+        backgroundColor: [],
+        borderColor: [],
+        hoverBackgroundColor: [],
+        hoverBorderColor: [],
+        borderWidth: 1
+    };
+
+    data.forEach((d, i) => {
+        preparedDataset.borderColor.push(`rgba(${_colorPool[index]}, 0.9)`);
+        preparedDataset.hoverBorderColor.push(`rgba(${_colorPool[index]}, 1)`);
+        preparedDataset.backgroundColor.push(`rgba(${_colorPool[index]}, ${type ? 0 : 0.4})`);
+        preparedDataset.hoverBackgroundColor.push(`rgba(${_colorPool[index]}, ${type ? 0 : 0.6})`);
+
+    });
+
+    return preparedDataset;
+};
+
+const _initPieChart = userOptions => userOptions;
+
+const _initBarChart = userOptions => {
+    const { type, data, beginAtZero, stacked } = userOptions;
+
+    return {
+        type,
+        data: {
+            labels: data.labels,
+            datasets: data.datasets.map((dataset, i) => _prepareDataset(dataset, i))
+        },
+        options: {
+            scales: {
+                xAxes: [
+                    {
+                        stacked,
+                        ticks: {
+                            beginAtZero
+                        }
+                    }
+                ],
+                yAxes: [
+                    {
+                        stacked,
+                        ticks: {
+                            beginAtZero
+                        }
+                    }
+                ]
+            }
+        }
+    };
+};
+
+const _init = (ctx, userOptions) => {
+    let options;
+
+    switch (userOptions.type) {
+        case "bar":
+        case "horizontalBar":
+            options = _initBarChart(userOptions);
+
+            break;
+        case "doughnut":
+        case "pie":
+            options = _initPieChart(userOptions);
+
+            break;
+        default:
+            console.warn(`Chart: Chart type "${userOptions.type}" is not supported.`);
+    }
+
+    if (options) {
+        new Chart(ctx, options);
+    }
+};
+
+const chart = (id, userOptions) => {
     const element = document.getElementById(id);
 
-    console.log(element);
-    // if (element) {
+    if (element && element.tagName === "CANVAS") {
+        const ctx = element.getContext("2d");
 
-    // } else {
-    //     console.warn("No ")
-    // }
+        _init(ctx, userOptions);
+    } else if (element) {
+        console.warn(`Chart: Element with id "${id}" is not of type canvas.`);
+    } else {
+        console.warn(`Chart: No element with id "${id}" found.`);
+    }
 };
 
 export default chart;
