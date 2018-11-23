531b3bac1e293389287c4a91b546d35bf4e81865
erikhallaraaker <erikhallaraaker@gmail.com>
2018-11-23 11:17:15 +0100

    Added snapshot tests for dashboard introduction and charts

---
+++ src/App/Documentation/dashboard/Introduction/index.test.js
@@ -0,0 +1,64 @@
+import React from "react";
+import { shallow } from "enzyme";
+
+import Introduction, { QuickStart, Css, JavaScript, IntroductionText } from "./index";
+
+describe("Dashboard: Introduction", () => {
+    it("is defined", () => {
+        expect(Introduction).toBeDefined();
+    });
+
+    it("renders", () => {
+        const wrapper = shallow(<Introduction />);
+
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    describe("QuickStart", () => {
+        it("is defined", () => {
+            expect(QuickStart).toBeDefined();
+        });
+
+        it("renders", () => {
+            const wrapper = shallow(<QuickStart />);
+
+            expect(wrapper).toMatchSnapshot();
+        });
+    });
+
+    describe("Css", () => {
+        it("is defined", () => {
+            expect(Css).toBeDefined();
+        });
+
+        it("renders", () => {
+            const wrapper = shallow(<Css />);
+
+            expect(wrapper).toMatchSnapshot();
+        });
+    });
+
+    describe("JavaScript", () => {
+        it("is defined", () => {
+            expect(JavaScript).toBeDefined();
+        });
+
+        it("renders", () => {
+            const wrapper = shallow(<JavaScript />);
+
+            expect(wrapper).toMatchSnapshot();
+        });
+    });
+
+    describe("IntroductionText", () => {
+        it("is defined", () => {
+            expect(IntroductionText).toBeDefined();
+        });
+
+        it("renders", () => {
+            const wrapper = shallow(<IntroductionText />);
+
+            expect(wrapper).toMatchSnapshot();
+        });
+    });
+});
