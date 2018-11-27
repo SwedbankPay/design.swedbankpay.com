2bce613b0439a8242a17599941dabb57de505fcc
erikhallaraaker <erikhallaraaker@gmail.com>
2018-11-27 15:08:04 +0100

    Added some other color options

--- src/px-script/dashboard/chart/index.js
+++ src/px-script/dashboard/chart/index.js
@@ -4,16 +4,36 @@ import initBarChart from "./bar";
 import initPieChart from "./pie";
 import initLineChart from "./line";
 
+// Shades of green
 const _colorPool = [
     "45, 169, 68", // original
-    "0, 67, 0", // darkened 40%
-    "147, 255, 170", // lightened 40%
-    "0, 93, 0", // darkened 30%
-    "122, 246, 145", // lightened 30%
-    "0, 118, 17", // darkened 20%
-    "96, 220, 119", // lightened 20%
-    "20, 144, 43", // darkened 10%
-    "71, 195, 94" // lightened 10%
+    "11, 93, 30",
+    "5, 59, 6",
+    "28, 105, 42",
+    "65, 245, 99",
+    "48, 182, 73",
+    "38, 143, 58"
+    // "0, 67, 0", // darkened 40%
+    // "147, 255, 170", // lightened 40%
+    // "0, 93, 0", // darkened 30%
+    // "122, 246, 145", // lightened 30%
+    // "0, 118, 17", // darkened 20%
+    // "96, 220, 119", // lightened 20%
+    // "20, 144, 43", // darkened 10%
+    // "71, 195, 94" // lightened 10%
+];
+
+const _colorPoolSecondary = [
+    "45, 169, 68", // brand
+    "38, 108, 154", // neutral
+    "125, 85, 170", // purple
+    "255, 159, 0", // warning
+    "205, 46, 0", // danger
+    "19, 70, 17", // up forest green (custom)
+    "0, 52, 89", // prussian blue (custom)
+    "106, 1, 54", // tyrian purple (custom)
+    "245, 184, 65", // maximum yellow (red) (custom)
+    "255, 34, 12" // red (ryb) (custom)
 ];
 
 const _init = (ctx, userOptions) => {
@@ -22,18 +42,18 @@ const _init = (ctx, userOptions) => {
     switch (userOptions.type) {
         case "bar":
         case "horizontalBar":
-            options = initBarChart(userOptions, _colorPool);
+            options = initBarChart(userOptions, _colorPoolSecondary);
 
             break;
 
         case "pie":
         case "doughnut":
-            options = initPieChart(userOptions, _colorPool);
+            options = initPieChart(userOptions, _colorPoolSecondary);
 
             break;
 
         case "line":
-            options = initLineChart(userOptions, _colorPool);
+            options = initLineChart(userOptions, _colorPoolSecondary);
 
             break;
         default:
