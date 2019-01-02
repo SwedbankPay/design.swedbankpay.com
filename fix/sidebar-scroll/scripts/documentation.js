832c7ce6ba9cf2280a6489d439430d7ba6d1905a
payex-dev <dev@payex.com>
2019-01-02 08:29:45 +0000

    GitHub release 0.16.0-fix-sidebar-scroll.1+25

---
+++ fix/sidebar-scroll/scripts/documentation.js
@@ -0,0 +1,2 @@
+(window.webpackJsonppayex=window.webpackJsonppayex||[]).push([[58],{418:function(p,n,w){}}]);
+//# sourceMappingURL=documentation.js.map?cff39d0076db2e66dded
---
+++ fix/sidebar-scroll/scripts/px.js
@@ -0,0 +1,2 @@
+(window.webpackJsonppayex=window.webpackJsonppayex||[]).push([[63],{417:function(p,n,w){}}]);
+//# sourceMappingURL=px.js.map?cff39d0076db2e66dded
