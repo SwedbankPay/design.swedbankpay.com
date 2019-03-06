ad3157ecb52a83dd2a839da365fb58d01617b313
payex-dev <dev@payex.com>
2019-03-06 12:24:44 +0000

    GitHub release 2.1.0-alpha.20

--- develop/scripts/documentation.js
+++ develop/scripts/documentation.js
@@ -1,2 +1,2 @@
 (window.webpackJsonppayex=window.webpackJsonppayex||[]).push([[58],{385:function(p,n,w){}}]);
-//# sourceMappingURL=documentation.js.map?585ccf8c2d520fd5b9a7
+//# sourceMappingURL=documentation.js.map?86fefc2f990e7afff695
