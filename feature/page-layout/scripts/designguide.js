6d2cf5ffc24ef21697ec308d28b3bc9927663919
payex-dev <dev@payex.com>
2019-04-15 14:16:06 +0000

    GitHub release 2.1.0-page-layout.1+50

--- feature/page-layout/scripts/designguide.js
+++ feature/page-layout/scripts/designguide.js
@@ -1,2 +1,2 @@
 (window.webpackJsonppayex=window.webpackJsonppayex||[]).push([[5],{343:function(p,n,w){}}]);
-//# sourceMappingURL=designguide.js.map?085aa3d612c8cb34e727
+//# sourceMappingURL=designguide.js.map?f0409194d425e7f30503
--- feature/page-layout/scripts/px.js
+++ feature/page-layout/scripts/px.js
@@ -1,2 +1,2 @@
-(window.webpackJsonppayex=window.webpackJsonppayex||[]).push([[62],{341:function(p,n,w){}}]);
-//# sourceMappingURL=px.js.map?085aa3d612c8cb34e727
+(window.webpackJsonppayex=window.webpackJsonppayex||[]).push([[63],{341:function(p,n,w){}}]);
+//# sourceMappingURL=px.js.map?f0409194d425e7f30503
