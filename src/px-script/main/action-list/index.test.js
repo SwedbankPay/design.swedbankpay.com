1a78469920a66ce2a1e034f76a7ef0d7a57184a2
Aksel Christoffer Wiig <aksel.christoffer.wiig@gmail.com>
2019-04-30 15:11:40 +0200

    Add action-list tests for open and close

--- src/px-script/main/action-list/index.test.js
+++ src/px-script/main/action-list/index.test.js
@@ -8,8 +8,8 @@ describe("px-script: action-list", () => {
 
     document.body.appendChild(div);
 
-    const ActionList = ({ active }) => (
-        <div className={`action-list${active ? " active" : ""}`}>
+    const ActionList = ({ active, id }) => (
+        <div className={`action-list${active ? " active" : ""}`} id={id}>
             <i className="material-icons">more_vert</i>
             <div className="action-menu">
                 <a href="#"><i className="material-icons">bookmark</i>Add bookmark</a>
@@ -17,6 +17,8 @@ describe("px-script: action-list", () => {
             </div>
         </div>
     );
+
+    beforeEach(() => ReactDOM.unmountComponentAtNode(div));
 
     it("is defined", () => {
         expect(actionList).toBeDefined();
@@ -41,8 +43,6 @@ describe("px-script: action-list", () => {
         toggleBtn.dispatchEvent(new Event("click"));
 
         expect(renderedActionList.classList).toContain("active");
-
-        ReactDOM.unmountComponentAtNode(div);
     });
 
     it("closes when clicking on icon while open", () => {
@@ -59,8 +59,6 @@ describe("px-script: action-list", () => {
         toggleBtn.dispatchEvent(new Event("click"));
 
         expect(renderedActionList.classList).not.toContain("active");
-
-        ReactDOM.unmountComponentAtNode(div);
     });
 
     it("closes open action menu when clicking on a link", () => {
@@ -75,8 +73,6 @@ describe("px-script: action-list", () => {
         renderedActionList.querySelector("a").click();
 
         expect(renderedActionList.classList).not.toContain("active");
-
-        ReactDOM.unmountComponentAtNode(div);
     });
 
     it("closes when clicking outside menu while open", () => {
@@ -91,7 +87,49 @@ describe("px-script: action-list", () => {
         document.querySelector("html").click(); // for clicking outside action list
 
         expect(renderedActionList.classList).not.toContain("active");
+    });
 
-        ReactDOM.unmountComponentAtNode(div);
+    it("opens actionlist when calling actionList.open", () => {
+        ReactDOM.render(<ActionList id="demo-actionlist" />, div);
+
+        const renderedActionList = document.querySelector(".action-list");
+
+        expect(renderedActionList).toBeTruthy();
+
+        actionList.init();
+        expect(renderedActionList.classList).not.toContain("active");
+
+        actionList.open("demo-actionlist");
+        expect(renderedActionList.classList).toContain("active");
+    });
+
+    it("does not open actionlist when calling actionlist.open with wrong id and prints error to console", () => {
+        console.error = jest.fn();
+
+        ReactDOM.render(<ActionList id="demo-actionlist" />, div);
+
+        const renderedActionList = document.querySelector(".action-list");
+
+        expect(renderedActionList).toBeTruthy();
+        expect(renderedActionList.classList).not.toContain("active");
+
+        actionList.open("invalid-id");
+
+        expect(renderedActionList.classList).not.toContain("active");
+        expect(console.error).toHaveBeenCalled();
+    });
+
+    it("closes an open actionlist when calling actionList.close", () => {
+        ReactDOM.render(<ActionList active id="demo-actionlist" />, div);
+
+        const renderedActionList = document.querySelector(".action-list");
+
+        expect(renderedActionList).toBeTruthy();
+        expect(renderedActionList.classList).toContain("active");
+
+        actionList.init();
+        actionList.close("demo-actionlist");
+
+        expect(renderedActionList.classList).not.toContain("active");
     });
 });
