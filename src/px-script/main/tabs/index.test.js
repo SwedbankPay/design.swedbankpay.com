9c0f97253d0617020791c71090ee64985d7a7c66
Aksel Christoffer Wiig <aksel.christoffer.wiig@gmail.com>
2019-05-06 10:45:18 +0200

    Finish tests for tabs

--- src/px-script/main/tabs/index.test.js
+++ src/px-script/main/tabs/index.test.js
@@ -32,9 +32,49 @@ describe("px-script: tabs", () => {
         expect(tabs).toBeTruthy();
     });
 
-    it("has an init method", () => {
-        expect(tabs.init).toBeTruthy();
-        expect(tabs.init).toBeInstanceOf(Function);
+    describe("test.init", () => {
+        it("has an init method", () => {
+            expect(tabs.init).toBeTruthy();
+            expect(tabs.init).toBeInstanceOf(Function);
+        });
+
+        it("returns one object when an ID is passed", () => {
+            ReactDOM.render(<NoActiveTab id="demo-tabs-1" />, div);
+
+            const renderedTab = document.querySelector(".tabs");
+
+            expect(renderedTab).toBeTruthy();
+
+            const returnVal = tabs.init("demo-tabs-1");
+
+            expect(Array.isArray(returnVal)).toBeFalsy();
+            expect(typeof returnVal).toEqual("object");
+        });
+
+        it("returns an array of objects when more than one element is initialized", () => {
+            ReactDOM.render(
+                <>
+                    <NoActiveTab />
+                    <NoActiveTab />
+                </>
+                , div);
+
+            const renderedTabs = document.querySelectorAll(".tabs");
+
+            expect(renderedTabs).toBeTruthy();
+            expect(renderedTabs.length).toEqual(2);
+
+            const returnVal = tabs.init();
+
+            expect(Array.isArray(returnVal)).toBeTruthy();
+            expect(returnVal.length).toEqual(2);
+        });
+
+        it("returns null if no tabs is found", () => {
+            const returnVal = tabs.init();
+
+            expect(returnVal).toBeNull();
+        });
     });
 
     it("sets an active item if none has been provided", () => {
@@ -175,7 +215,7 @@ describe("px-script: tabs", () => {
         it("prints an error message if no tabs with matching ID exists", () => {
             console.error = jest.fn();
 
-            tabs.open("demo-tabs-1");
+            tabs.open("demo-tabs-2"); // Tests persist the _tabs array, have to use unique ID to test [AW]
 
             expect(console.error).toHaveBeenCalled();
         });
@@ -192,7 +232,7 @@ describe("px-script: tabs", () => {
             expect(renderedTabs).toBeTruthy();
             expect(renderedTabs.classList).toContain("tabs-open");
 
-            tabs.close("demo-tabs");
+            tabs.close("demo-tabs"); // Tests persist the _tabs array, have to use unique ID to test [AW]
 
             expect(renderedTabs.classList).not.toContain("tabs-open");
         });
@@ -200,11 +240,11 @@ describe("px-script: tabs", () => {
         it("prints an error message if no tabs with matching ID exists", () => {
             console.error = jest.fn();
 
-            tabs.close("demo-tabs-1");
+            tabs.close("demo-tabs-2");
 
             expect(console.error).toHaveBeenCalled();
         });
     });
 
-    test.todo("Tests persist the _tabs array through test cases. No way to \"reset\" an imported module, have to use require. [AW]");
+    // Tests persist the _tabs array through test cases. No way to "reset" an imported module, have to use require. [AW]
 });
