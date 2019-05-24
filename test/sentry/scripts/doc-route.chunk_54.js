b313f9e04c85cc443bc292693ea4ca8a183c7b3f
payex-dev <dev@payex.com>
2019-05-24 07:57:28 +0000

    GitHub release 2.3.0-test-sentry.1+11

--- test/sentry/scripts/doc-route.chunk_54.js
+++ test/sentry/scripts/doc-route.chunk_54.js
@@ -1,2 +1,2 @@
 (window.webpackJsonppayex=window.webpackJsonppayex||[]).push([[53],{372:function(e,n,a){"use strict";a.r(n);var i=a(1),r=a.n(i),s=a(65),c=function(e){var n=e.icon;return r.a.createElement("i",{className:"material-icons"},n)};c.propTypes={icon:a.n(s).a.string.isRequired},n.default=c}}]);
-//# sourceMappingURL=doc-route.chunk_54.js.map?a391c8b65c95559f8d61
+//# sourceMappingURL=doc-route.chunk_54.js.map?f95ac7be328781d67a17
