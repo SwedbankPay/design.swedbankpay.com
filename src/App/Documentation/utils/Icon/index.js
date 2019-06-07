35db5a735f34f6a8d324263cfad2ae3c13278dee
Aksel Christoffer Wiig <aksel.christoffer.wiig@gmail.com>
2019-06-07 10:04:27 +0200

    Add classnames prop to Icon util

--- src/App/Documentation/utils/Icon/index.js
+++ src/App/Documentation/utils/Icon/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const Icon = ({ icon }) => <i className="material-icons">{icon}</i>;
+const Icon = ({ icon, classNames }) => <i className={`material-icons${classNames ? ` ${classNames}` : ""}`}>{icon}</i>;
 
 Icon.propTypes = { icon: PropTypes.string.isRequired };
 
