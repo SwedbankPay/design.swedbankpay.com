1c1c7efd3d72c205b416b1ed7e18e79393196826
payex-dev <dev@payex.com>
2019-06-13 11:14:49 +0000

    GitHub release 0.1.0-build-test.1+1968

---
+++ feature/build_test/scripts/designguide.js
@@ -0,0 +1,2 @@
+(window.webpackJsonppayex=window.webpackJsonppayex||[]).push([[5],{354:function(p,n,w){}}]);
+//# sourceMappingURL=designguide.js.map?81203e81393aa5d172d9
---
+++ feature/build_test/scripts/px.js
@@ -0,0 +1,2 @@
+(window.webpackJsonppayex=window.webpackJsonppayex||[]).push([[65],{352:function(p,n,w){}}]);
+//# sourceMappingURL=px.js.map?81203e81393aa5d172d9
