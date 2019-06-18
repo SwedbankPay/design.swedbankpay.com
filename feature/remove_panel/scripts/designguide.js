b600e863def014b87f439915f817b4efdeac9330
payex-dev <dev@payex.com>
2019-06-18 13:40:51 +0000

    GitHub release 2.3.0-remove-panel.1+146

---
+++ feature/remove_panel/scripts/designguide.js
@@ -0,0 +1,2 @@
+(window.webpackJsonpswedbankpay=window.webpackJsonpswedbankpay||[]).push([[5],{354:function(n,w,p){}}]);
+//# sourceMappingURL=designguide.js.map?4fd7280a18cb87729cb4
---
+++ feature/remove_panel/scripts/documentation.js
@@ -0,0 +1,2 @@
+(window.webpackJsonpswedbankpay=window.webpackJsonpswedbankpay||[]).push([[61],{353:function(n,w,p){}}]);
+//# sourceMappingURL=documentation.js.map?4fd7280a18cb87729cb4
