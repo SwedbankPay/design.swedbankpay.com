231df0eddb6e6e830594df1e11e620f971c60252
Aksel Christoffer Wiig <aksel.christoffer.wiig@gmail.com>
2019-07-01 11:36:04 +0200

    Landing page suggestion

--- src/App/Home/index.js
+++ src/App/Home/index.js
@@ -1,29 +1,62 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 
 import { setTitle } from "../utils";
 
-class Home extends Component {
-    componentDidMount () {
+const Home = () => {
+    useEffect(() => {
         setTitle("Home");
-    }
+    });
 
-    render () {
-        return (
-            <div className="container set-doc-height">
-                <div className="row justify-content-center">
-                    <div className="col-md-auto">
-                        <section className="panel panel-default">
-                            <div className="panel-body">
-                                <h2>Welcome to the PayEx DesignGuide</h2>
-                                <p>You might wanna go look at the <Link to="/docs">documentation</Link> </p>
+    return (
+        <div className="container fp-container pt-3 pb-3">
+            <div className="alert alert-warning">
+                <header className="alert-header">
+                    <i className="material-icons alert-icon">warning</i>
+                    <h3>Site under development</h3>
+                </header>
+                <div className="alert-body">
+                    <p>The Design Guide is under active development and thus subject to change.</p>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-12 col-md-4 pt-3 pt-md-0">
+                    <div className="card card-plain">
+                        <header className="card-header">
+                            <h2>Placeholder</h2>
+                        </header>
+                        <div className="card-body">
+                            <p>This paragraph contains some text.</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-12 col-md-4 pt-3 pt-md-0">
+                    <div className="card card-plain">
+                        <header className="card-header">
+                            <h2>Design Guide</h2>
+                        </header>
+                        <div className="card-body">
+                            <div className="d-flex justify-content-center">
+                                <i className="material-icons material-icons-large text-brand">computer</i>
                             </div>
-                        </section>
+                            <p>The Design Guide contains our component library for web development and general documentation for component use.</p>
+                            <p>Check out the <Link to="/docs/getting-started/introduction">documentation</Link>.</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-12 col-md-4 pt-3 pt-md-0">
+                    <div className="card card-plain">
+                        <header className="card-header">
+                            <h2>Placeholder</h2>
+                        </header>
+                        <div className="card-body">
+                            <p>This paragraph contains some text.</p>
+                        </div>
                     </div>
                 </div>
             </div>
-        );
-    }
-}
+        </div>
+    );
+};
 
 export default Home;
