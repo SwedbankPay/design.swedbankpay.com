a9f4df0cda99e8229932b19f6c185266fdfb461a
payex-dev <dev@payex.com>
2019-07-04 09:59:18 +0000

    GitHub release 3.2.0-cards-use-grid.1+18

--- feature/cards-use-grid/scripts/px.js
+++ feature/cards-use-grid/scripts/px.js
@@ -1,2 +1,2 @@
 (window.webpackJsonpswedbankpay=window.webpackJsonpswedbankpay||[]).push([[65],{352:function(n,w,p){}}]);
-//# sourceMappingURL=px.js.map?a4f652132b39961f0989
+//# sourceMappingURL=px.js.map?5cce6cc275fcc1f331c0
