011fb0c89169c17572c978763bc7071c1a965d92
payex-dev <dev@payex.com>
2019-07-08 08:55:00 +0000

    GitHub release 3.2.0-footer-breakpoint.1+30

---
+++ feature/footer-breakpoint/scripts/documentation.js
@@ -0,0 +1,2 @@
+(window.webpackJsonpswedbankpay=window.webpackJsonpswedbankpay||[]).push([[61],{353:function(n,w,p){}}]);
+//# sourceMappingURL=documentation.js.map?0b5cad10fc9b8fb53550
---
+++ feature/footer-breakpoint/scripts/px.js
@@ -0,0 +1,2 @@
+(window.webpackJsonpswedbankpay=window.webpackJsonpswedbankpay||[]).push([[65],{352:function(n,w,p){}}]);
+//# sourceMappingURL=px.js.map?0b5cad10fc9b8fb53550
