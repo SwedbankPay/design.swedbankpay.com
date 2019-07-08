0b7d8d09c0007fae3724bfae332283b4da7058cb
payex-dev <dev@payex.com>
2019-07-08 09:53:42 +0000

    GitHub release 3.2.0-front-page.1+37

--- feature/front-page/scripts/designguide.js
+++ feature/front-page/scripts/designguide.js
@@ -1,2 +1,2 @@
 (window.webpackJsonpswedbankpay=window.webpackJsonpswedbankpay||[]).push([[5],{355:function(n,w,p){}}]);
-//# sourceMappingURL=designguide.js.map?e9d7b5f4bb27604292a6
+//# sourceMappingURL=designguide.js.map?e19320e7c51b1687e754
--- feature/front-page/scripts/px.js
+++ feature/front-page/scripts/px.js
@@ -1,2 +1,2 @@
 (window.webpackJsonpswedbankpay=window.webpackJsonpswedbankpay||[]).push([[66],{353:function(n,w,p){}}]);
-//# sourceMappingURL=px.js.map?e9d7b5f4bb27604292a6
+//# sourceMappingURL=px.js.map?e19320e7c51b1687e754
