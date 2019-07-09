9e40b20182574944902fb1206c5532387e3816a6
payex-dev <dev@payex.com>
2019-07-09 07:38:40 +0000

    GitHub release 3.2.0-px-sheet-fix.1+31

--- feature/px-sheet-fix/scripts/designguide.js
+++ feature/px-sheet-fix/scripts/designguide.js
@@ -1,2 +1,2 @@
 (window.webpackJsonpswedbankpay=window.webpackJsonpswedbankpay||[]).push([[5],{354:function(n,w,p){}}]);
-//# sourceMappingURL=designguide.js.map?a68f2aaeea9d203d1bd2
+//# sourceMappingURL=designguide.js.map?6ef1ee1397d99e031f64
--- feature/px-sheet-fix/scripts/documentation.js
+++ feature/px-sheet-fix/scripts/documentation.js
@@ -1,2 +1,2 @@
 (window.webpackJsonpswedbankpay=window.webpackJsonpswedbankpay||[]).push([[61],{353:function(n,w,p){}}]);
-//# sourceMappingURL=documentation.js.map?a68f2aaeea9d203d1bd2
+//# sourceMappingURL=documentation.js.map?6ef1ee1397d99e031f64
