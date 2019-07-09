6d175536a7e276dcd0fc45532d0e813e184b2508
Aksel Christoffer Wiig <aksel.christoffer.wiig@gmail.com>
2019-07-09 09:51:40 +0200

    Update footer docs and set font on .page-footer-rights

--- src/App/Documentation/components/Footer/index.js
+++ src/App/Documentation/components/Footer/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 import { ComponentPreview, DocContainer, Property } from "#";
 import FooterComponent from "@/Footer";
@@ -9,7 +8,7 @@ const Overview = () => (
         <h2 id="overview">Overview</h2>
         <p>
             Our footer component comes with the same brand color as our topbar.
-            It also supports aligning text with <Property value=".page-footer-rights"/> when a page reaches the small <Link to="/docs/core/breakpoints">breakpoint</Link>.
+            The <Property value="page-footer-rights" /> class ensures that the wrapped text is centered on mobile.
         </p>
         <ComponentPreview language="html" showCasePanel codeFigure>
             <FooterComponent />
