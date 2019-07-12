1ed240716ecdb729bd34f9501905eb43e6a099b6
payex-dev <dev@payex.com>
2019-07-12 08:18:33 +0000

    GitHub release 3.3.0-typography-updates.1+2

---
+++ feature/typography-updates/scripts/designguide.js
@@ -0,0 +1,2 @@
+(window.webpackJsonpswedbankpay=window.webpackJsonpswedbankpay||[]).push([[5],{355:function(n,w,p){}}]);
+//# sourceMappingURL=designguide.js.map?4fc213b60573fdd42158
---
+++ feature/typography-updates/scripts/px.js
@@ -0,0 +1,2 @@
+(window.webpackJsonpswedbankpay=window.webpackJsonpswedbankpay||[]).push([[66],{353:function(n,w,p){}}]);
+//# sourceMappingURL=px.js.map?4fc213b60573fdd42158
