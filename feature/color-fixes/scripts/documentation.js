ee9b1cd5b91befa057db01c107e295a0cbe0908e
payex-dev <dev@payex.com>
2019-07-18 13:14:07 +0000

    GitHub release 3.3.0-color-fixes.1+5

--- feature/color-fixes/scripts/documentation.js
+++ feature/color-fixes/scripts/documentation.js
@@ -1,2 +1,2 @@
 (window.webpackJsonpswedbankpay=window.webpackJsonpswedbankpay||[]).push([[61],{353:function(n,w,p){}}]);
-//# sourceMappingURL=documentation.js.map?463a48ff59285b984287
+//# sourceMappingURL=documentation.js.map?677835b18ceca52e5da0
--- feature/color-fixes/scripts/px.js
+++ feature/color-fixes/scripts/px.js
@@ -1,2 +1,2 @@
 (window.webpackJsonpswedbankpay=window.webpackJsonpswedbankpay||[]).push([[65],{352:function(n,w,p){}}]);
-//# sourceMappingURL=px.js.map?463a48ff59285b984287
+//# sourceMappingURL=px.js.map?677835b18ceca52e5da0
