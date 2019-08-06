67c55e66bf9a98dee7568b20ce54b82eca95d39d
payex-dev <dev@payex.com>
2019-08-06 09:48:12 +0000

    GitHub release 3.5.0-chrome-topbar-icon-fix.1+2

---
+++ feature/chrome-topbar-icon-fix/scripts/designguide.js
@@ -0,0 +1,2 @@
+(window.webpackJsonpswedbankpay=window.webpackJsonpswedbankpay||[]).push([[6],{349:function(n,w,p){}}]);
+//# sourceMappingURL=designguide.js.map?388db0a1355c234fcb03
---
+++ feature/chrome-topbar-icon-fix/scripts/documentation.js
@@ -0,0 +1,2 @@
+(window.webpackJsonpswedbankpay=window.webpackJsonpswedbankpay||[]).push([[63],{348:function(n,w,p){}}]);
+//# sourceMappingURL=documentation.js.map?388db0a1355c234fcb03
