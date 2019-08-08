a54ee132192d3739ead22b4706c09bc77207d22f
Aksel Christoffer Wiig <aksel.christoffer.wiig@gmail.com>
2019-08-08 10:16:16 +0200

    Add dialog component

---
+++ src/App/components/Dialog/index.js
@@ -0,0 +1,33 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import { Icon } from "#";
+
+const Dialog = ({ diaId, diaHeader, children }) => (
+    <>
+        <div className="dialog" id={diaId}>
+            <section>
+                <header className="dialog-header">
+                    <h4>{diaHeader}</h4>
+                    <button type="button" className="dialog-close">
+                        <Icon type="close" />
+                    </button>
+                </header>
+                <div className="dialog-body">
+                    {children}
+                </div>
+                <footer className="dialog-footer">
+                    <button className="btn btn-guiding" type="button" data-dialog-close>Cancel</button>{"\n"}
+                    <button className="btn btn-destructive" type="button">Delete</button>{"\n"}
+                </footer>
+            </section>
+        </div>
+    </>
+);
+
+Dialog.propTypes = {
+    diaId: PropTypes.string,
+    diaHeader: PropTypes.string
+};
+
+export default Dialog;
---
+++ src/App/components/Dialog/index.test.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { shallow } from "enzyme";
+
+import Dialog from "./index";
+
+describe("Component: Dialog - ", () => {
+    it("is defined", () => {
+        expect(Dialog).toBeDefined();
+    });
+
+    it("renders", () => {
+        const wrapper = shallow(<Dialog />);
+
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    it("renders with the passed ID", () => {
+        const wrapper = shallow(<Dialog diaId="my-id"/>);
+
+        expect(wrapper).toMatchSnapshot();
+        expect(wrapper.html()).toContain("my-id");
+    });
+
+    it("renders with the passed header", () => {
+        const wrapper = shallow(<Dialog diaHeader="My heading"/>);
+
+        expect(wrapper).toMatchSnapshot();
+        expect(wrapper.html()).toContain("My heading");
+    });
+
+    it("renders with the passed children", () => {
+        const wrapper = shallow(<Dialog><p>My paragraph</p></Dialog>);
+
+        expect(wrapper).toMatchSnapshot();
+        expect(wrapper.html()).toContain("<p>My paragraph</p>");
+    });
+});
