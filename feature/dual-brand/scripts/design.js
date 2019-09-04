6581d531f1b373784ee9b0881470f55464049c1c
payex-dev <dev@payex.com>
2019-09-04 12:19:11 +0000

    GitHub release 3.6.0-dual-brand.1+18

--- feature/dual-brand/scripts/design.js
+++ feature/dual-brand/scripts/design.js
@@ -1,2 +1,2 @@
 (window.webpackJsonpswedbankpay=window.webpackJsonpswedbankpay||[]).push([[6],{351:function(n,w,p){}}]);
-//# sourceMappingURL=design.js.map?a354b274dc911bbde585
+//# sourceMappingURL=design.js.map?a987cb154369952be594
--- feature/dual-brand/scripts/documentation.js
+++ feature/dual-brand/scripts/documentation.js
@@ -1,2 +1,2 @@
 (window.webpackJsonpswedbankpay=window.webpackJsonpswedbankpay||[]).push([[65],{352:function(n,w,p){}}]);
-//# sourceMappingURL=documentation.js.map?a354b274dc911bbde585
+//# sourceMappingURL=documentation.js.map?a987cb154369952be594
