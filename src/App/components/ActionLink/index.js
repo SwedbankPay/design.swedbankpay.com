f0b7cb467cf5324d0d9c2dcf1cdfa131036bef5d
Phi-Thien Hoang <Phi-Thien.Hoang@payex.com>
2019-09-09 13:55:00 +0200

    Add ActionLink component and tests

---
+++ src/App/components/ActionLink/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const ActionLink = ({linkText, badge, smallText}) => (
+    <a className="action-link" href="#">{"\n"}
+        {badge ? <span className={`badge ${badge.type}`}>{badge.text}</span> : null}{"\n"}
+        {linkText}{"\n"}
+    </a>
+);
+
+ActionLink.prototype = {
+    linkText: PropTypes.string.isRequired,
+    badge: PropTypes.shape({
+        type: PropTypes.string.isRequired,
+        text: PropTypes.string.isRequired
+    }),
+    smallText: PropTypes.string
+};
+
+export default ActionLink;
