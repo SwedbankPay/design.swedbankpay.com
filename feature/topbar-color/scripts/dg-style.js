f50ff68b609c1821a49315c32a9b388a2b656cee
payex-dev <dev@payex.com>
2019-09-10 12:00:46 +0000

    GitHub release 2.0.2-topbar-color.1+548

---
+++ feature/topbar-color/scripts/dg-style.js
@@ -0,0 +1,2 @@
+(window.webpackJsonpswedbankpay=window.webpackJsonpswedbankpay||[]).push([[8],{351:function(n,w,p){}}]);
+//# sourceMappingURL=dg-style.js.map?e647889c9ab81c6d515e
---
+++ feature/topbar-color/scripts/documentation.js
@@ -0,0 +1,2 @@
+(window.webpackJsonpswedbankpay=window.webpackJsonpswedbankpay||[]).push([[65],{352:function(n,w,p){}}]);
+//# sourceMappingURL=documentation.js.map?e647889c9ab81c6d515e
