45a70dde982550fb2396a3e8330bd4af4382ed28
payex-dev <dev@payex.com>
2019-09-10 12:36:49 +0000

    GitHub release 2.0.2-badge-round.1+557

---
+++ feature/badge-round/scripts/dg-style.js
@@ -0,0 +1,2 @@
+(window.webpackJsonpswedbankpay=window.webpackJsonpswedbankpay||[]).push([[8],{351:function(n,w,p){}}]);
+//# sourceMappingURL=dg-style.js.map?8f7187f1908d2b011ac3
---
+++ feature/badge-round/scripts/documentation.js
@@ -0,0 +1,2 @@
+(window.webpackJsonpswedbankpay=window.webpackJsonpswedbankpay||[]).push([[65],{352:function(n,w,p){}}]);
+//# sourceMappingURL=documentation.js.map?8f7187f1908d2b011ac3
