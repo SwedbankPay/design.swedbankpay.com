20ae610842497f06f3e3d72a7f9ff82320f99253
payex-dev <dev@payex.com>
2019-09-10 14:03:50 +0000

    GitHub release 3.6.0-color-rename.1+71

---
+++ feature/color-rename/scripts/dg-style.js
@@ -0,0 +1,2 @@
+(window.webpackJsonpswedbankpay=window.webpackJsonpswedbankpay||[]).push([[8],{351:function(n,w,p){}}]);
+//# sourceMappingURL=dg-style.js.map?2a00a6250ad6fe8cfa81
---
+++ feature/color-rename/scripts/documentation.js
@@ -0,0 +1,2 @@
+(window.webpackJsonpswedbankpay=window.webpackJsonpswedbankpay||[]).push([[65],{352:function(n,w,p){}}]);
+//# sourceMappingURL=documentation.js.map?2a00a6250ad6fe8cfa81
