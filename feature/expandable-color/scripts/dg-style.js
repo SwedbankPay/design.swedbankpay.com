bbc7cee596e16981cd3ff132e3009204ec365c9f
payex-dev <dev@payex.com>
2019-09-10 12:21:15 +0000

    GitHub release 2.0.2-expandable-color.1+553

---
+++ feature/expandable-color/scripts/dg-style.js
@@ -0,0 +1,2 @@
+(window.webpackJsonpswedbankpay=window.webpackJsonpswedbankpay||[]).push([[8],{351:function(n,w,p){}}]);
+//# sourceMappingURL=dg-style.js.map?ff3adf6f30e12ae83f1c
---
+++ feature/expandable-color/scripts/documentation.js
@@ -0,0 +1,2 @@
+(window.webpackJsonpswedbankpay=window.webpackJsonpswedbankpay||[]).push([[65],{352:function(n,w,p){}}]);
+//# sourceMappingURL=documentation.js.map?ff3adf6f30e12ae83f1c
