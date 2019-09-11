2f77f3dbc26125358ee23443d09034a0c245e452
payex-dev <dev@payex.com>
2019-09-11 08:18:10 +0000

    GitHub release 3.6.0-configuration-rework.1+71

---
+++ feature/configuration-rework/scripts/69.js
@@ -0,0 +1,2 @@
+(window.webpackJsonpswedbankpay=window.webpackJsonpswedbankpay||[]).push([[69],{357:function(n,w,p){}}]);
+//# sourceMappingURL=69.js.map?6c2f488d7f4e27661d8b
---
+++ feature/configuration-rework/scripts/dg-style.js
@@ -0,0 +1,2 @@
+(window.webpackJsonpswedbankpay=window.webpackJsonpswedbankpay||[]).push([[4],{353:function(n,w,p){}}]);
+//# sourceMappingURL=dg-style.js.map?6c2f488d7f4e27661d8b
