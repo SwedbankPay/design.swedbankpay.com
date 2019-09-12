20ca5e7a1276f76a181654f38a4f0505d024ba82
payex-dev <dev@payex.com>
2019-09-12 12:43:01 +0000

    GitHub release 4.0.0

---
+++ v/4.0.0/scripts/dg-style.js
@@ -0,0 +1,2 @@
+(window.webpackJsonpswedbankpay=window.webpackJsonpswedbankpay||[]).push([[8],{366:function(n,w,p){}}]);
+//# sourceMappingURL=dg-style.js.map?f9d9bc7efbf9123182b8
---
+++ v/4.0.0/scripts/documentation.js
@@ -0,0 +1,2 @@
+(window.webpackJsonpswedbankpay=window.webpackJsonpswedbankpay||[]).push([[65],{367:function(n,w,p){}}]);
+//# sourceMappingURL=documentation.js.map?f9d9bc7efbf9123182b8
