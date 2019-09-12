c7de0ab7ac6fcab01c1b3d3874fe0f24ac5a800d
payex-dev <dev@payex.com>
2019-09-12 12:19:53 +0000

    GitHub release 3.6.0-update-sentry-token.1+81

---
+++ feature/update-sentry-token/scripts/dg-style.js
@@ -0,0 +1,2 @@
+(window.webpackJsonpswedbankpay=window.webpackJsonpswedbankpay||[]).push([[8],{351:function(n,w,p){}}]);
+//# sourceMappingURL=dg-style.js.map?af50a665eb13231b66be
---
+++ feature/update-sentry-token/scripts/documentation.js
@@ -0,0 +1,2 @@
+(window.webpackJsonpswedbankpay=window.webpackJsonpswedbankpay||[]).push([[65],{352:function(n,w,p){}}]);
+//# sourceMappingURL=documentation.js.map?af50a665eb13231b66be
