17e4cf134cc8d9e8fe494f45ec8e74f7e42c2b03
payex-dev <dev@payex.com>
2019-09-12 07:08:41 +0000

    GitHub release 3.6.0-topbar-logo.1+74

---
+++ feature/topbar-logo/scripts/dg-style.js
@@ -0,0 +1,2 @@
+(window.webpackJsonpswedbankpay=window.webpackJsonpswedbankpay||[]).push([[8],{351:function(n,w,p){}}]);
+//# sourceMappingURL=dg-style.js.map?87405e068b6e89f7fb1d
---
+++ feature/topbar-logo/scripts/documentation.js
@@ -0,0 +1,2 @@
+(window.webpackJsonpswedbankpay=window.webpackJsonpswedbankpay||[]).push([[65],{352:function(n,w,p){}}]);
+//# sourceMappingURL=documentation.js.map?87405e068b6e89f7fb1d
