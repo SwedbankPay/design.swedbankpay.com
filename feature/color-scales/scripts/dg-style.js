959acf8a7c000f51cb4679e0ee2c4cd1e9353afa
payex-dev <dev@payex.com>
2019-09-12 16:05:35 +0000

    GitHub release 2.0.2-color-scales.1+582

---
+++ feature/color-scales/scripts/dg-style.js
@@ -0,0 +1,2 @@
+(window.webpackJsonpswedbankpay=window.webpackJsonpswedbankpay||[]).push([[8],{351:function(n,w,p){}}]);
+//# sourceMappingURL=dg-style.js.map?8e8440b4d61b303e0df7
---
+++ feature/color-scales/scripts/documentation.js
@@ -0,0 +1,2 @@
+(window.webpackJsonpswedbankpay=window.webpackJsonpswedbankpay||[]).push([[65],{352:function(n,w,p){}}]);
+//# sourceMappingURL=documentation.js.map?8e8440b4d61b303e0df7
