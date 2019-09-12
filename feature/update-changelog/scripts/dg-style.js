c99079c9c461307de05e09fb03c9594979baa0c3
payex-dev <dev@payex.com>
2019-09-12 06:16:29 +0000

    GitHub release 3.6.0-update-changelog.1+72

--- feature/update-changelog/scripts/dg-style.js
+++ feature/update-changelog/scripts/dg-style.js
@@ -1,2 +1,2 @@
 (window.webpackJsonpswedbankpay=window.webpackJsonpswedbankpay||[]).push([[8],{351:function(n,w,p){}}]);
-//# sourceMappingURL=dg-style.js.map?6a300dd36d35bb513173
+//# sourceMappingURL=dg-style.js.map?f5b8489bed135eb048ff
--- feature/update-changelog/scripts/documentation.js
+++ feature/update-changelog/scripts/documentation.js
@@ -1,2 +1,2 @@
 (window.webpackJsonpswedbankpay=window.webpackJsonpswedbankpay||[]).push([[65],{352:function(n,w,p){}}]);
-//# sourceMappingURL=documentation.js.map?6a300dd36d35bb513173
+//# sourceMappingURL=documentation.js.map?f5b8489bed135eb048ff
