c1d40b8d4c01766d708336c5dadde092f940e178
payex-dev <dev@payex.com>
2019-09-13 10:36:01 +0000

    GitHub release 2.0.2-typography-font.1+589

--- feature/typography-font/scripts/doc-route.chunk_58.js
+++ feature/typography-font/scripts/doc-route.chunk_58.js
@@ -1,2 +1,2 @@
 (window.webpackJsonpswedbankpay=window.webpackJsonpswedbankpay||[]).push([[60],{363:function(a,e,n){"use strict";n.r(e);var s=n(1),t=n.n(s),c=n(65),p=function(a){var e=a.type,n=a.classNames;return t.a.createElement("i",{className:"material-icons".concat(n?" ".concat(n):"")},e)};p.propTypes={type:n.n(c).a.string.isRequired},e.default=p}}]);
-//# sourceMappingURL=doc-route.chunk_58.js.map?d9583a3ae14426e7a59b
+//# sourceMappingURL=doc-route.chunk_58.js.map?578f06f3c4990e9e5994
--- feature/typography-font/scripts/documentation.js
+++ feature/typography-font/scripts/documentation.js
@@ -1,2 +1,2 @@
 (window.webpackJsonpswedbankpay=window.webpackJsonpswedbankpay||[]).push([[65],{352:function(n,w,p){}}]);
-//# sourceMappingURL=documentation.js.map?d9583a3ae14426e7a59b
+//# sourceMappingURL=documentation.js.map?578f06f3c4990e9e5994
