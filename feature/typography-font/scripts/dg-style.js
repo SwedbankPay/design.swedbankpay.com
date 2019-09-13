04bba3825a02aeef27bd7418d26ca8f891fb51d9
payex-dev <dev@payex.com>
2019-09-13 11:59:49 +0000

    GitHub release 4.1.0-typography-font.1+17

--- feature/typography-font/scripts/dg-style.js
+++ feature/typography-font/scripts/dg-style.js
@@ -1,2 +1,2 @@
 (window.webpackJsonpswedbankpay=window.webpackJsonpswedbankpay||[]).push([[8],{351:function(n,w,p){}}]);
-//# sourceMappingURL=dg-style.js.map?578f06f3c4990e9e5994
+//# sourceMappingURL=dg-style.js.map?a6765be41d67125b7029
--- feature/typography-font/scripts/documentation.js
+++ feature/typography-font/scripts/documentation.js
@@ -1,2 +1,2 @@
 (window.webpackJsonpswedbankpay=window.webpackJsonpswedbankpay||[]).push([[65],{352:function(n,w,p){}}]);
-//# sourceMappingURL=documentation.js.map?578f06f3c4990e9e5994
+//# sourceMappingURL=documentation.js.map?a6765be41d67125b7029
