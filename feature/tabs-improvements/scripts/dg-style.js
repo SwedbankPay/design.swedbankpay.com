223ce58c206563252c0b9342c27c206f9a72ebfd
payex-dev <dev@payex.com>
2019-09-16 08:13:39 +0000

    GitHub release 2.0.2-tabs-improvements.1+595

---
+++ feature/tabs-improvements/scripts/dg-style.js
@@ -0,0 +1,2 @@
+(window.webpackJsonpswedbankpay=window.webpackJsonpswedbankpay||[]).push([[8],{351:function(n,w,p){}}]);
+//# sourceMappingURL=dg-style.js.map?339d62d6f4180beaa5dc
---
+++ feature/tabs-improvements/scripts/documentation.js
@@ -0,0 +1,2 @@
+(window.webpackJsonpswedbankpay=window.webpackJsonpswedbankpay||[]).push([[65],{352:function(n,w,p){}}]);
+//# sourceMappingURL=documentation.js.map?339d62d6f4180beaa5dc
