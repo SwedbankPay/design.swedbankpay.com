5ccd113c8fe82af02a640dc2bc307a4a88a64af6
payex-dev <dev@payex.com>
2019-09-16 13:51:28 +0000

    GitHub release 4.1.0-payex-themify.1+35

--- feature/payex-themify/scripts/404.chunk.js
+++ feature/payex-themify/scripts/404.chunk.js
@@ -1,2 +1,2 @@
 (window.webpackJsonpswedbankpay=window.webpackJsonpswedbankpay||[]).push([[4],{451:function(e,n,a){"use strict";a.r(n);var t=a(1),o=a.n(t);n.default=function(){return o.a.createElement(o.a.Fragment,null,o.a.createElement("h1",null,"This is not the page you are looking for."))}}}]);
-//# sourceMappingURL=404.chunk.js.map?3eebad4a87bf42a7b4ec
+//# sourceMappingURL=404.chunk.js.map?a1385569cc5defde842d
--- feature/payex-themify/scripts/dg-style.js
+++ feature/payex-themify/scripts/dg-style.js
@@ -1,2 +1,2 @@
 (window.webpackJsonpswedbankpay=window.webpackJsonpswedbankpay||[]).push([[8],{351:function(n,w,p){},352:function(n,w,p){}}]);
-//# sourceMappingURL=dg-style.js.map?3eebad4a87bf42a7b4ec
+//# sourceMappingURL=dg-style.js.map?a1385569cc5defde842d
