85d88696f809d34437c6b44b59259bb8f1816b57
Thomas Lorntzen <thomas.lorntzen@payex.com>
2019-09-20 11:14:19 +0200

    Keep the real sentry token while testing deploy

--- build/prepare-build.ps1
+++ build/prepare-build.ps1
@@ -4,8 +4,8 @@
     $Env:basename = "v/$Env:GitVersion_SemVer"
 
     # Replace placeholder with api-token in .sentryclirc
-    # (Get-Content C:\projects\design-swedbankpay-com\.sentryclirc).replace("<token>", $Env:sentry_token) |
-    # Set-Content C:\projects\design-swedbankpay-com\.sentryclirc
+    (Get-Content C:\projects\design-swedbankpay-com\.sentryclirc).replace("<token>", $Env:sentry_token) |
+    Set-Content C:\projects\design-swedbankpay-com\.sentryclirc
 
 } else {
     $Env:release = "false"
