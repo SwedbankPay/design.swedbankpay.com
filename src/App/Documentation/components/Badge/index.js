4da20be5d39734502c06fcd452bd90eb04e7165d
Phi-Thien Hoang <Phi-Thien.Hoang@payex.com>
2019-09-24 15:03:23 +0200

    Add badge number class

--- src/App/Documentation/components/Badge/index.js
+++ src/App/Documentation/components/Badge/index.js
@@ -15,16 +15,30 @@ const Overview = () => (
     </>
 );
 
+const BadgeNumber = () => (
+    <>
+        <h2 id="badgenumber">Badges with numbers</h2>
+        <p>Badges are often used with numbers. Use <Property value=".badge-number" /> when a badge suited for usage with numbers is desired.</p>
+        <ComponentPreview language="html" showCasePanel codeFigure>
+            <p>Badge <span className="badge badge-default badge-number">1</span></p>
+            <p>Badge <span className="badge badge-yellow badge-number">8</span></p>
+            <p>Badge <span className="badge badge-yellow badge-number">42</span></p>
+            <p>Badge <span className="badge badge-blue badge-number">100</span></p>
+        </ComponentPreview>
+    </>
+);
+
 const Badge = () => (
     <DocContainer docToc>
         <p className="lead">
             Badges are small rectangular boxes which can convey a message and or state with their text and color.
         </p>
         <Overview />
+        <BadgeNumber />
     </DocContainer>
 );
 
 export default Badge;
 
 /* For testing */
-export { Overview };
+export { Overview, BadgeNumber };
