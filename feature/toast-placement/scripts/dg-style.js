97f5bb3d20f9ea684bfd0590771bf6161f532409
payex-dev <dev@payex.com>
2019-09-24 13:19:11 +0000

    GitHub release 4.1.0-toast-placement.1+53

--- feature/toast-placement/scripts/dg-style.js
+++ feature/toast-placement/scripts/dg-style.js
@@ -1,2 +1,2 @@
 (window.webpackJsonpswedbankpay=window.webpackJsonpswedbankpay||[]).push([[8],{355:function(n,w,p){},356:function(n,w,p){}}]);
-//# sourceMappingURL=dg-style.js.map?9f90bb714b9773b58b21
+//# sourceMappingURL=dg-style.js.map?7dcdd40e0fbcde7e1d6d
