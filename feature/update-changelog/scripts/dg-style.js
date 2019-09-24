fe1a7dbdb7b2eab4f70816f6bd92cb5e86ca1392
payex-dev <dev@payex.com>
2019-09-24 11:52:06 +0000

    GitHub release 4.1.0-update-changelog.1+59

--- feature/update-changelog/scripts/dg-style.js
+++ feature/update-changelog/scripts/dg-style.js
@@ -1,2 +1,2 @@
-(window.webpackJsonpswedbankpay=window.webpackJsonpswedbankpay||[]).push([[8],{351:function(n,w,p){}}]);
-//# sourceMappingURL=dg-style.js.map?f5b8489bed135eb048ff
+(window.webpackJsonpswedbankpay=window.webpackJsonpswedbankpay||[]).push([[8],{351:function(n,w,p){},352:function(n,w,p){}}]);
+//# sourceMappingURL=dg-style.js.map?a54bf026bb610fd7d005
--- feature/update-changelog/scripts/doc-route.chunk_55.js
+++ feature/update-changelog/scripts/doc-route.chunk_55.js
@@ -1,2 +1,2 @@
 (window.webpackJsonpswedbankpay=window.webpackJsonpswedbankpay||[]).push([[58],{361:function(n,t,e){"use strict";e.r(t);var a=e(1),i=e.n(a),o=e(66);t.default=Object(o.k)(function(n){var t=n.location.pathname.split("/").filter(function(n){return n.length}).pop().split("-").map(function(n,t){return t>0?n:n.charAt(0).toUpperCase()+n.substr(1)}).join(" ");return document.title="".concat(t," | Swedbank Pay DesignGuide"),i.a.createElement("h1",null,t)})}}]);
-//# sourceMappingURL=doc-route.chunk_55.js.map?f5b8489bed135eb048ff
+//# sourceMappingURL=doc-route.chunk_55.js.map?a54bf026bb610fd7d005
