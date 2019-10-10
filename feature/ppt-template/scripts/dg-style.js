a29d6850b0a45b2867c58bba206134d6fe5aab5d
payex-dev <dev@payex.com>
2019-10-10 13:24:08 +0000

    GitHub release 4.2.0-ppt-template.1+13

---
+++ feature/ppt-template/scripts/dg-style.js
@@ -0,0 +1,2 @@
+(window.webpackJsonpswedbankpay=window.webpackJsonpswedbankpay||[]).push([[10],{356:function(n,w,p){},357:function(n,w,p){}}]);
+//# sourceMappingURL=dg-style.js.map?8de0282de287a52a75b8
