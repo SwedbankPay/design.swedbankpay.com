f17ad227b111da40013a587597df0296a7b58f4f
payex-dev <dev@payex.com>
2019-10-16 06:52:56 +0000

    GitHub release 4.2.0-badge-improvements.1+43

--- feature/badge-improvements/scripts/dg-style.js
+++ feature/badge-improvements/scripts/dg-style.js
@@ -1,2 +1,2 @@
-(window.webpackJsonpswedbankpay=window.webpackJsonpswedbankpay||[]).push([[8],{351:function(n,w,p){},352:function(n,w,p){}}]);
-//# sourceMappingURL=dg-style.js.map?3df7b18452ec27d14977
+(window.webpackJsonpswedbankpay=window.webpackJsonpswedbankpay||[]).push([[10],{356:function(n,w,p){},357:function(n,w,p){}}]);
+//# sourceMappingURL=dg-style.js.map?717d54fa566d00463087
