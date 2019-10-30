766c398b97378604d3180ebca4f1820152b4d5c3
payex-dev <dev@payex.com>
2019-10-30 12:42:45 +0000

    GitHub release 4.3.0-chart-colors.1+24

--- feature/chart-colors/scripts/dg-style.js
+++ feature/chart-colors/scripts/dg-style.js
@@ -1,2 +1,2 @@
 (window.webpackJsonpswedbankpay=window.webpackJsonpswedbankpay||[]).push([[10],{356:function(n,w,p){},357:function(n,w,p){}}]);
-//# sourceMappingURL=dg-style.js.map?ac1645caa3c4564cc460
+//# sourceMappingURL=dg-style.js.map?dcae8d29795096b65f51
