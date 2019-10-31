747200b4f4303d78ac3b8dd8e18ee26f243ae4b2
Phi-Thien Hoang <Phi-Thien.Hoang@payex.com>
2019-10-31 15:59:58 +0100

    Add examples to generate-route-copy-array.js

--- tools/generate-routes-copy-array.js
+++ tools/generate-routes-copy-array.js
@@ -1,22 +1,18 @@
-const ROOTROUTES = ["/docs", "/examples", "/templates", "/404"];
+const ROOTROUTES = ["/docs", "/examples", "/tmpl", "/res", "/404"];
 const docRoutes = require("../src/App/routes/docs");
 const tmplRoutes = require("../src/App/routes/templates");
 const resRoutes = require("../src/App/routes/resources");
+const examplesRoutes = require("../src/App/routes/examples");
 
 const getRoutes = () => {
     const ROUTES = [...ROOTROUTES];
 
-    docRoutes.forEach(route => {
-        ROUTES.push(route.path);
-        route.routes.forEach(r => ROUTES.push(r.path));
-    });
-
-    tmplRoutes.forEach(route => {
-        ROUTES.push(route.path);
-        route.routes.forEach(r => ROUTES.push(r.path));
-    });
-
-    resRoutes.forEach(route => {
+    [
+        ...docRoutes,
+        ...tmplRoutes,
+        ...resRoutes,
+        ...examplesRoutes
+    ].forEach(route => {
         ROUTES.push(route.path);
         route.routes.forEach(r => ROUTES.push(r.path));
     });
