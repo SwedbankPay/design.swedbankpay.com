0b79ebaf4b5bfb1cb9b0b34b74972f6a034f0525
payex-dev <dev@payex.com>
2019-11-01 14:09:53 +0000

    GitHub release 4.3.0-core-color-text.1+25

--- feature/core-color-text/scripts/dg-style.js
+++ feature/core-color-text/scripts/dg-style.js
@@ -1,2 +1,2 @@
 (window.webpackJsonpswedbankpay=window.webpackJsonpswedbankpay||[]).push([[10],{356:function(n,w,p){},357:function(n,w,p){}}]);
-//# sourceMappingURL=dg-style.js.map?81abd21af05e2b6077d4
+//# sourceMappingURL=dg-style.js.map?78f179dd0527d05fc9de
