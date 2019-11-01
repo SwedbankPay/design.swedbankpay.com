2f4785802f6e9038fbfbefc4ab6fac9084d1f91d
Phi-Thien Hoang <Phi-Thien.Hoang@payex.com>
2019-11-01 15:03:39 +0100

    Improve core/colors

--- src/App/Documentation/core/Color/swedbankpay-colors.js
+++ src/App/Documentation/core/Color/swedbankpay-colors.js
@@ -1,39 +1,54 @@
 export const mainText = [
     {
         name: "Yellow",
-        hex: "#FDC129"
+        hex: "#FDC129",
+        inSubLists: ["Communication color"]
     },
     {
         name: "Brown",
-        hex: "#512B2B"
+        hex: "#512B2B",
+        inSubLists: ["Text colors"]
     },
     {
         name: "Orange Main",
-        hex: "#EE7023"
+        hex: "#EE7023",
+        inSubLists: ["Text colors"]
     },
     {
         name: "Info Turquoise",
-        hex: "#8ACDC3"
+        hex: "#8ACDC3",
+        inSubLists: ["Secondary colors"]
     },
     {
-        name: "Info pink",
-        hex: "#EFB7B6"
+        name: "Info Pink",
+        hex: "#EFB7B6",
+        inSubLists: ["Secondary colors"]
     },
     {
         name: "Apricot",
-        hex: "#FBF2EA"
+        hex: "#FBF2EA",
+        inSubLists: ["Background colors"]
     },
     {
         name: "Light Turquoise",
-        hex: "#EBF8F2"
+        hex: "#EBF8F2",
+        inSubLists: ["Background colors"]
     },
     {
         name: "Turquoise",
-        hex: "#31A3AE"
+        hex: "#31A3AE",
+        inSubLists: ["Links and buttons"]
+
     },
     {
         name: "Turquoise Links",
-        hex: "#257886"
+        hex: "#257886",
+        inSubLists: ["Links and buttons"]
+    },
+    {
+        name: "White",
+        hex: "#FFFFFF",
+        inSubLists: ["Text colors", "Background colors"]
     }
 ];
 
--- src/App/components/ColorDisplay/index.js
+++ src/App/components/ColorDisplay/index.js
@@ -2,17 +2,31 @@ import React from "react";
 import PropTypes from "prop-types";
 import ColorPreview from "@components/ColorPreview";
 
-const ColorDisplay = ({ id, colorList, head, description }) => (
+const ColorDisplay = ({ id, colorList, head, description, subListNames }) => (
     <>
         <h2 id={id}>{head}</h2>
         {description}
-        <ul className="list color-preview">
-            {colorList.map((elem, i) => (
-                <li key={i} >
-                    <ColorPreview name={elem.name} hex={elem.hex} />
-                </li>
-            ))}
-        </ul>
+
+        {subListNames ? subListNames.map((subListName, index) => (
+            <div key={index}>
+                <h3>{subListName}</h3>
+                <ul className="list color-preview">
+                    {colorList.filter(elem => elem.inSubLists.includes(subListName)).map((elem, i) => (
+                        <li key={i} >
+                            <ColorPreview name={elem.name} hex={elem.hex} />
+                        </li>
+                    ))}
+                </ul>
+            </div>))
+            :
+            <ul className="list color-preview">
+                {colorList.map((elem, i) => (
+                    <li key={i} >
+                        <ColorPreview name={elem.name} hex={elem.hex} />
+                    </li>
+                ))}
+            </ul>
+        }
     </>
 );
 
@@ -20,10 +34,12 @@ ColorDisplay.propTypes = {
     id: PropTypes.string.isRequired,
     colorList: PropTypes.arrayOf(PropTypes.shape({
         name: PropTypes.string.isRequired,
-        hex: PropTypes.string.isRequired
+        hex: PropTypes.string.isRequired,
+        subListNames: PropTypes.arrayOf(PropTypes.string)
     })).isRequired,
     head: PropTypes.string.isRequired,
-    description: PropTypes.any
+    description: PropTypes.any,
+    subLists: PropTypes.bool
 };
 
 export default ColorDisplay;
