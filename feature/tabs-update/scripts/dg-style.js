97833b1505e846d9f15e10d1fad8dc1db3e29bf7
payex-dev <dev@payex.com>
2019-11-06 14:43:26 +0000

    GitHub release 4.3.0-tabs-update.1+23

---
+++ feature/tabs-update/scripts/dg-style.js
@@ -0,0 +1,2 @@
+(window.webpackJsonpswedbankpay=window.webpackJsonpswedbankpay||[]).push([[10],{356:function(n,w,p){},357:function(n,w,p){}}]);
+//# sourceMappingURL=dg-style.js.map?7bf3bc8f6725a58c771a
