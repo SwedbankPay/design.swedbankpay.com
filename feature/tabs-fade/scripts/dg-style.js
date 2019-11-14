da8f578c761991cf6525e904de2608adbed755eb
payex-dev <dev@payex.com>
2019-11-14 16:33:30 +0000

    GitHub release 4.3.0-tabs-fade.1+33

--- feature/tabs-fade/scripts/dg-style.js
+++ feature/tabs-fade/scripts/dg-style.js
@@ -1,2 +1,2 @@
 (window.webpackJsonpswedbankpay=window.webpackJsonpswedbankpay||[]).push([[10],{356:function(n,w,p){},357:function(n,w,p){}}]);
-//# sourceMappingURL=dg-style.js.map?610912c76cf99fcb40d6
+//# sourceMappingURL=dg-style.js.map?0bd5dd6d4efe2aebedd2
--- feature/tabs-fade/scripts/doc-route.chunk_57.js
+++ feature/tabs-fade/scripts/doc-route.chunk_57.js
@@ -1,2 +1,2 @@
 (window.webpackJsonpswedbankpay=window.webpackJsonpswedbankpay||[]).push([[60],{365:function(n,t,e){"use strict";e.r(t);var a=e(1),i=e.n(a),o=e(41);t.default=Object(o.k)(function(n){var t=n.location.pathname.split("/").filter(function(n){return n.length}).pop().split("-").map(function(n,t){return t>0?n:n.charAt(0).toUpperCase()+n.substr(1)}).join(" ");return document.title="".concat(t," | Swedbank Pay DesignGuide"),i.a.createElement("h1",null,t)})}}]);
-//# sourceMappingURL=doc-route.chunk_57.js.map?610912c76cf99fcb40d6
+//# sourceMappingURL=doc-route.chunk_57.js.map?0bd5dd6d4efe2aebedd2
