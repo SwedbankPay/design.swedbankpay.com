af5b6539ec818a8347868cdb791f80d5ac5aa5db
payex-dev <dev@payex.com>
2019-11-18 11:28:17 +0000

    GitHub release 4.3.0-core-color-text.1+27

--- feature/core-color-text/scripts/dg-style.js
+++ feature/core-color-text/scripts/dg-style.js
@@ -1,2 +1,2 @@
 (window.webpackJsonpswedbankpay=window.webpackJsonpswedbankpay||[]).push([[10],{356:function(n,w,p){},357:function(n,w,p){}}]);
-//# sourceMappingURL=dg-style.js.map?88df57662070feee7dd7
+//# sourceMappingURL=dg-style.js.map?d2a296392bf39d20cd78
--- feature/core-color-text/scripts/doc-route.chunk_57.js
+++ feature/core-color-text/scripts/doc-route.chunk_57.js
@@ -1,2 +1,2 @@
 (window.webpackJsonpswedbankpay=window.webpackJsonpswedbankpay||[]).push([[60],{365:function(n,t,e){"use strict";e.r(t);var a=e(1),i=e.n(a),o=e(41);t.default=Object(o.k)(function(n){var t=n.location.pathname.split("/").filter(function(n){return n.length}).pop().split("-").map(function(n,t){return t>0?n:n.charAt(0).toUpperCase()+n.substr(1)}).join(" ");return document.title="".concat(t," | Swedbank Pay DesignGuide"),i.a.createElement("h1",null,t)})}}]);
-//# sourceMappingURL=doc-route.chunk_57.js.map?88df57662070feee7dd7
+//# sourceMappingURL=doc-route.chunk_57.js.map?d2a296392bf39d20cd78
