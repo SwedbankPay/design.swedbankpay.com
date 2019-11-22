7a5e58c899b22cb5473d8a5d7613602f0a673ab8
payex-dev <dev@payex.com>
2019-11-22 08:49:35 +0000

    GitHub release 2.0.2-badge-improvements.1+723

--- feature/badge-improvements/scripts/dg-style.js
+++ feature/badge-improvements/scripts/dg-style.js
@@ -1,2 +1,2 @@
 (window.webpackJsonpswedbankpay=window.webpackJsonpswedbankpay||[]).push([[10],{356:function(n,w,p){},357:function(n,w,p){}}]);
-//# sourceMappingURL=dg-style.js.map?815570c7dd206a72e9e2
+//# sourceMappingURL=dg-style.js.map?a7f2a444333a64febc67
--- feature/badge-improvements/scripts/doc-route.chunk_57.js
+++ feature/badge-improvements/scripts/doc-route.chunk_57.js
@@ -1,2 +1,2 @@
 (window.webpackJsonpswedbankpay=window.webpackJsonpswedbankpay||[]).push([[60],{365:function(n,t,e){"use strict";e.r(t);var a=e(1),i=e.n(a),o=e(41);t.default=Object(o.k)(function(n){var t=n.location.pathname.split("/").filter(function(n){return n.length}).pop().split("-").map(function(n,t){return t>0?n:n.charAt(0).toUpperCase()+n.substr(1)}).join(" ");return document.title="".concat(t," | Swedbank Pay DesignGuide"),i.a.createElement("h1",null,t)})}}]);
-//# sourceMappingURL=doc-route.chunk_57.js.map?815570c7dd206a72e9e2
+//# sourceMappingURL=doc-route.chunk_57.js.map?a7f2a444333a64febc67
