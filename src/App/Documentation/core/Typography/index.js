df108df4d1d486972a272b8dc1c0035d8d9cc266
Phi-Thien Hoang <Phi-Thien.Hoang@payex.com>
2019-12-02 13:18:03 +0100

    h4-h6 defined

--- src/App/Documentation/core/Typography/index.js
+++ src/App/Documentation/core/Typography/index.js
@@ -19,12 +19,18 @@ const Headings = () => (
             <h1>Heading h1</h1>
             <h2>Heading h2</h2>
             <h3>Heading h3</h3>
+            <h4>Heading h4</h4>
+            <h5>Heading h5</h5>
+            <h6>Heading h6</h6>
         </ComponentPreview>
         <p>The classes <Property value=".h1" /> through <Property value=".h6" /> are also available, for when you want to match the font styling of a heading but cannot use the associated HTML element.</p>
         <ComponentPreview language="html" showCasePanel codeFigure>
             <p className="h1">.h1 heading</p>
             <p className="h2">.h2 heading</p>
             <p className="h3">.h3 heading</p>
+            <p className="h4">.h4 heading</p>
+            <p className="h5">.h5 heading</p>
+            <p className="h6">.h6 heading</p>
         </ComponentPreview>
     </>
 );
