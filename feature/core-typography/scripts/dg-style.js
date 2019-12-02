6bc6af3997fc54381b48e242215cc4c58fa06d25
payex-dev <dev@payex.com>
2019-12-02 12:24:54 +0000

    GitHub release 4.3.0-core-typography.1+76

---
+++ feature/core-typography/scripts/dg-style.js
@@ -0,0 +1,2 @@
+(window.webpackJsonpswedbankpay=window.webpackJsonpswedbankpay||[]).push([[10],{356:function(n,w,p){},357:function(n,w,p){}}]);
+//# sourceMappingURL=dg-style.js.map?500edc9db17aee262fc1
