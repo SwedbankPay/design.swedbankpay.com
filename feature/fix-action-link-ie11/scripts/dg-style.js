579d9fcad409cd2196a86cd57db0654c98d4eb40
payex-dev <dev@payex.com>
2020-01-13 14:27:40 +0000

    GitHub release 4.3.0-fix-action-link-ie11.1+120

---
+++ feature/fix-action-link-ie11/scripts/dg-style.js
@@ -0,0 +1,2 @@
+(window.webpackJsonpswedbankpay=window.webpackJsonpswedbankpay||[]).push([[10],{356:function(n,w,p){},357:function(n,w,p){}}]);
+//# sourceMappingURL=dg-style.js.map?6a2954e2790bbdbafbd8
---
+++ feature/fix-action-link-ie11/scripts/doc-route.chunk_58.js
@@ -0,0 +1,2 @@
+(window.webpackJsonpswedbankpay=window.webpackJsonpswedbankpay||[]).push([[61],{365:function(n,t,e){"use strict";e.r(t);var a=e(1),i=e.n(a),o=e(41);t.default=Object(o.k)(function(n){var t=n.location.pathname.split("/").filter(function(n){return n.length}).pop().split("-").map(function(n,t){return t>0?n:n.charAt(0).toUpperCase()+n.substr(1)}).join(" ");return document.title="".concat(t," | Swedbank Pay DesignGuide"),i.a.createElement("h1",null,t)})}}]);
+//# sourceMappingURL=doc-route.chunk_58.js.map?6a2954e2790bbdbafbd8
