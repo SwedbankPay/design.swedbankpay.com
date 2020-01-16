204140df7fea4449e0d21aac64bd9bc1c5677639
payex-dev <dev@payex.com>
2020-01-16 15:28:13 +0000

    GitHub release 4.4.0-sidebar.1+4

---
+++ feature/sidebar/scripts/dg-style.js
@@ -0,0 +1,2 @@
+(window.webpackJsonpswedbankpay=window.webpackJsonpswedbankpay||[]).push([[10],{356:function(n,w,p){},357:function(n,w,p){}}]);
+//# sourceMappingURL=dg-style.js.map?e9623b0aaab0edcd4242
