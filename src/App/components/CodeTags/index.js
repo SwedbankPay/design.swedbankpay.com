7a000b9ac3499144f12078a5b2d7175998624a00
Phi Thien Hoang <phth@netlight.com>
2020-01-21 14:25:31 +0100

    Change code tags to use code instead of span

--- src/App/components/CodeTags/index.js
+++ src/App/components/CodeTags/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const CodeTags = ({ type, code }) => (
-    <span className={`code-tags code-tags-${type}`}>{code}</span>
+    <code className={`code-tags code-tags-${type}`}>{code}</code>
 );
 
 CodeTags.propTypes = {
