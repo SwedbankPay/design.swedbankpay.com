ac42ff0892a331e07342ec30de9eba47d2ee2e7f
Thomas Lorntzen <cyrthomas1@gmail.com>
2020-01-21 15:11:01 +0100

    Check gitversion variable

--- build/prepare-deploy.ps1
+++ build/prepare-deploy.ps1
@@ -1,3 +1,5 @@
+Write-Host $Env:GitVersion_PreReleaseLabel
+
 # Avoid deploying PR's as they do not have access to secrets
 if ($Env:GitVersion_PreReleaseLabel -NotMatch "PullRequest" -And $Env:GitVersion_PreReleaseLabel -NotMatch "dependabot") {
     # Deploy to github pages
