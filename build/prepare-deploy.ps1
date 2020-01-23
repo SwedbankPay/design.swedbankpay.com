24ced9e926dab6311cdb5e99059f16ccbae9beef
Thomas Lorntzen <cyrthomas1@gmail.com>
2020-01-23 15:00:19 +0100

    Update deploy conditional

--- build/prepare-deploy.ps1
+++ build/prepare-deploy.ps1
@@ -1,13 +1,7 @@
-Write-Host $Env:GitVersion_PreReleaseLabel
-
-if($Env:APPVEYOR_PULL_REQUEST_NUMBER) {
-    Write-Host "This is a pull request"
-} else {
-    Write-Host "This is NOT a pull request"
-}
+Write-Host "Pull request number?" $Env:APPVEYOR_PULL_REQUEST_NUMBER
 
 # Avoid deploying PR's as they do not have access to secrets
-if ($Env:GitVersion_PreReleaseLabel -NotMatch "PullRequest" -And $Env:GitVersion_PreReleaseLabel -NotMatch "dependabot") {
+if ($null -eq $Env:APPVEYOR_PULL_REQUEST_NUMBER -And $Env:GitVersion_PreReleaseLabel -NotMatch "dependabot") {
     # Deploy to github pages
     ./build/deploy-github-pages.ps1
 
