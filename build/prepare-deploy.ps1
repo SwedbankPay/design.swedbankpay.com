bb301871d99dc3c6539ba8ea0992b75caea7d665
Thomas Lorntzen <cyrthomas1@gmail.com>
2020-01-23 14:04:02 +0100

    test

--- build/prepare-deploy.ps1
+++ build/prepare-deploy.ps1
@@ -1,5 +1,11 @@
 Write-Host $Env:GitVersion_PreReleaseLabel
 
+if($Env:APPVEYOR_PULL_REQUEST_NUMBER) {
+    Write-Host "This is a pull request"
+} else {
+    Write-Host "This is NOT a pull request"
+}
+
 # Avoid deploying PR's as they do not have access to secrets
 if ($Env:GitVersion_PreReleaseLabel -NotMatch "PullRequest" -And $Env:GitVersion_PreReleaseLabel -NotMatch "dependabot") {
     # Deploy to github pages
