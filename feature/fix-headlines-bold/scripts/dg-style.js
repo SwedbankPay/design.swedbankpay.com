b0816f17b97a473806de17d087cf908c4ef0c5b6
payex-dev <dev@payex.com>
2020-01-23 13:58:23 +0000

    GitHub release 4.4.0-fix-headlines-bold.1+2

---
+++ feature/fix-headlines-bold/scripts/dg-style.js
@@ -0,0 +1,2 @@
+(window.webpackJsonpswedbankpay=window.webpackJsonpswedbankpay||[]).push([[10],{356:function(n,w,p){},357:function(n,w,p){}}]);
+//# sourceMappingURL=dg-style.js.map?d50d0c1ce1536b7e8acd
