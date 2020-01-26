b86b8261b0ae41e45d834da18a93eb355e34cfa0
payex-dev <dev@payex.com>
2020-01-26 16:11:34 +0000

    GitHub release 4.4.0-move-charts-lists-tables.1+10

---
+++ feature/move-charts-lists-tables/scripts/dg-style.js
@@ -0,0 +1,2 @@
+(window.webpackJsonpswedbankpay=window.webpackJsonpswedbankpay||[]).push([[10],{356:function(n,w,p){},357:function(n,w,p){}}]);
+//# sourceMappingURL=dg-style.js.map?443a9b0fd2e972aa46e8
