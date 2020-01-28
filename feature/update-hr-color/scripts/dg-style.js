86327ab41b6f315e02f7da7dbbea196ef5d1ddb3
payex-dev <dev@payex.com>
2020-01-28 08:44:08 +0000

    GitHub release 4.4.0-update-hr-color.1+10

---
+++ feature/update-hr-color/scripts/dg-style.js
@@ -0,0 +1,2 @@
+(window.webpackJsonpswedbankpay=window.webpackJsonpswedbankpay||[]).push([[10],{356:function(n,w,p){},357:function(n,w,p){}}]);
+//# sourceMappingURL=dg-style.js.map?cd59ec72d66c8d52a0f5
