649ac8ee14563eb26e6a8e2af5bcd427a72b1978
payex-dev <dev@payex.com>
2020-01-28 11:24:58 +0000

    GitHub release 4.4.0-update-topbar-styling.1+10

---
+++ feature/update-topbar-styling/scripts/dg-style.js
@@ -0,0 +1,2 @@
+(window.webpackJsonpswedbankpay=window.webpackJsonpswedbankpay||[]).push([[10],{356:function(n,w,p){},357:function(n,w,p){}}]);
+//# sourceMappingURL=dg-style.js.map?30b18a169eab6352e50c
