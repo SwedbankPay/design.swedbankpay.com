2c29ce6d4bc662efef8cb5a763e4899863407ca5
Thomas Lorntzen <cyrthomas1@gmail.com>
2020-02-03 11:41:27 +0100

    Add github scripts

---
+++ .github/scripts/variables.sh
@@ -0,0 +1,9 @@
+if [ "$BRAND" == "payex" ]; then
+    echo ::set-env name=BRAND_URL::https://payexdesignguide.z16.web.core.windows.net
+    echo ::set-env name=BRAND_BUILD_SCRIPT::build:prod:payex
+elif [ "$BRAND" == "swedbankpay" ]; then
+    echo ::set-env name=BRAND_URL::https://swedbankpaydesignguide.z16.web.core.windows.net
+    echo ::set-env name=BRAND_BUILD_SCRIPT::build:prod
+else
+    echo "Unknown brand!"
+fi
