e13f64a735472c778f03d9d4030f49d28bd92de6
payex-dev <dev@payex.com>
2020-02-03 10:56:39 +0000

    GitHub release 4.4.0-github-actions-and-azure.1+16

---
+++ feature/github-actions-and-azure/scripts/dg-style.js
@@ -0,0 +1,2 @@
+(window.webpackJsonpswedbankpay=window.webpackJsonpswedbankpay||[]).push([[10],{356:function(n,w,p){},357:function(n,w,p){}}]);
+//# sourceMappingURL=dg-style.js.map?724c4db8344a266f5b4f
