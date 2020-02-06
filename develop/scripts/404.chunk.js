cc23b885c26da45039c64779404841508360b4d8
payex-dev <dev@payex.com>
2020-02-06 12:42:34 +0000

    GitHub release 4.4.0-alpha.67

--- develop/scripts/404.chunk.js
+++ develop/scripts/404.chunk.js
@@ -1,2 +1,2 @@
-(window.webpackJsonpswedbankpay=window.webpackJsonpswedbankpay||[]).push([[3],{409:function(e,n,a){"use strict";a.r(n);var t=a(1),o=a.n(t);n.default=function(){return o.a.createElement(o.a.Fragment,null,o.a.createElement("h1",null,"This is not the page you are looking for."))}}}]);
-//# sourceMappingURL=404.chunk.js.map?fb4d474e6e278244fe76
+(window.webpackJsonpswedbankpay=window.webpackJsonpswedbankpay||[]).push([[5],{415:function(e,n,a){"use strict";a.r(n);var t=a(1),o=a.n(t);n.default=function(){return o.a.createElement(o.a.Fragment,null,o.a.createElement("h1",null,"This is not the page you are looking for."))}}}]);
+//# sourceMappingURL=404.chunk.js.map?34b9930e5e8bf61e66e3
--- develop/scripts/dg-style.js
+++ develop/scripts/dg-style.js
@@ -1,2 +1,2 @@
-(window.webpackJsonpswedbankpay=window.webpackJsonpswedbankpay||[]).push([[10],{356:function(n,w,p){},357:function(n,w,p){}}]);
-//# sourceMappingURL=dg-style.js.map?fb4d474e6e278244fe76
+(window.webpackJsonpswedbankpay=window.webpackJsonpswedbankpay||[]).push([[12],{356:function(n,w,p){},357:function(n,w,p){}}]);
+//# sourceMappingURL=dg-style.js.map?34b9930e5e8bf61e66e3
