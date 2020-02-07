7c5e297f948ed0bb3fba9b07891d2d7ec62e41ae
payex-dev <dev@payex.com>
2020-02-07 15:33:54 +0000

    GitHub release 4.4.0-add-logos.1+75

---
+++ feature/add-logos/scripts/dg-style.js
@@ -0,0 +1,2 @@
+(window.webpackJsonpswedbankpay=window.webpackJsonpswedbankpay||[]).push([[14],{358:function(n,w,p){},359:function(n,w,p){}}]);
+//# sourceMappingURL=dg-style.js.map?9ada823198f93c521ba3
