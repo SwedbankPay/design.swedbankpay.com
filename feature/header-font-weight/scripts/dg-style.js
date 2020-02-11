ef928c5e92b262681601e037872ae5799abb2dfd
payex-dev <dev@payex.com>
2020-02-11 10:30:01 +0000

    GitHub release 4.4.0-header-font-weight.1+101

---
+++ feature/header-font-weight/scripts/dg-style.js
@@ -0,0 +1,2 @@
+(window.webpackJsonpswedbankpay=window.webpackJsonpswedbankpay||[]).push([[15],{356:function(n,w,p){},357:function(n,w,p){}}]);
+//# sourceMappingURL=dg-style.js.map?d95ece2bc04642248b47
