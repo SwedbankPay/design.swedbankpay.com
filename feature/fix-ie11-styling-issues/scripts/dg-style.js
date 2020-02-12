ed41fc6ddb22746c2d447966c9d2fc809ac4a8ee
payex-dev <dev@payex.com>
2020-02-12 13:36:36 +0000

    GitHub release 4.5.0-fix-ie11-styling-issues.1+2

---
+++ feature/fix-ie11-styling-issues/scripts/dg-style.js
@@ -0,0 +1,2 @@
+(window.webpackJsonpswedbankpay=window.webpackJsonpswedbankpay||[]).push([[15],{356:function(n,w,p){},357:function(n,w,p){}}]);
+//# sourceMappingURL=dg-style.js.map?2496f1bfeefdbc358feb
