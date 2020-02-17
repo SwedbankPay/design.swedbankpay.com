9f1b88972b4fb2df3cc5c7fbed62f6053724ed04
payex-dev <dev@payex.com>
2020-02-17 10:31:05 +0000

    GitHub release 4.5.0-update-bordered-item-list.1+2

---
+++ feature/update-bordered-item-list/scripts/dg-style.js
@@ -0,0 +1,2 @@
+(window.webpackJsonpswedbankpay=window.webpackJsonpswedbankpay||[]).push([[15],{356:function(n,w,p){},357:function(n,w,p){}}]);
+//# sourceMappingURL=dg-style.js.map?6c2dc74b42b378524891
