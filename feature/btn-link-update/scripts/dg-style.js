cce48736e5b176255cab3bea7f9e6b0e4ae7a6ee
payex-dev <dev@payex.com>
2020-02-18 14:43:34 +0000

    GitHub release 4.5.0-btn-link-update.1+6

--- feature/btn-link-update/scripts/dg-style.js
+++ feature/btn-link-update/scripts/dg-style.js
@@ -1,2 +1,2 @@
 (window.webpackJsonpswedbankpay=window.webpackJsonpswedbankpay||[]).push([[15],{356:function(n,w,p){},357:function(n,w,p){}}]);
-//# sourceMappingURL=dg-style.js.map?416e7560e83a0a1652cb
+//# sourceMappingURL=dg-style.js.map?cd368a46e5c4ea46b90e
