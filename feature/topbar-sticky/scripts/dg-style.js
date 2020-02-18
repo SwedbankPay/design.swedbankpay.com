da70148bca91b1387fde3ffe6adc19c8fcfe279d
payex-dev <dev@payex.com>
2020-02-18 16:09:06 +0000

    GitHub release 4.5.0-topbar-sticky.1+6

--- feature/topbar-sticky/scripts/dg-style.js
+++ feature/topbar-sticky/scripts/dg-style.js
@@ -1,2 +1,2 @@
 (window.webpackJsonpswedbankpay=window.webpackJsonpswedbankpay||[]).push([[16],{356:function(n,w,p){},357:function(n,w,p){}}]);
-//# sourceMappingURL=dg-style.js.map?66557db63ac25a414ead
+//# sourceMappingURL=dg-style.js.map?809c7228e1b263b79948
--- feature/topbar-sticky/scripts/doc-route.chunk_99.js
+++ feature/topbar-sticky/scripts/doc-route.chunk_99.js
@@ -1,2 +1,2 @@
 (window.webpackJsonpswedbankpay=window.webpackJsonpswedbankpay||[]).push([[119],{382:function(e,a,n){"use strict";n.r(a);var s=n(1),t=n.n(s),c=n(17),o=n.n(c),d=function(e){var a=e.type,n=e.code;return t.a.createElement("code",{className:"code-tags code-tags-".concat(a)},n)};d.propTypes={type:o.a.string.isRequired,code:o.a.string.isRequired},a.default=d}}]);
-//# sourceMappingURL=doc-route.chunk_99.js.map?66557db63ac25a414ead
+//# sourceMappingURL=doc-route.chunk_99.js.map?809c7228e1b263b79948
