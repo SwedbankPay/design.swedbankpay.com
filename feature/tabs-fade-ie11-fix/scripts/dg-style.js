6e0c5c0c33640cf29d11eb7f4babbc0d30bdbb14
payex-dev <dev@payex.com>
2020-02-19 12:03:05 +0000

    GitHub release 4.5.0-tabs-fade-ie11-fix.1+5

---
+++ feature/tabs-fade-ie11-fix/scripts/dg-style.js
@@ -0,0 +1,2 @@
+(window.webpackJsonpswedbankpay=window.webpackJsonpswedbankpay||[]).push([[15],{356:function(n,w,p){},357:function(n,w,p){}}]);
+//# sourceMappingURL=dg-style.js.map?f71527736007ebd88e34
