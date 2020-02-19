c47ff3a5ff957fb68cc16fdacf8445e3a512d898
payex-dev <dev@payex.com>
2020-02-19 13:22:57 +0000

    GitHub release 4.5.0-variables-less.1+5

---
+++ feature/variables-less/scripts/doc-route.chunk_104.js
@@ -0,0 +1,2 @@
+(window.webpackJsonpswedbankpay=window.webpackJsonpswedbankpay||[]).push([[22],{404:function(a,e,n){"use strict";n.r(e);var s=n(1),t=n.n(s),c=n(17),o=function(a){var e=a.className,n=a.children;return t.a.createElement("footer",{className:"page-footer".concat(e?" ".concat(e):"")},n||t.a.createElement("p",{className:"page-footer-rights"},"© Swedbank Pay"))};o.propTypes={className:n.n(c).a.string},e.default=o}}]);
+//# sourceMappingURL=doc-route.chunk_104.js.map?4b8627718aca4862554a
