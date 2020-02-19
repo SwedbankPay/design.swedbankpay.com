fb702fe016ad4d014198e30f4fda0f9c72bdfc1a
payex-dev <dev@payex.com>
2020-02-19 16:09:33 +0000

    GitHub release 4.5.0-alpha.9

--- develop/scripts/dg-style.js
+++ develop/scripts/dg-style.js
@@ -1,2 +1,2 @@
 (window.webpackJsonpswedbankpay=window.webpackJsonpswedbankpay||[]).push([[15],{356:function(n,w,p){},357:function(n,w,p){}}]);
-//# sourceMappingURL=dg-style.js.map?e27769e05997961e35a6
+//# sourceMappingURL=dg-style.js.map?62e7c2e607b043e73ca5
--- develop/scripts/doc-route.chunk_104.js
+++ develop/scripts/doc-route.chunk_104.js
@@ -1,2 +1,2 @@
 (window.webpackJsonpswedbankpay=window.webpackJsonpswedbankpay||[]).push([[22],{404:function(a,e,n){"use strict";n.r(e);var s=n(1),t=n.n(s),c=n(17),o=function(a){var e=a.className,n=a.children;return t.a.createElement("footer",{className:"page-footer".concat(e?" ".concat(e):"")},n||t.a.createElement("p",{className:"page-footer-rights"},"© Swedbank Pay"))};o.propTypes={className:n.n(c).a.string},e.default=o}}]);
-//# sourceMappingURL=doc-route.chunk_104.js.map?e27769e05997961e35a6
+//# sourceMappingURL=doc-route.chunk_104.js.map?62e7c2e607b043e73ca5
