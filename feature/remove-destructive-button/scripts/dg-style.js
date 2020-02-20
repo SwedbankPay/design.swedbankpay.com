861e3d29b02bddbc151f37d5c9207ce07365f9d1
payex-dev <dev@payex.com>
2020-02-20 14:55:26 +0000

    GitHub release 4.5.0-remove-destructive-button.1+12

---
+++ feature/remove-destructive-button/scripts/dg-style.js
@@ -0,0 +1,2 @@
+(window.webpackJsonpswedbankpay=window.webpackJsonpswedbankpay||[]).push([[15],{356:function(n,w,p){},357:function(n,w,p){}}]);
+//# sourceMappingURL=dg-style.js.map?1c26743545de4eec8486
---
+++ feature/remove-destructive-button/scripts/doc-route.chunk_79.js
@@ -0,0 +1,2 @@
+(window.webpackJsonpswedbankpay=window.webpackJsonpswedbankpay||[]).push([[104],{376:function(a,e,n){"use strict";n.r(e);var s=n(1),t=n.n(s),c=n(17),p=function(a){var e=a.type,n=a.classNames;return t.a.createElement("i",{className:"material-icons".concat(n?" ".concat(n):"")},e)};p.propTypes={type:n.n(c).a.string.isRequired},e.default=p}}]);
+//# sourceMappingURL=doc-route.chunk_79.js.map?1c26743545de4eec8486
