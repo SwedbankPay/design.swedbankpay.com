a2d445d523e92c5a8d785208eb3a43e7571aa224
payex-dev <dev@payex.com>
2020-02-21 14:10:43 +0000

    GitHub release 4.5.0-topbar-sidebar-updates.1+8

---
+++ feature/topbar-sidebar-updates/scripts/dg-style.js
@@ -0,0 +1,2 @@
+(window.webpackJsonpswedbankpay=window.webpackJsonpswedbankpay||[]).push([[16],{356:function(n,w,p){},357:function(n,w,p){}}]);
+//# sourceMappingURL=dg-style.js.map?1bf6a6197d40b7503125
---
+++ feature/topbar-sidebar-updates/scripts/doc-route.chunk_99.js
@@ -0,0 +1,2 @@
+(window.webpackJsonpswedbankpay=window.webpackJsonpswedbankpay||[]).push([[119],{382:function(e,a,n){"use strict";n.r(a);var s=n(1),t=n.n(s),c=n(17),o=n.n(c),d=function(e){var a=e.type,n=e.code;return t.a.createElement("code",{className:"code-tags code-tags-".concat(a)},n)};d.propTypes={type:o.a.string.isRequired,code:o.a.string.isRequired},a.default=d}}]);
+//# sourceMappingURL=doc-route.chunk_99.js.map?1bf6a6197d40b7503125
