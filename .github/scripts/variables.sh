5084d7d6e84ff0fd3013e5dbfddec1e6c94fbeeb
Thomas Lorntzen <cyrthomas1@gmail.com>
2020-02-25 13:51:37 +0100

    Add compressed size action

--- .github/scripts/variables.sh
+++ .github/scripts/variables.sh
@@ -1,9 +1,11 @@
 if [ "$BRAND" == "payex" ]; then
     echo ::set-env name=BRAND_URL::https://payexdesignguide.z16.web.core.windows.net
     echo ::set-env name=BRAND_BUILD_SCRIPT::build:prod:payex
+    echo ::set-output name=BRAND_BUILD_SCRIPT::build:prod:payex
 elif [ "$BRAND" == "swedbankpay" ]; then
     echo ::set-env name=BRAND_URL::https://swedbankpaydesignguide.z16.web.core.windows.net
     echo ::set-env name=BRAND_BUILD_SCRIPT::build:prod
+    echo ::set-output name=BRAND_BUILD_SCRIPT::build:prod
 else
     echo "Unknown brand!"
 fi
