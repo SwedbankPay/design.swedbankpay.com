0dec1f121b73567eb0294aa6e234d61dcc5cf1ba
payex-dev <dev@payex.com>
2020-02-25 09:56:46 +0000

    GitHub release 4.5.0-fix-javascript-docs.1+35

---
+++ feature/fix-javascript-docs/scripts/dg-style.js
@@ -0,0 +1,2 @@
+(window.webpackJsonpswedbankpay=window.webpackJsonpswedbankpay||[]).push([[15],{356:function(n,w,p){},357:function(n,w,p){}}]);
+//# sourceMappingURL=dg-style.js.map?ac120392c036d090f398
