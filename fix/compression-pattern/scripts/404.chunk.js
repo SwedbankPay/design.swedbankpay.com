34b5aa873952d8ad91011bfb002236ada057ab82
payex-dev <dev@payex.com>
2020-02-27 13:43:26 +0000

    GitHub release 4.5.0-fix-compression-pattern.1+49

---
+++ fix/compression-pattern/scripts/404.chunk.js
@@ -0,0 +1,2 @@
+(window.webpackJsonpswedbankpay=window.webpackJsonpswedbankpay||[]).push([[5],{426:function(e,n,a){"use strict";a.r(n);var t=a(1),o=a.n(t);n.default=function(){return o.a.createElement(o.a.Fragment,null,o.a.createElement("h1",null,"This is not the page you are looking for."))}}}]);
+//# sourceMappingURL=404.chunk.js.map?5c6a6df6236f8bb9a80b
---
+++ fix/compression-pattern/scripts/dg-style.js
@@ -0,0 +1,2 @@
+(window.webpackJsonpswedbankpay=window.webpackJsonpswedbankpay||[]).push([[15],{356:function(n,w,p){},357:function(n,w,p){}}]);
+//# sourceMappingURL=dg-style.js.map?5c6a6df6236f8bb9a80b
