299f96510b31f0246b184ed9538f5aa241db0212
payex-dev <dev@payex.com>
2020-02-27 17:18:17 +0000

    GitHub release 4.5.0-accordion-component.1+46

--- feature/accordion-component/scripts/dg-style.js
+++ feature/accordion-component/scripts/dg-style.js
@@ -1,2 +1,2 @@
 (window.webpackJsonpswedbankpay=window.webpackJsonpswedbankpay||[]).push([[15],{356:function(n,w,p){},357:function(n,w,p){}}]);
-//# sourceMappingURL=dg-style.js.map?1300bc17563b9be0aff9
+//# sourceMappingURL=dg-style.js.map?a043b07abb78fed38b50
