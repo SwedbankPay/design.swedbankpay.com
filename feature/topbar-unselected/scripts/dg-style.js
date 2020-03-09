637f62e607964749c78fea4c4e7676cce4f8e9d7
payex-dev <dev@payex.com>
2020-03-09 12:41:22 +0000

    GitHub release 4.6.0-topbar-unselected.1+1

---
+++ feature/topbar-unselected/scripts/dg-style.js
@@ -0,0 +1,2 @@
+(window.webpackJsonpswedbankpay=window.webpackJsonpswedbankpay||[]).push([[16],{356:function(n,w,p){},357:function(n,w,p){}}]);
+//# sourceMappingURL=dg-style.js.map?dfc5ccebb9c8f7e4176e
