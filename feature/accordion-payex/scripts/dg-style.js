d31d33312fdfdaa4116927fdc1f9c548b146ccd6
payex-dev <dev@payex.com>
2020-03-11 15:29:18 +0000

    GitHub release 4.6.0-accordion-payex.1+1

---
+++ feature/accordion-payex/scripts/dg-style.js
@@ -0,0 +1,2 @@
+(window.webpackJsonpswedbankpay=window.webpackJsonpswedbankpay||[]).push([[16],{356:function(n,w,p){},357:function(n,w,p){}}]);
+//# sourceMappingURL=dg-style.js.map?68533144ec9953e49552
---
+++ feature/accordion-payex/scripts/doc-route.chunk_101.js
@@ -0,0 +1,2 @@
+(window.webpackJsonpswedbankpay=window.webpackJsonpswedbankpay||[]).push([[20],{382:function(e,a,n){"use strict";n.r(a);var s=n(1),t=n.n(s),c=n(17),o=n.n(c),d=function(e){var a=e.type,n=e.code;return t.a.createElement("code",{className:"code-tags code-tags-".concat(a)},n)};d.propTypes={type:o.a.string.isRequired,code:o.a.string.isRequired},a.default=d}}]);
+//# sourceMappingURL=doc-route.chunk_101.js.map?68533144ec9953e49552
