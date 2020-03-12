f611ac9dc965e0cdd001247de08ef44223dbae6c
payex-dev <dev@payex.com>
2020-03-12 11:29:01 +0000

    GitHub release 4.6.0-alpha.12

--- develop/scripts/dg-style.js
+++ develop/scripts/dg-style.js
@@ -1,2 +1,2 @@
 (window.webpackJsonpswedbankpay=window.webpackJsonpswedbankpay||[]).push([[17],{356:function(n,w,p){},357:function(n,w,p){}}]);
-//# sourceMappingURL=dg-style.js.map?50aee988946db4882e49
+//# sourceMappingURL=dg-style.js.map?a5dc3dddb3ba20ede0eb
--- develop/scripts/doc-route.chunk_100.js
+++ develop/scripts/doc-route.chunk_100.js
@@ -1,2 +1,2 @@
 (window.webpackJsonpswedbankpay=window.webpackJsonpswedbankpay||[]).push([[20],{382:function(e,a,n){"use strict";n.r(a);var s=n(1),t=n.n(s),c=n(17),o=n.n(c),d=function(e){var a=e.type,n=e.code;return t.a.createElement("code",{className:"code-tags code-tags-".concat(a)},n)};d.propTypes={type:o.a.string.isRequired,code:o.a.string.isRequired},a.default=d}}]);
-//# sourceMappingURL=doc-route.chunk_100.js.map?50aee988946db4882e49
+//# sourceMappingURL=doc-route.chunk_100.js.map?a5dc3dddb3ba20ede0eb
