8a0225a444efd934dd7161e5e5a6f2202d1d4e9f
payex-dev <dev@payex.com>
2020-03-12 16:30:10 +0000

    GitHub release 4.6.0-executive-button-color.1+13

---
+++ feature/executive-button-color/scripts/dg-style.js
@@ -0,0 +1,2 @@
+(window.webpackJsonpswedbankpay=window.webpackJsonpswedbankpay||[]).push([[17],{356:function(n,w,p){},357:function(n,w,p){}}]);
+//# sourceMappingURL=dg-style.js.map?cc020585ab3072c311e1
