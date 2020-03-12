ba82e531823566ddc7417ac743b21012fec93763
payex-dev <dev@payex.com>
2020-03-12 10:35:13 +0000

    GitHub release 4.6.0-revert-294-feature-update-resources-section.1+9

---
+++ revert-294-feature/update-resources-section/scripts/dg-style.js
@@ -0,0 +1,2 @@
+(window.webpackJsonpswedbankpay=window.webpackJsonpswedbankpay||[]).push([[16],{356:function(n,w,p){},357:function(n,w,p){}}]);
+//# sourceMappingURL=dg-style.js.map?6cf00e333a04a7e64d57
