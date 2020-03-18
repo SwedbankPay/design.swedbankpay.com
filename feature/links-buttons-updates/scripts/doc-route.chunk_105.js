fb5525dfbd2a227c9bddb42b3af3a0b7ad5be479
payex-dev <dev@payex.com>
2020-03-18 08:53:24 +0000

    GitHub release 4.7.0-links-buttons-updates.1+4

--- feature/links-buttons-updates/scripts/doc-route.chunk_105.js
+++ feature/links-buttons-updates/scripts/doc-route.chunk_105.js
@@ -1,2 +1,2 @@
 (window.webpackJsonp=window.webpackJsonp||[]).push([[25],{406:function(e,a,n){"use strict";n.r(a);var t=n(1),c=n.n(t),s=n(17),o=function(e){var a=e.className,n=e.children;return c.a.createElement("footer",{className:"page-footer".concat(a?" ".concat(a):"")},n||c.a.createElement("p",{className:"page-footer-rights"},"© Swedbank Pay"))};o.propTypes={className:n.n(s).a.string},a.default=o}}]);
-//# sourceMappingURL=doc-route.chunk_105.js.map?2faf564137b384a01339
+//# sourceMappingURL=doc-route.chunk_105.js.map?68f4afb867e289a0c304
