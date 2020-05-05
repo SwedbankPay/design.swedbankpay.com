6e85b95f48462b841b5b8e341bc3a293b5005e68
payex-dev <dev@payex.com>
2020-05-05 16:51:57 +0000

    GitHub release 4.7.0-mobile-sidebar-in-topbar.1+50

--- feature/mobile-sidebar-in-topbar/scripts/dg-style.js
+++ feature/mobile-sidebar-in-topbar/scripts/dg-style.js
@@ -1,2 +1,2 @@
 (window.webpackJsonp=window.webpackJsonp||[]).push([[16],{358:function(n,o,w){},359:function(n,o,w){}}]);
-//# sourceMappingURL=dg-style.js.map?9a027ad9b40baa77d138
+//# sourceMappingURL=dg-style.js.map?90fa438d092ce3207add
