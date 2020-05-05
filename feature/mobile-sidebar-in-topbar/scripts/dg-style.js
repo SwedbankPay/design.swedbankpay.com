280f90173334dbe30e115fb6af8dd6b3c6862b2c
payex-dev <dev@payex.com>
2020-05-05 16:36:13 +0000

    GitHub release 4.7.0-mobile-sidebar-in-topbar.1+49

--- feature/mobile-sidebar-in-topbar/scripts/dg-style.js
+++ feature/mobile-sidebar-in-topbar/scripts/dg-style.js
@@ -1,2 +1,2 @@
 (window.webpackJsonp=window.webpackJsonp||[]).push([[16],{358:function(n,o,w){},359:function(n,o,w){}}]);
-//# sourceMappingURL=dg-style.js.map?6a9514c6cb1b5e48ff1e
+//# sourceMappingURL=dg-style.js.map?9a027ad9b40baa77d138
--- feature/mobile-sidebar-in-topbar/scripts/doc-route.chunk_105.js
+++ feature/mobile-sidebar-in-topbar/scripts/doc-route.chunk_105.js
@@ -1,2 +1,2 @@
 (window.webpackJsonp=window.webpackJsonp||[]).push([[24],{408:function(e,a,n){"use strict";n.r(a);var t=n(1),c=n.n(t),s=n(17),o=function(e){var a=e.className,n=e.children;return c.a.createElement("footer",{className:"page-footer".concat(a?" ".concat(a):"")},n||c.a.createElement("p",{className:"page-footer-rights"},"© Swedbank Pay"))};o.propTypes={className:n.n(s).a.string},a.default=o}}]);
-//# sourceMappingURL=doc-route.chunk_105.js.map?6a9514c6cb1b5e48ff1e
+//# sourceMappingURL=doc-route.chunk_105.js.map?9a027ad9b40baa77d138
