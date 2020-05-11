0277f762938943e3227d754a607eaada5c13693c
Phi Thien Hoang <phth@netlight.com>
2020-05-11 15:05:48 +0200

    Change topbar-info styling and positioning

--- src/App/AppHeader/index.js
+++ src/App/AppHeader/index.js
@@ -33,6 +33,9 @@ const AppHeader = () => (
                 {location.pathname.includes("/res") && <SelectPanel id="doc-topbar-sidebar" topbarId="dg-topbar" routes={resRoutes} />}
                 {/* <SelectPanel id="res-topbar-sidebar" topbarId="dg-topbar" routes={resRoutes} /> */}
                 <div className="topbar-info topbar-link-right">
+                    <div className="topbar-info-version">
+                        <a href={`https://github.com/${brand}/design.${brand}.com/releases/tag/${pkg.version}`} target="_blank" rel="noopener noreferrer">v.{process.env.version || pkg.version}</a>
+                    </div>
                     <div className="topbar-info-contact">
                         <a href={`https://github.com/${brand}/design.${brand}.com`} target="_blank" rel="noopener noreferrer">
                             <GithubLogo />
@@ -40,9 +43,6 @@ const AppHeader = () => (
                         <a href="https://payex.slack.com/messages/C0L3W8B2S/" target="_blank" rel="noopener noreferrer">
                             <SlackLogo />
                         </a>
-                    </div>
-                    <div className="topbar-info-version">
-                        <a href={`https://github.com/${brand}/design.${brand}.com/releases/tag/${pkg.version}`} target="_blank" rel="noopener noreferrer">v.{process.env.version || pkg.version}</a>
                     </div>
                 </div>
             </div>
