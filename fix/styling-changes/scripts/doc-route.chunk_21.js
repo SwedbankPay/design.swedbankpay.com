ca247997fe3f8c63f81495c474278dd50e2fc84d
payex-dev <dev@payex.com>
2020-05-26 08:11:47 +0000

    GitHub release 4.7.0-fix-styling-changes.1+66

--- fix/styling-changes/scripts/doc-route.chunk_21.js
+++ fix/styling-changes/scripts/doc-route.chunk_21.js
@@ -10,4 +10,4 @@
   http://jedwatson.github.io/classnames
 */
 !function(){"use strict";var o={}.hasOwnProperty;function c(){for(var e=[],t=0;t<arguments.length;t++){var n=arguments[t];if(n){var a=r(n);if("string"===a||"number"===a)e.push(n);else if(Array.isArray(n)&&n.length){var l=c.apply(null,n);l&&e.push(l)}else if("object"===a)for(var i in n)o.call(n,i)&&n[i]&&e.push(i)}}return e.join(" ")}e.exports?(c.default=c,e.exports=c):"object"===r(n(489))&&n(489)?void 0===(a=function(){return c}.apply(t,[]))||(e.exports=a):window.classNames=c}()}}]);
-//# sourceMappingURL=doc-route.chunk_21.js.map?a7fae9d9a0fffc24b81e
+//# sourceMappingURL=doc-route.chunk_21.js.map?852f41bfdcb03076e95e
--- fix/styling-changes/scripts/doc-route.chunk_77.js
+++ fix/styling-changes/scripts/doc-route.chunk_77.js
@@ -1,2 +1,2 @@
 (window.webpackJsonp=window.webpackJsonp||[]).push([[107],{374:function(n,t,e){"use strict";e.r(t);var a=e(1),i=e.n(a),o=e(41);t.default=Object(o.k)(function(n){var t=n.location.pathname.split("/").filter(function(n){return n.length}).pop().split("-").map(function(n,t){return t>0?n:n.charAt(0).toUpperCase()+n.substr(1)}).join(" ");return document.title="".concat(t," | Swedbank Pay DesignGuide"),i.a.createElement("h1",null,t)})}}]);
-//# sourceMappingURL=doc-route.chunk_77.js.map?a7fae9d9a0fffc24b81e
+//# sourceMappingURL=doc-route.chunk_77.js.map?852f41bfdcb03076e95e
