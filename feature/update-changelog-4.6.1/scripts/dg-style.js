715c62b8f0a4f3520378a804af85eddff97b3db0
payex-dev <dev@payex.com>
2020-05-29 09:15:22 +0000

    GitHub release 2.0.2-update-changelog-4-6-1.1+1125

--- feature/update-changelog-4.6.1/scripts/dg-style.js
+++ feature/update-changelog-4.6.1/scripts/dg-style.js
@@ -1,2 +1,2 @@
 (window.webpackJsonp=window.webpackJsonp||[]).push([[17],{356:function(n,o,w){},357:function(n,o,w){}}]);
-//# sourceMappingURL=dg-style.js.map?3b5d1fe19c8e6c99e5ab
+//# sourceMappingURL=dg-style.js.map?d609764bfaa2daee49d7
--- feature/update-changelog-4.6.1/scripts/doc-route.chunk_77.js
+++ feature/update-changelog-4.6.1/scripts/doc-route.chunk_77.js
@@ -1,2 +1,2 @@
 (window.webpackJsonp=window.webpackJsonp||[]).push([[107],{374:function(n,t,e){"use strict";e.r(t);var a=e(1),i=e.n(a),o=e(41);t.default=Object(o.k)(function(n){var t=n.location.pathname.split("/").filter(function(n){return n.length}).pop().split("-").map(function(n,t){return t>0?n:n.charAt(0).toUpperCase()+n.substr(1)}).join(" ");return document.title="".concat(t," | Swedbank Pay DesignGuide"),i.a.createElement("h1",null,t)})}}]);
-//# sourceMappingURL=doc-route.chunk_77.js.map?3b5d1fe19c8e6c99e5ab
+//# sourceMappingURL=doc-route.chunk_77.js.map?d609764bfaa2daee49d7
