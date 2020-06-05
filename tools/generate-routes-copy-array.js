0f48b0831242fc9399da0d94cf311fc48763a3e7
Phi Thien Hoang <pthoang94@gmail.com>
2020-06-05 13:48:45 +0200

    Fix examples routes missing from generate-routes-copy-array.js

--- tools/generate-routes-copy-array.js
+++ tools/generate-routes-copy-array.js
@@ -1,9 +1,10 @@
-const ROOTROUTES = ["/components", "/core", "/utilities", "/getting-started", "/resources", "/404"];
+const ROOTROUTES = ["/components", "/core", "examples", "/utilities", "/getting-started", "/resources", "/404"];
 const resRoutes = require("../src/App/routes/resources");
 const componentsRoutes = require("../src/App/routes/components");
 const coreRoutes = require("../src/App/routes/core");
 const gettingStartedRoutes = require("../src/App/routes/getting-started");
 const utilitiesRoutes = require("../src/App/routes/utilities");
+const examplesRoutes = require("../src/App/routes/examples");
 
 const getRoutes = () => {
     const ROUTES = [...ROOTROUTES];
@@ -13,7 +14,8 @@ const getRoutes = () => {
         ...componentsRoutes,
         ...coreRoutes,
         ...gettingStartedRoutes,
-        ...utilitiesRoutes
+        ...utilitiesRoutes,
+        ...examplesRoutes
     ].forEach(route => {
         ROUTES.push(route.path);
         route.routes.forEach(r => ROUTES.push(r.path));
