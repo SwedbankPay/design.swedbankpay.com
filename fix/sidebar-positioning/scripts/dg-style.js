1d2621b35bd243c1c9c18ac259ace697edb367f9
payex-dev <dev@payex.com>
2020-06-05 14:01:50 +0000

    GitHub release 4.8.0-fix-sidebar-positioning.1+3

---
+++ fix/sidebar-positioning/scripts/dg-style.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[19],{364:function(n,o,w){},365:function(n,o,w){}}]);
+//# sourceMappingURL=dg-style.js.map?64286c451131f8e15182
---
+++ fix/sidebar-positioning/scripts/doc-route.chunk_67.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{434:function(t,n,e){"use strict";e.r(n);var a=e(1),i=e.n(a),r=e(68);n.default=function(){return i.a.createElement(r.RenderPage,{path:"/getting-started",initPath:"/getting-started/introduction"})}}}]);
+//# sourceMappingURL=doc-route.chunk_67.js.map?64286c451131f8e15182
