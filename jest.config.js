869bf5f13a676d7723d10ac9a44e8d738a1988c6
Thomas Lorntzen <cyrthomas1@gmail.com>
2020-07-17 14:50:30 +0200

    Add root jest config files

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,35 @@
 module.exports = {
-    projects: ["<rootDir>/src/app", "<rootDir>/src/scripts"],
+    rootDir: ".",
+    moduleNameMapper: {
+        "\\.(svg)$": "<rootDir>/__mocks__/svgMock.js"
+    },
     collectCoverage: true,
+    collectCoverageFrom: [
+        "<rootDir>/src/App/**/*.{js,jsx}",
+        "<rootDir>/src/scripts/**/*.{js,jsx}"
+    ],
     coverageDirectory: "coverage",
     coverageReporters: [
         "json",
         "lcov",
         "text-summary"
+    ],
+    setupFiles: [
+        "<rootDir>/jest.setup.js"
+    ],
+    snapshotSerializers: [
+        "enzyme-to-json/serializer"
+    ],
+    testMatch: [
+        "<rootDir>/src/App/**/*.test.js",
+        "<rootDir>/src/scripts/**/*.test.js"
+    ],
+    testPathIgnorePatterns: [
+        "\\.spec\\.js",
+        "<rootDir>/src/App/docutils/ComponentPreview/",
+        "<rootDir>/src/App/docutils/JavascriptDocs/",
+        "<rootDir>/src/scripts/main/datepicker/",
+        "<rootDir>/src/scripts/main/topbar/",
+        "<rootDir>/dist/"
     ]
 };
