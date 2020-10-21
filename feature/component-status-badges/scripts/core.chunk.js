556ecdc7379e0f101a002fac86060a9ec5691eb4
payex-dev <dev@payex.com>
2020-10-21 13:28:54 +0000

    GitHub release 4.8.0-component-status-badges.1+109

---
+++ feature/component-status-badges/scripts/core.chunk.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{430:function(e,n,t){"use strict";t.r(n);var o=t(1),r=t.n(o),a=t(68);n.default=function(){return r.a.createElement(a.RenderPage,{path:"/core",initPath:"/core/color"})}}}]);
+//# sourceMappingURL=core.chunk.js.map?909768644e178f7117a9
---
+++ feature/component-status-badges/scripts/dg-style.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[14],{364:function(n,o,w){},365:function(n,o,w){}}]);
+//# sourceMappingURL=dg-style.js.map?909768644e178f7117a9
