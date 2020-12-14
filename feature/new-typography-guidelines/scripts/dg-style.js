6e75251a1d486e9d6ea1c2717dc8cd395a91311b
payex-dev <dev@payex.com>
2020-12-14 11:28:47 +0000

    GitHub release 4.9.0-new-typography-guidelines.1+30

---
+++ feature/new-typography-guidelines/scripts/dg-style.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[12],{384:function(n,o,w){},385:function(n,o,w){}}]);
+//# sourceMappingURL=dg-style.js.map?e0c8c620ed44783eb356
---
+++ feature/new-typography-guidelines/scripts/doc-route.chunk_70.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{439:function(t,n,e){"use strict";e.r(n);var a=e(0),i=e.n(a),r=e(70);n.default=function(){return i.a.createElement(r.RenderPage,{path:"/getting-started",initPath:"/getting-started/introduction"})}}}]);
+//# sourceMappingURL=doc-route.chunk_70.js.map?e0c8c620ed44783eb356
