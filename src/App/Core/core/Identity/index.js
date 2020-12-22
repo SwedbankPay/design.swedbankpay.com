afe3212db3e6b343824b2ad5a95a063fb8ba12ce
PhungNg <phungng91@gmail.com>
2020-12-22 13:19:14 +0100

    added id to h2, changed from grid to DG classes

--- src/App/Core/core/Identity/index.js
+++ src/App/Core/core/Identity/index.js
@@ -5,7 +5,7 @@ import { DocContainer } from "@docutils";
 
 const OurBrand = () => (
     <section>
-        <h2>Our Brand</h2>
+        <h2 id="our-brand">Our Brand</h2>
         <p>Open, simple and caring. These are our values that we always strive to come back to when we design both internal and external experiences. </p>
 
         <div className="brand-quote">
@@ -24,65 +24,65 @@ const OurBrand = () => (
 
 const MenuOverview = () => (
     <section>
-        <h2>Read More</h2>
-        <div className="overview-grid">
-            <Link to="/core/accessibility" className="cards cards-wide m-0">
+        <h2 id="read-more">Read More</h2>
+        <div className="component-overview">
+            <Link to="/core/accessibility" className="cards cards-wide">
                 <div className="cards-content m-0">
                     <span className="h3">Accessibility</span>
                     <span>Principles & WCAG 2.1 guidelines</span>
                 </div>
                 <i className="material-icons">arrow_forward</i>
             </Link>
-            <Link to="/core/color" className="cards cards-wide m-0">
+            <Link to="/core/color" className="cards cards-wide">
                 <div className="cards-content m-0">
                     <span className="h3">Colors</span>
                     <span>Our color palette with HEX codes</span>
                 </div>
                 <i className="material-icons">arrow_forward</i>
             </Link>
-            <Link to="/core/copywriting" className="cards cards-wide m-0">
+            <Link to="/core/copywriting" className="cards cards-wide">
                 <div className="cards-content m-0">
                     <span className="h3">Copywriting</span>
                     <span>Tonality & UX copy in our products</span>
                 </div>
                 <i className="material-icons">arrow_forward</i>
             </Link>
-            <Link to="/core/imagery" className="cards cards-wide m-0">
+            <Link to="/core/imagery" className="cards cards-wide">
                 <div className="cards-content m-0">
                     <span className="h3">Imagery</span>
                     <span>Guidelines and image library</span>
                 </div>
                 <i className="material-icons">arrow_forward</i>
             </Link>
-            <Link to="/core/iconography" className="cards cards-wide m-0">
+            <Link to="/core/iconography" className="cards cards-wide">
                 <div className="cards-content m-0">
                     <span className="h3">Iconography</span>
                     <span>Guidelines and icon library</span>
                 </div>
                 <i className="material-icons">arrow_forward</i>
             </Link>
-            <Link to="/core/grid" className="cards cards-wide m-0">
+            <Link to="/core/grid" className="cards cards-wide">
                 <div className="cards-content m-0">
                     <span className="h3">Grid</span>
                     <span>Our grid and best practices</span>
                 </div>
                 <i className="material-icons">arrow_forward</i>
             </Link>
-            <Link to="/core/logotype" className="cards cards-wide m-0">
+            <Link to="/core/logotype" className="cards cards-wide">
                 <div className="cards-content m-0">
                     <span className="h3">Logotype</span>
                     <span>The Swedbank Pay logotype</span>
                 </div>
                 <i className="material-icons">arrow_forward</i>
             </Link>
-            <Link to="/core/spacing" className="cards cards-wide m-0">
+            <Link to="/core/spacing" className="cards cards-wide">
                 <div className="cards-content m-0">
                     <span className="h3">Spacing</span>
                     <span>8-point spacing system</span>
                 </div>
                 <i className="material-icons">arrow_forward</i>
             </Link>
-            <Link to="/core/typography" className="cards cards-wide m-0">
+            <Link to="/core/typography" className="cards cards-wide">
                 <div className="cards-content m-0">
                     <span className="h3">Typography</span>
                     <span>See our fronts and sizing</span>
