9ddca05e8e01392fbe1aaf51ee41bee47b359a72
payex-dev <dev@payex.com>
2020-12-22 12:58:02 +0000

    GitHub release 2.1.0-implement-imagery-guidelines.1+1459

--- feature/implement-imagery-guidelines/scripts/dg-style.js
+++ feature/implement-imagery-guidelines/scripts/dg-style.js
@@ -1,2 +1,2 @@
 (window.webpackJsonp=window.webpackJsonp||[]).push([[12],{384:function(n,o,w){},385:function(n,o,w){}}]);
-//# sourceMappingURL=dg-style.js.map?4e09905e26c68d78befd
+//# sourceMappingURL=dg-style.js.map?b91992fb7dacd9e79530
--- feature/implement-imagery-guidelines/scripts/utilities.chunk.js
+++ feature/implement-imagery-guidelines/scripts/utilities.chunk.js
@@ -1,2 +1,2 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{443:function(t,e,i){"use strict";i.r(e);var n=i(0),a=i.n(n),o=i(70);e.default=function(){return a.a.createElement(o.RenderPage,{path:"/utilities",initPath:"/utilities/colors"})}}}]);
-//# sourceMappingURL=utilities.chunk.js.map?4e09905e26c68d78befd
+(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{442:function(t,e,i){"use strict";i.r(e);var n=i(0),a=i.n(n),o=i(70);e.default=function(){return a.a.createElement(o.RenderPage,{path:"/utilities",initPath:"/utilities/colors"})}}}]);
+//# sourceMappingURL=utilities.chunk.js.map?b91992fb7dacd9e79530
