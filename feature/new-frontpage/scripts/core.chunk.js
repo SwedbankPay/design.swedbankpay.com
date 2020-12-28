d14bc89ee085ab310ef4d1780af1fd2f683fd11d
payex-dev <dev@payex.com>
2020-12-28 12:46:28 +0000

    GitHub release 4.9.0-new-frontpage.1+89

--- feature/new-frontpage/scripts/core.chunk.js
+++ feature/new-frontpage/scripts/core.chunk.js
@@ -1,2 +1,2 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{440:function(e,n,t){"use strict";t.r(n);var a=t(0),r=t.n(a),c=t(70);n.default=function(){return r.a.createElement(c.RenderPage,{path:"/core",initPath:"/core/"})}}}]);
-//# sourceMappingURL=core.chunk.js.map?646750c2a24459f9d587
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{441:function(e,n,t){"use strict";t.r(n);var a=t(0),r=t.n(a),c=t(70);n.default=function(){return r.a.createElement(c.RenderPage,{path:"/core",initPath:"/core/"})}}}]);
+//# sourceMappingURL=core.chunk.js.map?40624de0ff6e1c3c3927
--- feature/new-frontpage/scripts/dg-style.js
+++ feature/new-frontpage/scripts/dg-style.js
@@ -1,2 +1,2 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[12],{384:function(n,o,w){},385:function(n,o,w){}}]);
-//# sourceMappingURL=dg-style.js.map?646750c2a24459f9d587
+(window.webpackJsonp=window.webpackJsonp||[]).push([[12],{385:function(n,o,w){},386:function(n,o,w){}}]);
+//# sourceMappingURL=dg-style.js.map?40624de0ff6e1c3c3927
