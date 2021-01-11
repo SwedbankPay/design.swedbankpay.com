c57f985b18da6ea61325728d9294b6b7751ba52c
payex-dev <dev@payex.com>
2021-01-11 09:31:32 +0000

    GitHub release 4.9.0-fix-v5-dg-cleanup.1+134

---
+++ fix/v5-dg-cleanup/scripts/dg-style.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[12],{385:function(n,o,w){},386:function(n,o,w){}}]);
+//# sourceMappingURL=dg-style.js.map?ee2e48c5f6ac2fafc55f
---
+++ fix/v5-dg-cleanup/scripts/doc-route.chunk_74.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{439:function(t,n,e){"use strict";e.r(n);var a=e(0),i=e.n(a),r=e(70);n.default=function(){return i.a.createElement(r.RenderPage,{path:"/getting-started",initPath:"/getting-started/introduction"})}}}]);
+//# sourceMappingURL=doc-route.chunk_74.js.map?ee2e48c5f6ac2fafc55f
