0870d99c46370ac7f07c400949dc2407e6e94760
payex-dev <dev@payex.com>
2021-01-15 12:45:31 +0000

    GitHub release 4.9.0-fix-remove-old-spacing-page.1+202

---
+++ fix/remove-old-spacing-page/scripts/core.chunk.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{443:function(t,n,e){"use strict";e.r(n);var i=e(0),a=e.n(i),d=e(69);n.default=function(){return a.a.createElement(d.RenderPage,{path:"/identity",initPath:"/identity/identity"})}}}]);
+//# sourceMappingURL=core.chunk.js.map?b5400122088d986aa630
---
+++ fix/remove-old-spacing-page/scripts/dg-style.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[11],{387:function(n,o,w){},388:function(n,o,w){}}]);
+//# sourceMappingURL=dg-style.js.map?b5400122088d986aa630
