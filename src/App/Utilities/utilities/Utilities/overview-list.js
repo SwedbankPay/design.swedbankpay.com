1e431585590e29ef2ea2c7671da96ffb11e58220
PhungNg <phungng91@gmail.com>
2021-01-18 12:36:40 +0100

    Improved code

---
+++ src/App/Utilities/utilities/Utilities/overview-list.js
@@ -0,0 +1,54 @@
+const overviewList = [
+    {
+        path: "/utilities/borders",
+        title: "Borders",
+        text: "Set borders that suit your needs"
+    },
+    {
+        path: "/utilities/breakpoints",
+        title: "Breakpoints",
+        text: "See our display breakpoints"
+    },
+    {
+        path: "/utilities/colors",
+        title: "Colors",
+        text: "See our color utility classes"
+    },
+    {
+        path: "/utilities/display",
+        title: "Display",
+        text: "See our display properties"
+    },
+    {
+        path: "/utilities/flex",
+        title: "Flex",
+        text: "Layout, alignment and grid"
+    },
+    {
+        path: "/utilities/images",
+        title: "Images",
+        text: "Modify how images are displayed"
+    },
+    {
+        path: "/utilities/sizing",
+        title: "Sizing",
+        text: "Size according to your needs"
+    },
+    {
+        path: "/utilities/spacing",
+        title: "Spacing",
+        text: "Responsive margin and padding"
+    },
+    {
+        path: "/utilities/text",
+        title: "Text",
+        text: "Control wrapping, weight and more"
+    },
+    {
+        path: "/utilities/visibility",
+        title: "Visibility",
+        text: "Control visibilities of display"
+    }
+];
+
+export default overviewList;
