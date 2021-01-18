26b73009e1e5c03f5c5850930c7d79d72dbf1c11
payex-dev <dev@payex.com>
2021-01-18 10:20:14 +0000

    GitHub release 5.1.0-fix-cards-in-identity-page.1+206

---
+++ fix/cards-in-identity-page/scripts/examples.chunk.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{444:function(e,n,a){"use strict";a.r(n);var t=a(0),i=a.n(t),p=a(69);n.default=function(){return i.a.createElement(p.RenderPage,{path:"/examples",initPath:"/examples/login"})}}}]);
+//# sourceMappingURL=examples.chunk.js.map?d5cf0ce07b5c43b5964d
---
+++ fix/cards-in-identity-page/scripts/utilities.chunk.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{445:function(t,i,e){"use strict";e.r(i);var n=e(0),a=e.n(n),u=e(69);i.default=function(){return a.a.createElement(u.RenderPage,{path:"/utilities",initPath:"/utilities/utilities"})}}}]);
+//# sourceMappingURL=utilities.chunk.js.map?d5cf0ce07b5c43b5964d
