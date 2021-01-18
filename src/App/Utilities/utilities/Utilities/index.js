bdb1e83bb52fc6fb60aaf2e75351fa91bca7e7f2
PhungNg <phungng91@gmail.com>
2021-01-18 14:39:43 +0100

    Update index.js

--- src/App/Utilities/utilities/Utilities/index.js
+++ src/App/Utilities/utilities/Utilities/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import overviewList from "@src/app/routes/utilities";
+import overviewList from "@src/App/routes/utilities";
 
 import { DocContainer } from "@docutils";
 
