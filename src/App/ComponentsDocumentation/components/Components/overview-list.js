73dc872d6134fabb75d1ee031ae4c8789917be6e
PhungNg <phungng91@gmail.com>
2021-01-18 12:13:19 +0100

    Improved code

---
+++ src/App/ComponentsDocumentation/components/Components/overview-list.js
@@ -0,0 +1,154 @@
+const overviewList = [
+    {
+        path: "/components/accordion",
+        title: "Accordion",
+        icon: "calendar_view_day",
+        outlined: true
+    },
+    {
+        path: "/components/action-list",
+        title: "Action list",
+        icon: "list",
+        outlined: false
+    },
+    {
+        path: "/components/alerts",
+        title: "Alerts",
+        icon: "error",
+        outlined: true
+    },
+    {
+        path: "/components/badge",
+        title: "Badges",
+        icon: "more",
+        outlined: true
+    },
+    {
+        path: "/components/breadcrumb",
+        title: "Breadcrumb",
+        icon: "last_page",
+        outlined: true
+    },
+    {
+        path: "/components/buttons",
+        title: "Buttons",
+        icon: "touch_app",
+        outlined: true
+    },
+    {
+        path: "/components/cards",
+        title: "Cards",
+        icon: "view_agenda",
+        outlined: true
+    },
+    {
+        path: "/components/charts",
+        title: "Charts",
+        icon: "bar_chart",
+        outlined: true
+    },
+    {
+        path: "/components/code-tags",
+        title: "Code tags",
+        icon: "code",
+        outlined: true
+    },
+    {
+        path: "/components/datepickers",
+        title: "Datepicker",
+        icon: "event",
+        outlined: true
+    },
+    {
+        path: "/components/dialog",
+        title: "Dialog",
+        icon: "picture_in_picture",
+        outlined: true
+    },
+    {
+        path: "/components/footer",
+        title: "Footer",
+        icon: "call_to_action",
+        outlined: true
+    },
+    {
+        path: "/components/lists",
+        title: "List",
+        icon: "sort",
+        outlined: true
+    },
+    {
+        path: "/components/loaders",
+        title: "Loader",
+        icon: "loop",
+        outlined: true
+    },
+    {
+        path: "/components/links",
+        title: "Links",
+        icon: "link",
+        outlined: true
+    },
+    {
+        path: "/components/nav",
+        title: "Navigation",
+        icon: "explore",
+        outlined: true
+    },
+    {
+        path: "/components/pagination",
+        title: "Pagination",
+        icon: "power_input",
+        outlined: true
+    },
+    {
+        path: "/components/progress-steps",
+        title: "Progress steps",
+        icon: "hdr_strong",
+        outlined: true
+    },
+    {
+        path: "/components/forms",
+        title: "Forms",
+        icon: "text_snippet",
+        outlined: true
+    },
+    {
+        path: "/components/sheet",
+        title: "Sheet",
+        icon: "vertical_split",
+        outlined: true
+    },
+    {
+        path: "/components/status",
+        title: "Status",
+        icon: "check_circle",
+        outlined: true
+    },
+    {
+        path: "/components/tabs",
+        title: "Tabs",
+        icon: "folder",
+        outlined: true
+    },
+    {
+        path: "/components/tables",
+        title: "Tables",
+        icon: "view_list",
+        outlined: true
+    },
+    {
+        path: "/components/toast",
+        title: "Toast",
+        icon: "picture_in_picture",
+        outlined: true
+    },
+    {
+        path: "/components/tooltips",
+        title: "Tooltips",
+        icon: "filter_frames",
+        outlined: true
+    }
+];
+
+export default overviewList;
