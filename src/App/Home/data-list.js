9d4e030979caae70404f14ca0be61ef8b5ce2a18
PhungNg <phungng91@gmail.com>
2021-01-19 16:00:52 +0100

    Moved data to a seperated file

---
+++ src/App/Home/data-list.js
@@ -0,0 +1,49 @@
+export const entryCards = [
+    {
+        title: "Get started",
+        text: "Learn more about how to setup and use our system",
+        icon: "wb_incandescent",
+        url: "/get-started/introduction"
+    },
+    {
+        title: "Identity",
+        text: "Learn about our fundamental principles",
+        icon: "palette",
+        url: "/identity"
+    },
+    {
+        title: "Components",
+        text: "Find all currently available components here",
+        icon: "category",
+        url: "/components"
+    },
+    {
+        title: "Utility",
+        text: "See all available CSS classes",
+        icon: "build",
+        url: "/utilities"
+    }
+];
+
+export const changeLogs = [
+    {
+        version: "5.0.0",
+        title: "Major overhaul of Design Guide landing page and more ♥️",
+        text: "Happy New Year everyone! We have a brand new look for the Design Guide landing page, a new left side menu updated to suit the branding of Swedbank Pay. We have finally added both “Get started” and “Identity” and re-written all the related pages."
+    },
+    {
+        version: "4.8.1",
+        title: "Update the actual logotype and guidelines",
+        text: "The journey has finally begun! We’re reconsidering the structure of the Design Guide, starting off with updating and moving ‘Logotype guidelines’ under ‘Resources’ to ‘Core’. We’ve also added the new logotype and guidelines on how to use it."
+    },
+    {
+        version: "4.8.0",
+        title: "Major component updates",
+        text: "Is it Christmas already, you might wonder? No, we’ve just worked really hard and are excited to show you all new component updates. New Cards, Slabs, Status, Tooltips, destructive buttons for everyone. But we’ve also updated and fixed some stuff that was already there. Check out the repository for full overview!"
+    },
+    {
+        version: "4.7.0",
+        title: "Introducing the the new codeview component",
+        text: "We would like to introduce a new way to interact with our components in Design Guide through codeview. Developers can now dynamically adjust the component to fit their needs and generate code based on the adjustments. Besides that we have also fixed many bugs and made adjustment according to feedback!"
+    }
+];
