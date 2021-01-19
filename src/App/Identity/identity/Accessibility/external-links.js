a925448f1fb44862bc0a442eef5cc66c29cc8498
PhungNg <phungng91@gmail.com>
2021-01-19 11:53:54 +0100

    Moved the urls to a seperated list

---
+++ src/App/Identity/identity/Accessibility/external-links.js
@@ -0,0 +1,37 @@
+export const accessibilityAndLaw = [
+    {
+        url: "https://www.funka.com/design-for-alla/lagar-och-regler/",
+        text: "Funka.com – Lagar och regler"
+    },
+    {
+        url: "https://www.funka.com/en/design-for-all/accessibility/web-accessibility-directive-norway-is-facing-a-choice/",
+        text: "Funka.com – Web accessibility directive: Norway is facing a choice"
+    },
+    {
+        url: "https://medium.com/confrere/its-illegal-to-have-an-inaccessible-website-in-norway-and-that-s-good-news-for-all-of-us-b59a9e929d54",
+        text: "Medium.com – It’s illegal to have an inaccessible website in Norway and that’s good for all of us"
+    }
+];
+
+export const othersAboutAccessibility = [
+    {
+        url: "https://www.swedbank.com/sv/hallbarhet/ansvarsfulla-affarer/betala.html",
+        text: "Swedbank – Ansvarsfulla affärer"
+    },
+    {
+        url: "https://polaris.shopify.com/foundations/accessibility",
+        text: "Shopify – Accessibility"
+    },
+    {
+        url: "https://material.io/design/usability/accessibility.html",
+        text: "Google Material – Accessibility"
+    },
+    {
+        url: "http://www.lightningdesignsystem.com/accessibility/overview/",
+        text: "Salesforce Lightning – Accessibility"
+    },
+    {
+        url: "https://spotify.design/article/spotify-inclusive-design-global-accessibility-awareness-day-round-up",
+        text: "Spotify – Inclusive design global accessibility awareness day round up"
+    }
+];
