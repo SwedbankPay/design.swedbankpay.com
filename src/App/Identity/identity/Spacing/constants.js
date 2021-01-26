c46a69013310cdd556face82abfe4bbacef5e473
PhungNg <phungng91@gmail.com>
2021-01-26 14:03:59 +0100

    Changed to constants

---
+++ src/App/Identity/identity/Spacing/constants.js
@@ -0,0 +1,18 @@
+export const diveDeeper = [
+    {
+        path: "https://www.justinmind.com/blog/your-guide-to-space-and-layout-in-ui-design/",
+        text: "Just in mind – Your guide to space and layout in UI design"
+    },
+    {
+        path: "https://www.w3.org/TR/mobile-accessibility-mapping/#touch-target-size-and-spacing",
+        text: "WCAG 2.0 – Touch Target Size and Spacing"
+    },
+    {
+        path: "https://lawsofux.com/laws/law-of-proximity/",
+        text: "Law of UX – Law of Proximity"
+    },
+    {
+        path: "https://uxplanet.org/white-space-in-ui-design-8647d4f685a7",
+        text: "UX Planet – White space in UI design"
+    }
+];
