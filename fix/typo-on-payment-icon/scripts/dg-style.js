5cc0a0fb73db7c8d0b82fd422a93a315fefcc2b4
payex-dev <dev@payex.com>
2021-01-29 10:41:02 +0000

    GitHub release 5.1.0-fix-typo-on-payment-icon.1+265

--- fix/typo-on-payment-icon/scripts/dg-style.js
+++ fix/typo-on-payment-icon/scripts/dg-style.js
@@ -1,2 +1,2 @@
 (window.webpackJsonp=window.webpackJsonp||[]).push([[11],{387:function(n,o,w){},388:function(n,o,w){}}]);
-//# sourceMappingURL=dg-style.js.map?1166dd0f1ed4b7c03f06
+//# sourceMappingURL=dg-style.js.map?401e3036f6429e5f1d8f
--- fix/typo-on-payment-icon/scripts/utilities.chunk.js
+++ fix/typo-on-payment-icon/scripts/utilities.chunk.js
@@ -1,2 +1,2 @@
 (window.webpackJsonp=window.webpackJsonp||[]).push([[7],{444:function(t,i,e){"use strict";e.r(i);var n=e(0),a=e.n(n),u=e(69);i.default=function(){return a.a.createElement(u.RenderPage,{path:"/utilities",initPath:"/utilities/utilities"})}}}]);
-//# sourceMappingURL=utilities.chunk.js.map?1166dd0f1ed4b7c03f06
+//# sourceMappingURL=utilities.chunk.js.map?401e3036f6429e5f1d8f
