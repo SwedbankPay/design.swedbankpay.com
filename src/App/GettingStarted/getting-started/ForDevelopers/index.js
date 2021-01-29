01d914f3364067487e5c630fdd2cab36e9eaaf15
Phung Nguyen <44967773+PhungNg@users.noreply.github.com>
2021-01-29 09:21:47 +0100

    Update src/App/GettingStarted/getting-started/ForDevelopers/index.js
    Co-authored-by: Thomas Lorntzen <thomas.lorntzen@payex.com>

--- src/App/GettingStarted/getting-started/ForDevelopers/index.js
+++ src/App/GettingStarted/getting-started/ForDevelopers/index.js
@@ -14,7 +14,7 @@ const Installation = () => (
         <h3>Include in the header</h3>
         <p>Copy-paste the following CSS code into <CodeTags type="secondary" code="<head>"/> before all the other stylesheets in order to load our CSS.</p>
         <ComponentPreview language="html" codeFigure>
-            <link rel="stylesheet" href={`https://design.swedbankpay.com/v/${packageJson.version}/styles`} />
+            <link rel="stylesheet" href={`https://design.swedbankpay.com/v/${packageJson.version}/styles/dg-style.css`} />
         </ComponentPreview>
 
         <h3>Include at the bottom</h3>
