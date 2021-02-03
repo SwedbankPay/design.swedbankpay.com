edea0558b4fb320a4097bd3533f1e16032fce3a2
Thomas Lorntzen <cyrthomas1@gmail.com>
2021-02-03 10:54:36 +0100

    Split flows into prod and stage

--- .github/scripts/variables.sh
+++ .github/scripts/variables.sh
@@ -54,13 +54,17 @@ generate_variables() {
 
     if [ "$brand" == "payex" ]; then
         echo ::set-output name=BRAND_NAME::PayEx
-        echo ::set-output name=BRAND_URL::https://payexdesignguide.z6.web.core.windows.net
-        echo ::set-output name=AZURE_ACCOUNT::payexdesignguide
+        echo ::set-output name=BRAND_URL_PROD::https://payexdesignguide.z6.web.core.windows.net
+        echo ::set-output name=BRAND_URL_STAGE::https://design.stage.payex.com
+        echo ::set-output name=AZURE_ACCOUNT_PROD::payexdesignguide
+        echo ::set-output name=AZURE_ACCOUNT_STAGE::pxdesignguidestage
         echo ::set-output name=BUILD_SCRIPT::build:prod:payex
     elif [ "$brand" == "swedbankpay" ]; then
         echo ::set-output name=BRAND_NAME::SwedbankPay
-        echo ::set-output name=BRAND_URL::https://swedbankpaydesignguide.z6.web.core.windows.net
-        echo ::set-output name=AZURE_ACCOUNT::swedbankpaydesignguide
+        echo ::set-output name=BRAND_URL_PROD::https://swedbankpaydesignguide.z6.web.core.windows.net
+        echo ::set-output name=BRAND_URL_STAGE::https://design.stage.swedbankpay.com
+        echo ::set-output name=AZURE_ACCOUNT_PROD::swedbankpaydesignguide
+        echo ::set-output name=AZURE_ACCOUNT_STAGE::spdesignguidestage
         echo ::set-output name=BUILD_SCRIPT::build:prod
     else
         echo "Unknown brand '$brand'!"
