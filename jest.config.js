e7f1d631fd8715b9fb28ee441597bcc34d20cfe0
Thomas Lorntzen <cyrthomas1@gmail.com>
2021-02-17 10:41:06 +0100

    Revert jest to old setup

--- jest.config.js
+++ jest.config.js
@@ -1,39 +1,10 @@
 module.exports = {
-    rootDir: ".",
-    moduleNameMapper: {
-        "\\.(svg)$": "<rootDir>/__mocks__/svgMock.js"
-    },
+    projects: ["<rootDir>/src/app", "<rootDir>/src/scripts"],
     collectCoverage: true,
-    collectCoverageFrom: [
-        "<rootDir>/src/App/**/*.{js,jsx}",
-        "<rootDir>/src/scripts/**/*.{js,jsx}"
-    ],
     coverageDirectory: "coverage",
     coverageReporters: [
         "json",
         "lcov",
         "text-summary"
-    ],
-    setupFiles: [
-        "<rootDir>/jest.setup.js"
-    ],
-    snapshotSerializers: [
-        "enzyme-to-json/serializer"
-    ],
-    testMatch: [
-        "<rootDir>/src/App/**/*.test.js",
-        "<rootDir>/src/scripts/**/*.test.js"
-    ],
-    testPathIgnorePatterns: [
-        "\\.spec\\.js",
-        // The snapshot generated from the following files simply destroyed agents, lets just skip the entire thing [THN]
-        "<rootDir>/src/App/docutils/ComponentPreview/",
-        // TODO test fails now that jest has config on root level for some reason
-        "<rootDir>/src/App/docutils/JavascriptDocs/",
-        // TODO test fails now that jest has config on root level for some reason
-        "<rootDir>/src/scripts/main/datepicker/",
-        // TODO test fails now that jest has config on root level for some reason
-        "<rootDir>/src/scripts/main/topbar/",
-        "<rootDir>/dist/"
     ]
 };
---
+++ src/App/jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    collectCoverageFrom: [
+        "**/*.{js,jsx}"
+    ],
+    moduleNameMapper: {
+        "\\.(svg)$": "<rootDir>/../../__mocks__/svgMock.js"
+    },
+    setupFiles: [
+        "./jest.setup.js"
+    ],
+    snapshotSerializers: [
+        "enzyme-to-json/serializer"
+    ],
+    testPathIgnorePatterns: [
+        "\\.spec\\.js",
+
+        // TODO: The following snapshot is 360k long and breaks everything, ignore it [THN]
+        "./docutils/ComponentPreview/"
+    ]
+};
