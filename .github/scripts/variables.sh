2ff6af409d81b6d949a4489c07ece17fd15cafa0
Thomas Lorntzen <cyrthomas1@gmail.com>
2021-03-10 10:59:53 +0100

    Remove deprecated URL variables

--- .github/scripts/variables.sh
+++ .github/scripts/variables.sh
@@ -54,12 +54,10 @@ generate_variables() {
 
     if [ "$brand" == "payex" ]; then
         echo ::set-output name=BRAND_NAME::PayEx
-        echo ::set-output name=BRAND_URL::https://payexdesignguide.z6.web.core.windows.net
         echo ::set-output name=AZURE_ACCOUNT::payexdesignguide
         echo ::set-output name=BUILD_SCRIPT::build:prod:payex
     elif [ "$brand" == "swedbankpay" ]; then
         echo ::set-output name=BRAND_NAME::SwedbankPay
-        echo ::set-output name=BRAND_URL::https://swedbankpaydesignguide.z6.web.core.windows.net
         echo ::set-output name=AZURE_ACCOUNT::swedbankpaydesignguide
         echo ::set-output name=BUILD_SCRIPT::build:prod
     else
