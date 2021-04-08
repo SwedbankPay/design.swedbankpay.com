8849f0bb0cfd63a84ec2bb04ba3a024e1741b031
payex-dev <dev@payex.com>
2021-04-08 10:15:22 +0000

    GitHub release 5.1.0-alpha.302

--- develop/scripts/dg-style.js
+++ develop/scripts/dg-style.js
@@ -1,2 +1,2 @@
 (window.webpackJsonp=window.webpackJsonp||[]).push([[11],{387:function(n,o,w){},388:function(n,o,w){}}]);
-//# sourceMappingURL=dg-style.js.map?7a5df51976ef3b0699a4
+//# sourceMappingURL=dg-style.js.map?22a7dd36fa04651508f2
