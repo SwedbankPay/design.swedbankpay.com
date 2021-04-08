af3399ff4a407a4af370fcf57895f0331b1659a5
payex-dev <dev@payex.com>
2021-04-08 10:21:01 +0000

    GitHub release 2.1.0-update-slider-component.1+1713

--- feature/update-slider-component/scripts/dg-style.js
+++ feature/update-slider-component/scripts/dg-style.js
@@ -1,2 +1,2 @@
 (window.webpackJsonp=window.webpackJsonp||[]).push([[11],{387:function(n,o,w){},388:function(n,o,w){}}]);
-//# sourceMappingURL=dg-style.js.map?9a9ef76d99b46c4d7436
+//# sourceMappingURL=dg-style.js.map?6c7565252c65cafbe101
