8ae30f61ae62bcb42b123e3133dd75daf1945592
Thomas Lorntzen <cyrthomas1@gmail.com>
2021-04-13 16:37:04 +0200

    Attempt a cleaner config

--- jest.config.js
+++ jest.config.js
@@ -1,21 +1,6 @@
 module.exports = {
-    rootDir: process.cwd(),
     projects: [
-        "<rootDir>/src/app",
+        "<rootDir>/src/App",
         "<rootDir>/src/scripts"
-    ],
-    roots: [
-        "<rootDir>/src/app",
-        "<rootDir>/src/scripts"
-    ],
-    collectCoverage: true,
-    coverageDirectory: "coverage",
-    coverageReporters: [
-        "json",
-        "lcov",
-        "text-summary"
-    ],
-    collectCoverageFrom: [
-        "**/*.{js,jsx}"
     ]
 };
