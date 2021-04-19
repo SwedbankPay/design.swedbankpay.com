9bcaf9c73273f32e4d7d2c5a34d602cb3e7af5a5
PhungNg <phungng91@gmail.com>
2021-04-19 12:41:37 +0200

    Add constant sliderShowCase
    Also change howToUse.text value from string to tags

--- src/App/ComponentsDocumentation/components/Rangeslider/constants.js
+++ src/App/ComponentsDocumentation/components/Rangeslider/constants.js
@@ -1,42 +1,95 @@
 import React from "react";
-import RangesliderComponent from "@components/FormComponents/Rangeslider";
+import Rangeslider from "@components/FormComponents/Rangeslider";
 
 export const howToUse = [
     {
         type: "success",
-        component: <RangesliderComponent label="Label" min={0} max={200} value={100}/>,
-        text: "Do use sliders for content with numeric values."
+        component: <Rangeslider label="Label" min={0} max={200} value={100}/>,
+        text: <p>Do use <b>sliders</b> for content with numeric values.</p>
     },
     {
         type: "success",
-        component: <RangesliderComponent label="Label" min={0} max={100} value={50} valueLabelPostfix="%"/>,
-        text: "The sliders component also have a post label for special symbols to help the user easier understand the slider value. It could be shown as above with the precentage symbol."
+        component: <Rangeslider label="Label" min={0} max={100} value={50} valueLabelPostfix="%"/>,
+        text: <p>The <b>sliders</b> component also have a post label for special symbols to help the user easier understand the slider value. It could be shown as above with the percentage symbol.</p>
     },
     {
         type: "success",
-        component: <RangesliderComponent label="Label" min={0} max={200} value={100} valueLabelPrefix="$" />,
-        text: "It also have a pre label for other symbols such as currency e.g. €, $ and £."
+        component: <Rangeslider label="Label" min={0} max={200} value={100} valueLabelPrefix="$" />,
+        text: <p>It also have a pre label for other symbols such as currency e.g. €, $ and £.</p>
     },
     {
         type: "error",
-        component: <RangesliderComponent label="Label" value={50} />,
-        text: "Don’t modify and remove label and maximum/minimum indicators as this makes it harder for user to grasp the content and context of use."
+        component: <Rangeslider label="Label" value={50} />,
+        text: <p>Don’t modify and remove label and maximum/minimum indicators as this makes it harder for user to grasp the content and context of use.</p>
     },
     {
         type: "error",
         component:
-            <div>{/* addlabel */}
+            <div>
                 <div className="rangeslider">
+                    <p className="rangeslider-label">Label</p>
                     <input type="range"/>
                     <output className="value-label">
                     Average
                     </output>
-                </div>
-                <div className="value-range">
-                    <small>Poor</small>
-                    <small>Great</small>
+                    <div className="value-range">
+                        <small>Poor</small>
+                        <small>Great</small>
+                    </div>
                 </div>
             </div>,
-        text: "Do not use sliders for content without numeric values."
+        text: <p>Don’t use sliders for content without numeric values.</p>
     }
 ];
+
+export const sliderShowCase = {
+    id: "no-tabs",
+    elements: [
+        {
+            component: <Rangeslider label="Label" min={0} max={100} value={50}/>,
+            options: {
+                checkbox: [
+                    {
+                        title: "Label add on",
+                        inputs: [
+                            {
+                                id: "help_icon",
+                                name: "With help icon",
+                                value: {}
+                            }
+                        ]
+                    }
+                ],
+                radio: [
+                    {
+                        id: "unit_radio",
+                        title: "Unit value",
+                        values: [
+                            {
+                                name: "Default",
+                                value: {
+                                    valueLabelPrefix: "",
+                                    valueLabelPostfix: ""
+                                }
+                            },
+                            {
+                                name: "Currency",
+                                value: {
+                                    valueLabelPrefix: "$"
+                                }
+                            },
+                            {
+                                name: "Percent",
+                                value: {
+                                    valueLabelPostfix: "%"
+                                }
+                            }
+                        ]
+                    }
+                ]
+            },
+            title: "Slider",
+            description: "The slider component has a label which decribes the intent of the slider. It also has a minimum and maximum range which displays the range which user can pick inbetween. By holding and draging the circle indicator along the line, the user can see the value change and update in real time."
+        }
+    ]
+};
