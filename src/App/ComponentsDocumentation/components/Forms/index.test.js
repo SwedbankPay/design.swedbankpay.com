27070d985bd6fcf77f488f49e4b539779c3c3ec6
PhungNg <phungng91@gmail.com>
2021-04-19 15:35:15 +0200

    Remove rangeslider documentation from forms page
    (cherry picked from commit 7f8bacfc5bf8180e3a80183154e59cf457bf972d)

--- src/App/ComponentsDocumentation/components/Forms/index.test.js
+++ src/App/ComponentsDocumentation/components/Forms/index.test.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { shallow } from "enzyme";
 
-import Forms, { Overview, FormGrid, Validation, UsageWithFieldsets, StaticText, Dropdown, Checkboxes, Toggleboxes, RangeSlider, DisabledFormComponents, JavascriptMethods } from "./index";
+import Forms, { Overview, FormGrid, Validation, UsageWithFieldsets, StaticText, Dropdown, Checkboxes, Toggleboxes, DisabledFormComponents, JavascriptMethods } from "./index";
 
 describe("Documentation: Forms", () => {
     it("is defined", () => {
@@ -136,18 +136,6 @@ describe("Documentation: Forms", () => {
 
         it("renders", () => {
             const wrapper = shallow(<Toggleboxes />);
-
-            expect(wrapper).toMatchSnapshot();
-        });
-    });
-
-    describe("RangeSlider", () => {
-        it("is defined", () => {
-            expect(RangeSlider).toBeDefined();
-        });
-
-        it("renders", () => {
-            const wrapper = shallow(<RangeSlider />);
 
             expect(wrapper).toMatchSnapshot();
         });
