891871728c14a2cee48b45289673bcaf01c09d9c
PhungNg <phungng91@gmail.com>
2021-04-19 15:18:24 +0200

    Add tooltip on rangeslider component

--- src/App/components/FormComponents/Rangeslider.js
+++ src/App/components/FormComponents/Rangeslider.js
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const Rangeslider = ({ label, name, min, max, step, value, disabled, valueLabelPrefix, valueLabelPostfix }) => {
+const Rangeslider = ({ label, name, min, max, step, value, disabled, valueLabelPrefix, valueLabelPostfix, tooltip }) => {
     const attrs = {
         type: "range",
         name,
@@ -23,17 +23,24 @@ const Rangeslider = ({ label, name, min, max, step, value, disabled, valueLabelP
     );
 
     return (
-        <>
-            <div className={`rangeslider ${disabled ? "disabled" : ""}`}>{"\n"}
-                <p className="rangeslider-label">{label}</p>{"\n"}
-                <input {...attrs}/>{"\n"}
-                <ValueLabel />
-                <div className="value-range">{"\n"}
-                    <small>{min}{valueLabelPostfix ? valueLabelPostfix : null}</small>{"\n"}
-                    <small>{max}{valueLabelPostfix ? valueLabelPostfix : null}</small>{"\n"}
-                </div>
+        <div className={`rangeslider ${disabled ? "disabled" : ""}`}>{"\n"}
+            <label htmlFor="rangeslider" className="rangeslider-label">{"\n"}
+                {label}{"\n"}
+                {tooltip &&
+                <>
+                    <i className="material-icons" data-tooltip="Some informational text" data-tooltip-position="top">{"\n"}
+                        help_outline{"\n"}
+                    </i>{"\n"}
+                </>}
+            </label>{"\n"}
+            <input {...attrs}/>{"\n"}
+
+            <ValueLabel />
+            <div className="value-range">{"\n"}
+                <small>{min}{valueLabelPostfix ? valueLabelPostfix : null}</small>{"\n"}
+                <small>{max}{valueLabelPostfix ? valueLabelPostfix : null}</small>{"\n"}
             </div>
-        </>
+        </div>
     );
 };
 
