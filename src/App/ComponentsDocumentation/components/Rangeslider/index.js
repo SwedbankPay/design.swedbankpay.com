a3241daac5ef197451a87e38f4447f49056c9454
PhungNg <phungng91@gmail.com>
2021-04-19 12:46:07 +0200

    Update rangeslider page

--- src/App/ComponentsDocumentation/components/Rangeslider/index.js
+++ src/App/ComponentsDocumentation/components/Rangeslider/index.js
@@ -2,45 +2,33 @@ import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 
 import { ComponentPreview, DocContainer } from "@docutils";
-import RangesliderComponent from "@components/FormComponents/Rangeslider";
-import { howToUse } from "./constants";
+import { howToUse, sliderShowCase } from "./constants";
 import CodeTags from "@components/CodeTags";
-
-const { rangeslider } = window.dg;
 
 const Overview = () => (
     <section>
         <h2 id="overview">Overview</h2>
-        <div className="showcase-panel">
-            <RangesliderComponent min={0} max={200} step={1} value={100} />
-        </div>
-        <div className="p-4 border-right border-left">
-            <h2>Slider</h2>
-            <p className="m-0">The slider component has a label which decribes the intent of the slider. It also has a minimum and maximum range which displays the range which user can pick inbetween. By holding and draging the circle indicator along the line, the user can see the value change and update in real time.</p>
-        </div>
-        <ComponentPreview language="html" codeFigure>
-            <RangesliderComponent min={0} max={200} step={1} value={100} />
-        </ComponentPreview>
+        <ComponentPreview language="html" showCasePanel showCasePanelAdvanced={sliderShowCase} codeFigure />
 
         <h3>When to consider something else</h3>
         <ul className="list list-bullet">
             <li>Use a text input instead when the user needs to select specific number.</li>
-        </ul>
+        </ul>{/* Link to text-input */}
 
         <h3>How to use sliders</h3>
 
-        {howToUse.map(({ type, component, text }) => (
-            <>
+        {howToUse.map(({ type, component, text }, i) => (
+            <React.Fragment key={i}>
                 <div className={`slab slab-plain slab-border-${type} h-100`}>
-                    <h3>Do</h3>
+                    <h3 className="mt-0">Do{type === "error" && "n’t"}</h3>
                     <div className="d-flex flex-column align-items-center">
-                        <div className="pt-3 pb-5 col-8">
+                        <div className="py-3 col-8">
                             {component}
                         </div>
                     </div>
                 </div>
-                <p>{text}</p>
-            </>
+                {text}
+            </React.Fragment>
         ))}
     </section>
 );
@@ -50,19 +38,19 @@ const ContentGuidelines = () => (
         <h2 id="content-guidelines">Content guidelines</h2>
 
         <div className="slab slab-plain h-100">
-            <h3>Do</h3>
             <div className="d-flex flex-column align-items-center">
-                <div className="pt-3 pb-5 col-8">
+                <div className="py-3 col-8">
                     <div>{/* add label */}
-                        <div className="rangeslider">{"\n"}
+                        <div className="rangeslider">
+                            <p className="rangeslider-label">Label</p>
                             <input type="range"/>
                             <output className="value-label">
                                 Selected Value
                             </output>
-                        </div>
-                        <div className="value-range">
-                            <small>Minimum Value</small>
-                            <small>Maximum Value</small>
+                            <div className="value-range">
+                                <small>Minimum Value</small>
+                                <small>Maximum Value</small>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -70,7 +58,7 @@ const ContentGuidelines = () => (
         </div>
 
         <ul className="list list-bullet">
-            <li>The label should clearly explain the context of the slider</li>
+            <li>The <b>label</b> should clearly explain the context of the slider</li>
             <li>Make sure to align the <b>values</b> so that they all follow the same structure and not mixed values or symbols</li>
         </ul>
     </section>
