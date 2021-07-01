5bca7f3dcafc022cc7c9da98db7cb0340d9c4c84
PhungNg <phungng91@gmail.com>
2021-07-01 15:41:12 +0200

    Add checkbox group and error state

--- src/App/components/FormComponents/Checkbox.js
+++ src/App/components/FormComponents/Checkbox.js
@@ -1,19 +1,36 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const Checkbox = ({ id, checked, disabled, label }) => {
+const Checkbox = ({ id, name, disabled, label, errorMessage, required, group, options }) => {
     const attrs = {
         type: "checkbox",
         id: id || null,
+        name,
         disabled: disabled || null,
-        defaultChecked: checked || null
+        required
     };
 
     return (
-        <div className="checkbox">{"\n"}
-            <input {...attrs} />{"\n"}
-            {label ? <label htmlFor={id}>{label}</label> : null}{label ? "\n" : null}
-        </div>
+        <>
+            {group ? <form>
+                <fieldset className={`checkbox-group${errorMessage ? " has-error" : ""}`} disabled={disabled}>
+                    <legend>{label}</legend>
+                    {options.map(({option, id}, i) => <div className="checkbox" key={i}>{"\n"}
+                        <input {...attrs} id={id} />{"\n"}
+                        <label htmlFor={id}>{option}</label>{"\n"}
+                    </div>)}
+                    {errorMessage ? <div className="help-block" data-error={errorMessage || null}></div> : null}
+                </fieldset>
+            </form>
+                : <>
+                    <div className={`checkbox${errorMessage ? " has-error" : ""}`}>{"\n"}
+                        <input {...attrs} />{"\n"}
+                        <label htmlFor={id}>{label}</label>{"\n"}
+                    </div>
+                    {errorMessage ? <div className="help-block" data-error={errorMessage || null}></div> : null}
+                </>
+            }
+        </>
     );
 };
 
