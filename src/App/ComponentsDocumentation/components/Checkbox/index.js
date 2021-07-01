ae5748eb27be02c1bd690bd78c212a1ee50ed2c6
PhungNg <phungng91@gmail.com>
2021-07-01 15:40:18 +0200

    Update index.js

--- src/App/ComponentsDocumentation/components/Checkbox/index.js
+++ src/App/ComponentsDocumentation/components/Checkbox/index.js
@@ -3,14 +3,8 @@ import { Link } from "react-router-dom";
 
 import { ComponentPreview, DocContainer } from "@docutils";
 import CheckboxComponent from "@components/FormComponents/Checkbox";
-
-const ButtonExamples = () => (
-    <React.Fragment>
-        <CheckboxComponent id="radio-example-1" label="Checkbox label" />
-        <CheckboxComponent id="radio-example-2" label="Checkbox label" />
-        <CheckboxComponent id="radio-example-3" label="Checkbox label" />
-    </React.Fragment>
-);
+import CodeTags from "@components/CodeTags";
+import { checkboxShowCase, guidelineOptions, developerDocOptions, doOptions, dontOptions } from "./constants";
 
 const HowTo = () => (
     <section>
@@ -18,13 +12,10 @@ const HowTo = () => (
         <div className="row">
             <div className="col-6">
                 <div className="slab slab-plain slab-border-success h-100">
-                    <h3>Do</h3>
+                    <span className="h3">Do</span>
                     <div className="d-flex flex-column align-items-center">
                         <div className="pt-3 pb-5">
-                            <h4>Button size</h4>
-                            <CheckboxComponent id="radio-do-example-1" label="Small" />
-                            <CheckboxComponent id="radio-do-example-2" label="Medium" />
-                            <CheckboxComponent id="radio-do-example-3" label="Large" />
+                            <CheckboxComponent label="Button size" name="radio-guidelines" options={doOptions} group />
                         </div>
                         <p>Short and concise, similar length on the text is always preferred when setting up a checkbox group. Try to sort the options in a logical order.</p>
                     </div>
@@ -32,13 +23,10 @@ const HowTo = () => (
             </div>
             <div className="col-6">
                 <div className="slab slab-plain slab-border-error">
-                    <h3>Don&#x27;t</h3>
+                    <span className="h3">Don&#x27;t</span>
                     <div className="d-flex flex-column align-items-center">
                         <div className="pt-3 pb-5 w-50">
-                            <h4>Button size</h4>
-                            <CheckboxComponent id="radio-dont-example-1" label="Super duper tiny small" />
-                            <CheckboxComponent id="radio-dont-example-2" label="Large" />
-                            <CheckboxComponent id="radio-dont-example-3" label="Small" />
+                            <CheckboxComponent label="Button size" name="radio-guidelines" options={dontOptions} group />
                         </div>
                         <p>Avoid having labels longer than the rest when possible and displaying options without order.</p>
                     </div>
@@ -51,19 +39,8 @@ const HowTo = () => (
 const Overview = () => (
     <section>
         <h2 id="overview">Overview</h2>
-        <div className="showcase-panel d-flex flex-column align-items-center">
-            <div>
-                <h4>Group heading</h4>
-                <ButtonExamples />
-            </div>
-        </div>
-        <div className="p-4 border-right border-left">
-            <h2>Checkbox group</h2>
-            <p className="m-0">The default setup of the checkbox group always has a label on top which describes the checkbox group and each individual checkbox is then followed by a label which clearly describes what the options and what checking the box represents</p>
-        </div>
-        <ComponentPreview language="html" codeFigure>
-            <ButtonExamples />
-        </ComponentPreview>
+
+        <ComponentPreview language="html" showCasePanel showCasePanelAdvanced={checkboxShowCase} />
 
         <h3>When to consider something else</h3>
         <ul className="list list-bullet">
@@ -79,10 +56,37 @@ const Overview = () => (
 const ContentGuidelines = () => (
     <section>
         <h2 id="content-guidelines">Content guidelines</h2>
+        <div className="slab slab-plain">
+            <div className="d-flex flex-column align-items-center py-5">
+                <CheckboxComponent label="Group heading" name="radio-guidelines" options={guidelineOptions} group />
+            </div>
+        </div>
         <ul className="list list-bullet">
             <li>The <b>Group heading</b> for the radio button group should be descriptive, explaining the context and what actions the user is choosing from. </li>
             <li>The <b>radio button label</b> should be short, to the point and clearly explain the option. It is also preferred to have fewer than three words per label as longer words could extend a label to multiple rows which could disrupt the reading experience.</li>
         </ul>
+    </section>
+);
+
+const DeveloperDocumentation = () => (
+    <section>
+        <h2 id="developer-documentation">Developer documentation</h2>
+        <span className="h3 d-inline-block mt-3">Disabled state</span>
+        <p>Disable a checkbox by adding <CodeTags type="primary" code={"disabled"} /> attribute to the desired Checkbox. You can also disable the <CodeTags type="secondary" code={".form-group"} /> or <CodeTags type="primary" code={"fieldset"} /> in which the checkbox is located, refer to <Link to="/components/forms">forms</Link> for more info.</p>
+
+        <ComponentPreview language="html" showCasePanel codeFigure removeOuterTag>
+            <div className="d-flex justify-content-center">
+                <CheckboxComponent id="disabled-example" label="Checkbox label" disabled/>
+            </div>
+        </ComponentPreview>
+
+        <span className="h3 d-inline-block mt-3">Error state</span>
+        <p>Insert the error message in the attribute <CodeTags type="primary" code={"data-error"} /> in the <CodeTags type="secondary" code={".help-block"} /> element. </p>
+        <ComponentPreview language="html" showCasePanel codeFigure removeOuterTag>
+            <div className="w-50 m-auto pl-5">
+                <CheckboxComponent label="Group heading" name="checkbox-group-example" options={developerDocOptions} group errorMessage="Descriptive helpful error message." />
+            </div>
+        </ComponentPreview>
     </section>
 );
 
@@ -91,6 +95,7 @@ const Checkbox = () => (
         <p className="lead">The checkbox component should be used when the user are presented with a list of options where one or multiple options can be selected by the user.</p>
         <Overview />
         <ContentGuidelines />
+        <DeveloperDocumentation />
     </DocContainer>
 );
 
