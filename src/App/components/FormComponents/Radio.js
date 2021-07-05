4a0ccdd0f6526c7a0ac87ccf2dc21757f54727a4
PhungNg <phungng91@gmail.com>
2021-07-05 11:42:37 +0200

    Update Radio button component
    Add radio-group, new props and proptypes

--- src/App/components/FormComponents/Radio.js
+++ src/App/components/FormComponents/Radio.js
@@ -1,20 +1,35 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const Radio = ({ id, checked, disabled, label, name }) => {
+const Radio = ({ id, checked, disabled, label, group, groupTitle, options, name, required }) => {
     const attrs = {
         type: "radio",
         id: id || null,
         name: name || null,
         disabled: disabled || null,
-        defaultChecked: checked || null
+        defaultChecked: checked || null,
+        required
     };
 
     return (
-        <div className="radio">{"\n"}
-            <input {...attrs} />{"\n"}
-            {label ? <label htmlFor={id}>{label}</label> : null}{label ? "\n" : null}
-        </div>
+        <>
+            {group ? <form>
+                <fieldset className="radio-group" disabled={disabled}>
+                    <legend>{groupTitle}</legend>
+                    {options.map(({ label, id, checked }, i) => <div className="radio" key={i}>{"\n"}
+                        <input {...attrs} id={id} defaultChecked={checked}/>{"\n"}
+                        <label htmlFor={id}>{label}</label>{"\n"}
+                    </div>)}
+                </fieldset>
+            </form>
+                : <>
+                    <div className="radio">{"\n"}
+                        <input {...attrs} />{"\n"}
+                        {label ? <label htmlFor={id}>{label}</label> : null}{label ? "\n" : null}
+                    </div>
+                </>
+            }
+        </>
     );
 };
 
@@ -23,7 +38,11 @@ Radio.propTypes = {
     name: PropTypes.string,
     checked: PropTypes.bool,
     disabled: PropTypes.bool,
-    label: PropTypes.string
+    label: PropTypes.string,
+    group: PropTypes.bool,
+    groupTitle: PropTypes.string,
+    options: PropTypes.array,
+    require: PropTypes.bool
 };
 
 export default Radio;
