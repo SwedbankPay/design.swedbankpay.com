8daa947c521fa9de352330887465e13d4572eb3f
PhungNg <phungng91@gmail.com>
2021-07-06 11:15:34 +0200

    Add optional and tooltip to togglebox component

--- src/App/components/FormComponents/Togglebox.js
+++ src/App/components/FormComponents/Togglebox.js
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const Togglebox = ({ id, checked, disabled, label }) => {
+const Togglebox = ({ id, checked, disabled, label, optional, tooltip }) => {
     const attrs = {
         type: "checkbox",
         id: id || null,
@@ -12,7 +12,16 @@ const Togglebox = ({ id, checked, disabled, label }) => {
     return (
         <div className="togglebox">{"\n"}
             <input {...attrs} />{"\n"}
-            {label ? <label htmlFor={id}>{label}</label> : null}{label ? "\n" : null}
+            {label ? <label htmlFor={id}>
+                {label}
+                {optional && " (optional)"}
+                {tooltip &&
+                    <>
+                        <i className="material-icons" data-tooltip="Some informational text" data-tooltip-position="top">{"\n"}
+                                help_outline{"\n"}
+                        </i>{"\n"}
+                    </>}
+            </label> : null}{label ? "\n" : null}
         </div>
     );
 };
