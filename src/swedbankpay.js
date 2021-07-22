4b2aabf29c4125b428395e92c73cbf3f44ea9f23
Thomas Lorntzen <cyrthomas1@gmail.com>
2021-07-22 20:59:27 +0200

    Update brand entries
    Add design guide and sentry to these files instead of the main script file for the design guide

--- src/swedbankpay.js
+++ src/swedbankpay.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { render } from "react-dom";
+import { init } from "@sentry/browser";
+import "./designguide";
 import App from "./App";
 
 import "./less/swedbankpay.less";
@@ -8,6 +10,19 @@ import "./less/documentation-swedbankpay.less";
 const importAll = r => r.keys().map(r);
 
 importAll(require.context("./img/swedbankpay/logo/", false, /\.(png|jpe?g|svg)$/));
+
+// Sentry reporter
+if (process.env.sentry) {
+    init({
+        dsn: "https://a80375d3150043fa85964c8d5eb24e0b@sentry.io/1481099",
+        whitelistUrls: [
+            "https://design.swedbankpay.com/",
+            "https://design.payex.com/",
+            "https://swedbankpaydesignguide.z16.web.core.windows.net/",
+            "https://payexdesignguide.z16.web.core.windows.net/"
+        ]
+    });
+}
 
 render(
     <App />,
