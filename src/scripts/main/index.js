2f70422e0b48840ffb27946d43fd7df3ea05e77d
Thomas Lorntzen <cyrthomas1@gmail.com>
2021-07-22 21:02:30 +0200

    Remove global dg object and sentry from main dg file

--- src/scripts/main/index.js
+++ src/scripts/main/index.js
@@ -1,5 +1,4 @@
-import { init } from "~/node_modules/@sentry/browser";
-import "@src/polyfills";
+import "../../polyfills";
 
 import accordion from "./accordion";
 import actionList from "./action-list";
@@ -18,19 +17,6 @@ import tooltips from "./tooltips";
 import topbar from "./topbar";
 import utils from "./utils";
 import validation from "./validation";
-
-// Sentry reporter
-if (process.env.sentry) {
-    init({
-        dsn: "https://a80375d3150043fa85964c8d5eb24e0b@sentry.io/1481099",
-        whitelistUrls: [
-            "https://design.swedbankpay.com/",
-            "https://design.payex.com/",
-            "https://swedbankpaydesignguide.z16.web.core.windows.net/",
-            "https://payexdesignguide.z16.web.core.windows.net/"
-        ]
-    });
-}
 
 document.addEventListener("mousedown", () => { document.body.classList.add("intent-mouse"); });
 document.addEventListener("keydown", () => { document.body.classList.remove("intent-mouse"); });
@@ -59,17 +45,10 @@ const dg = {
     validation
 };
 
-window.dg = window.dg ? Object.assign(dg, window.dg) : dg;
-
-if (!window.stopDg) {
-    document.addEventListener("DOMContentLoaded", () => {
-        dg.script.initAll();
-    });
-}
-
 export default dg;
 
 export {
+    accordion,
     actionList,
     alert,
     datepicker,
@@ -78,6 +57,7 @@ export {
     nav,
     rangeslider,
     sheet,
+    sidebar,
     script,
     tabs,
     toast,
