36d3c7bece0dbf57f2184eec89a8be6b34183c17
Eskil Hognestad <eskil_hognestad@hotmail.com>
2021-09-07 14:19:04 +0200

    Update code-preview in link-page for looking more pretty

--- src/App/components/Link/index.js
+++ src/App/components/Link/index.js
@@ -11,8 +11,8 @@ const Link = ({ linkText, leftIcon, rightIcon, smallFont, bright }) => {
 
     return (
         <a href="" className={linkClasses} onClick={e => e.preventDefault()}>
-            {leftIcon && <i className="material-icons" aria-hidden="true">{leftIcon}</i>}
-            {leftIcon || rightIcon ? <span className={(leftIcon ? "ml-2" : null) || (rightIcon ? "mr-2" : null)}>{linkText}</span> : linkText}
+            {leftIcon && <>{"\n"}<i className="material-icons" aria-hidden="true">{leftIcon}</i></>}
+            {leftIcon || rightIcon ? <>{"\n"}<span className={(leftIcon ? "ml-2" : null) || (rightIcon ? "mr-2" : null)}>{linkText}</span>{"\n"}</> : linkText}
             {rightIcon && <i className="material-icons" aria-hidden="true">{rightIcon}</i>}
         </a>
     );
