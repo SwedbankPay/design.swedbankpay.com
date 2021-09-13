54c3c28ac63dfe181224fba85945e03f74617fd4
Phung Nguyen <44967773+PhungNg@users.noreply.github.com>
2021-09-13 13:42:31 +0200

    Update H3 on Select page

--- src/App/ComponentsDocumentation/components/Select/index.js
+++ src/App/ComponentsDocumentation/components/Select/index.js
@@ -47,7 +47,7 @@ const Overview = () => (
             <li>To present a list of possible navigation options or actions that for example effects the filtering of the page, use the <Link to="/components/dropdown-menu">Dropdown menu</Link> component instead of a Select. </li>
         </ul> */}
 
-        <h3>When to use Select</h3>
+        <h3>When to consider something else</h3>
 
         <WhenToUse />
 
