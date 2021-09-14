f11456ee5cc854900b9b66bb5b1f78dfdbfd2c22
Phung Nguyen <44967773+PhungNg@users.noreply.github.com>
2021-09-14 14:53:28 +0200

    Edit description for Tabs

--- src/App/ComponentsDocumentation/components/Tabs/constants.js
+++ src/App/ComponentsDocumentation/components/Tabs/constants.js
@@ -38,7 +38,7 @@ export const tabsShowCasePanel = {
                 ]
             },
             title: "Tabs",
-            description: <>Tabs appear in a single row, above their content. Only one tab can be selected at a time and the selected tab decides which content that is shown in the container beneath. The width of each tab is dependent on its label and the width of the row of tabs is dependent on the container. If the container can’t display all the tabs at once the tabs can be set to scrollable by adding the class <CodeTags code=".tabs-scroll" type="secondary"/>.</>
+            description: <>Tabs appear in a single row, above their content. Only one tab can be selected at a time and the selected tab decides which content that is shown in the container beneath. The width of each tab is dependent on its label and the width of the row of tabs is dependent on the container. If the container can’t display all the tabs at once the tabs can be set to scrollable by adding the class <CodeTags code=".tabs-scroll" type="secondary"/>, but it is not optimized for desktop usage at the moment.</>
         }
     ]
 };
