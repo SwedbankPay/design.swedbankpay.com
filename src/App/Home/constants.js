583ba03cd7c462e1e92c41df6346b05612251a07
Phung Nguyen <44967773+PhungNg@users.noreply.github.com>
2021-09-23 08:54:27 +0200

    Release/7.0.1

--- src/App/Home/constants.js
+++ src/App/Home/constants.js
@@ -1,5 +1,10 @@
 export const changeLogs = [
     {
+        version: "7.0.1",
+        title: "A patch of love",
+        text: "Some things didn’t end up looking the way we wanted them (Safari, we're mostly looking at you). This patch will remove some styling bugs from previous release."
+    },
+    {
         version: "7.0.0",
         title: "Favicon, component updates and copy code feature",
         text: "You asked and we heard you, Favicon assets can now be found in the logotype section. We also redesigned a few of our components and updated the documentation. Goodbye to Tabs looking disabled on hover, Hello new fresh looking Alerts and Loaders. And you know what, you will no longer need to manually copy code from our code viewer since we have a brand new copy code button in place."
@@ -18,10 +23,5 @@ export const changeLogs = [
         version: "6.0.0",
         title: "Splitting up forms component",
         text: "Long time no see! We are back with a bigger update regarding the forms page and its components. In order make it easier to locate the various components (checkbox, select, radio buttons etc) that were included under the name “forms”, you will now be able to find components with a slightly updated design on their own pages with more detailed explanations and guides of usage!"
-    },
-    {
-        version: "5.0.3",
-        title: "Icon patch",
-        text: "Trustly icon patched."
-    },
+    }
 ];
