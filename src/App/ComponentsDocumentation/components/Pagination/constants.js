9c0c2dd3746d581a9e66ab811c8c51568ed243fb
Eskil Hognestad <eskil_hognestad@hotmail.com>
2021-09-27 14:18:43 +0200

    Update do and dont for pagination page and some content

---
+++ src/App/ComponentsDocumentation/components/Pagination/constants.js
@@ -0,0 +1,48 @@
+import React from "react";
+import Pagination from "~/src/App/components/Pagination";
+
+export const paginationItems = [
+    /* eslint-disable object-property-newline */
+    { name: "1", href: "#", active: true },
+    { name: "2", href: "#", active: false },
+    { name: "3", href: "#", active: false },
+    { name: "4", href: "#", active: false },
+    { name: "5", href: "#", active: false }
+    /* eslint-enable object-property-newline */
+];
+
+export const PaginationShowCasePanel = {
+    id: "no-tabs",
+    elements: [
+        {
+            component: <Pagination type="simple" mobileView={false} items={paginationItems} arrows text="1" />,
+            options: {
+                radio: [
+                    {
+                        id: "device_radio",
+                        title: "Device",
+                        values: [
+                            {
+                                name: "Desktop",
+                                value: {
+                                    mobileView: false
+                                }
+                            },
+                            {
+                                name: "Mobile",
+                                value: {
+                                    mobileView: true
+                                }
+                            }
+                        ]
+                    }
+                ]
+            },
+            title: "Pagination",
+            description: <>
+                <p>The pagination component consist of page number links as well as arrows to go to next or previous page. The arrow to previous page is disabled when the first page is active and the next arrow when the last page is active. If there are 7 pages or fewer, all page number links will be shown, but if there are more pages an ellipsis is used to truncate the pages as follows: [first] ... [current-1] [current] [current+1] ... [last].</p>
+                <p> On smaller screens the page links are replaced with text showing the page information, still using arrows to go to next or previous page but with the option to go to the first or last page as well.</p>
+            </>
+        }
+    ]
+};
--- src/App/ComponentsDocumentation/components/Pagination/index.js
+++ src/App/ComponentsDocumentation/components/Pagination/index.js
@@ -1,54 +1,52 @@
 import React from "react";
 
 import { ComponentPreview, DocContainer } from "@docutils";
-import PaginationComponent from "@components/Pagination";
-import CodeTags from "@components/CodeTags";
+import { PaginationShowCasePanel, paginationItems } from "./constants";
+import PaginationComponent from "~/src/App/components/Pagination";
 
-const paginationItems = [
-    /* eslint-disable object-property-newline */
-    { name: "1", href: "#", active: true },
-    { name: "2", href: "#", active: false },
-    { name: "3", href: "#", active: false },
-    { name: "4", href: "#", active: false },
-    { name: "5", href: "#", active: false }
-    /* eslint-enable object-property-newline */
-];
+const Overview = () => (
+    <section id="pagination-overview">
+        <h2 id="pagination">Pagination</h2>
+        <p>Pagination is commonly used for things like table listings, search results or directories.The need for pagination is influenced by the amount of data to be displayed and the aim is to not overwhelm users with information and to improve the loading performance of the system. </p>
+        <ComponentPreview language="html" showCasePanel showCasePanelAdvanced={PaginationShowCasePanel} codeFigure/>
+        <h3>When to consider something else</h3>
+        <ul className="pl-3 list list-bullet">
+            <li>For exploration of content, where users are browsing aimlessly for something interesting, infinite scroll is better suited then using pagination. Pagination is best when the user is trying to accomplish a goal, for instance trying to find a particular article from a list. </li>
+        </ul>
 
-const DefaultPagination = () => (
-    <>
-        <h2 id="default-pagination">Default pagination</h2>
-        <p>
-                Default pagination shows the pages as numbers. Add <CodeTags type="secondary" code=".pagination" /> to an <CodeTags type="primary" code={"<ul>"} /> containing
-            <CodeTags type="primary" code={"<li>"} />. If you wish to present the user with navigational arrows you need to add those as <CodeTags type="primary" code={"<li>"} /> elements.
-        </p>
-        <ComponentPreview language="html" showCasePanel codeFigure>
-            <PaginationComponent type="simple"  items={paginationItems} arrows text="1" />
-        </ComponentPreview>
-    </>
-);
+        <h3>How to use Pagination</h3>
+        <h4>Items per page</h4>
+        <p>10 items per page can be used as a general rule, but in reality it depends on your specific context. Consider the display size of each item, showing 10 items and leaving half the screen unused is not good practice.</p>
+        <h4>Placement</h4>
+        <p>Place pagination below the set of items it paginates through.</p>
 
-const PaginationBullets = () => (
-    <>
-        <h2 id="pagination-bullets">Pagination bullets</h2>
-        <p>
-                If you want to show bullets instead of numbers you can add <CodeTags type="secondary" code=".pagination-bullets" /> to your <CodeTags type="primary" code={"<ul>"} />.
-        </p>
-        <ComponentPreview language="html" showCasePanel codeFigure>
-            { /*<PaginationComponent type="bullets" items={paginationItems} arrows farArrows />*/}
-        </ComponentPreview>
-    </>
-);
-
-const SimplePagination = () => (
-    <>
-        <h2 id="simple-pagination">Simple pagination</h2>
-        <p>
-            If the number of pages get high enough then it is better to use numbers.
-        </p>
-        <ComponentPreview language="html" showCasePanel codeFigure>
-            {/*<PaginationComponent text="16 of 256" arrows farArrows />*/}
-        </ComponentPreview>
-    </>
+        <div className="row">
+            <div className="col-lg-6">
+                <div className="slab slab-plain slab-border-success pb-5 h100" >
+                    <h4>Do</h4>
+                    <div className="d-flex flex-column align-items-center justify-content-center mt-4 mb-4">
+                        <div className="pagination-content-box-example"></div>
+                        <div className="pagination-content-box-example"></div>
+                        <div className="pagination-content-box-example"></div>
+                        <div className="pagination-content-box-example"></div>
+                        <PaginationComponent type="simple" items={paginationItems} arrows text="1"/>
+                    </div>
+                </div>
+            </div>
+            <div className="col-lg-6">
+                <div className="slab slab-plain slab-border-error pb-5 h100">
+                    <h4>{"Don't"}</h4>
+                    <div className="d-flex flex-column align-items-center justify-content-center mt-4 mb-4">
+                        <PaginationComponent type="simple" items={paginationItems} arrows text="1"/>
+                        <div className="pagination-content-box-example"></div>
+                        <div className="pagination-content-box-example"></div>
+                        <div className="pagination-content-box-example"></div>
+                        <div className="pagination-content-box-example"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 );
 
 const Pagination = () => (
@@ -56,13 +54,11 @@ const Pagination = () => (
         <p className="lead">
             Use the pagination component to indicate that a series of related content exist across multiple pages.
         </p>
-        <DefaultPagination />
-        <PaginationBullets />
-        <SimplePagination />
+        <Overview/>
     </DocContainer>
 );
 
 export default Pagination;
 
 /* For testing */
-export { DefaultPagination, SimplePagination, PaginationBullets };
+export { Overview };
