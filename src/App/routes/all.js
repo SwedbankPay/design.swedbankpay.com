37c07a73958bec3dabee9f662bc85ca0f383d05c
Phung Nguyen <44967773+PhungNg@users.noreply.github.com>
2021-09-29 15:29:20 +0200

    Add Routing for Templates

--- src/App/routes/all.js
+++ src/App/routes/all.js
@@ -2,12 +2,14 @@ const componentsRoutes = require("./components");
 const identityRoutes = require("./identity");
 const gettingStartedRoutes = require("./getting-started");
 const utilitiesRoutes = require("./utilities");
+const templatesRoutes = require("./templates");
 // const examplesRoutes = require("./examples");
 
 module.exports = [
     ...gettingStartedRoutes,
     ...identityRoutes,
     ...componentsRoutes,
+    ...templatesRoutes,
     ...utilitiesRoutes
     // ...examplesRoutes
 ];
