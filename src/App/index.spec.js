61bb5c1ddb2e903720de5e952cbac90cc8bc1023
Thomas Lorntzen <cyrthomas1@gmail.com>
2021-10-06 16:18:33 +0200

    Use brandTitle where applicable

--- src/App/index.spec.js
+++ src/App/index.spec.js
@@ -3,8 +3,10 @@ import { Selector } from "testcafe";
 fixture `Frontpage`
     .page `http://localhost:3000`;
 
+const brandTitle = process.env.brandTitle;
+
 test("Frontpage test", async () => {
     const header = await Selector(".panel-body").find("h2");
 
-    expect(header.textContent).contains("Welcome to the Swedbank Pay DesignGuide");
+    expect(header.textContent).contains(`Welcome to the ${brandTitle} Design Guide`);
 });
