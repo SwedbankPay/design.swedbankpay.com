3ef65d34fbf86db2be0840be7d5650fdea4fea9a
Eskil Hognestad <eskil_hognestad@hotmail.com>
2021-10-07 13:23:21 +0200

    Pleasing lint, remove redundant dotts and element-push of list

--- src/scripts/main/pagination/index.js
+++ src/scripts/main/pagination/index.js
@@ -9,114 +9,117 @@ const _createPagination = paginationContainer => {
     const arrowBack = paginationContainer.querySelector("li.arrow-back");
 
     const _initialActive = () => {
-        console.log("init active");
         [...pages][0].classList.add("active");
         arrowBack.classList.add("disabled");
-    }
+    };
 
     const _getActiveIndex = () => {
         const currentActive = [...pages].find(page => page.classList.contains("active"));
+
         return [].indexOf.call(pages, currentActive);
-    }
+    };
 
     const _addListener = () => {
         [...pages].map(page => {
             page.addEventListener("click", e => {
                 _setActiveOnClick(e);
-            })
+            });
         });
 
         arrowForward.addEventListener("click", e => {
             e.preventDefault();
             _arrowClick("forward");
-        })
+        });
 
         arrowBack.addEventListener("click", e => {
             e.preventDefault();
             _arrowClick("backward");
-        })
+        });
     };
 
     const _arrowClick = arrowDirection => {
         const activeIndex = _getActiveIndex();
+
         if (arrowDirection === "forward") {
             pages[activeIndex + 1].classList.add("active");
         } else {
             pages[activeIndex - 1].classList.add("active");
         }
+
         pages[activeIndex].classList.remove("active");
         _setDisabledArrow();
         _paginate();
-    }
+    };
 
-    const _setActiveOnClick = (e) => {
+    const _setActiveOnClick = e => {
         e.preventDefault();
-        [...pages].map(page => page.classList.remove("active"))
+        [...pages].map(page => page.classList.remove("active"));
         e.currentTarget.classList.add("active");
         _setDisabledArrow();
         _paginate();
-    }
+    };
 
     const _setDisabledArrow = () => {
         const activeIndex = _getActiveIndex();
+
         _removeDisabledArrow();
+
         if (activeIndex === 0) {
             arrowBack.classList.add("disabled");
         } else if (activeIndex === pages.length - 1) {
             arrowForward.classList.add("disabled");
         }
-    }
+    };
 
     const _removeDisabledArrow = () => {
         arrowBack.classList.contains("disabled") ? arrowBack.classList.remove("disabled") : null;
         arrowForward.classList.contains("disabled") ? arrowForward.classList.remove("disabled") : null;
-    }
+    };
 
     const _paginate = () => {
-        console.log("paginate");
         const paginatedPages = [];
         const activeIndex = _getActiveIndex();
+
         if (pages.length > 7) {
             const anchor = document.createElement("a");
-            const anchor2 = document.createElement("a");
             const dotts = document.createElement("LI");
-            const dotts2 = document.createElement("LI");
+
             dotts.appendChild(anchor);
-            dotts2.appendChild(anchor2);
             dotts.classList.add("dotts");
-            dotts2.classList.add("dotts");
             anchor.innerText = "..";
-            anchor2.innerText = "..";
+
+            paginatedPages.push(pages[0]);
+
             if (activeIndex < 4) {
-                paginatedPages.push(pages[0]);
-                [...pages].slice(1,5).map(page => paginatedPages.push(page));
+                [...pages].slice(1, 5).map(page => paginatedPages.push(page));
                 paginatedPages.push(dotts);
                 paginatedPages.push(pages[pages.length - 1]);
             }
+
             if (activeIndex > 3 && activeIndex < pages.length - 4) {
-                paginatedPages.push(pages[0]);
-                activeIndex >= 4 ? paginatedPages.push(dotts) : null; 
+                activeIndex >= 4 ? paginatedPages.push(dotts) : null;
                 [...pages].slice(activeIndex - 1, activeIndex + 2).map(page => paginatedPages.push(page));
-                activeIndex >= pages.length - 3 ? null : paginatedPages.push(dotts2);
+                activeIndex >= pages.length - 3 ? null : paginatedPages.push(dotts.cloneNode(true));
                 paginatedPages.push(pages[pages.length - 1]);
             }
-            if (activeIndex >= pages.length - 4){
-                paginatedPages.push(pages[0]);
+
+            if (activeIndex >= pages.length - 4) {
                 paginatedPages.push(dotts);
                 [...pages].slice(-5).map(page => paginatedPages.push(page));
             }
         } else {
             [...pages].map(page => paginatedPages.push(page));
         }
+
         _paginateContainer(paginatedPages);
-    }
+    };
 
     const _paginateContainer = paginatedPages => {
         paginationContainer.innerHTML = "";
         paginationContainer.appendChild(arrowBack);
         [...paginatedPages].map(page => paginationContainer.appendChild(page));
         paginationContainer.appendChild(arrowForward);
-    }
+    };
 
     _paginate();
     _initialActive();
