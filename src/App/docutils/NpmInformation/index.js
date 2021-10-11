0245bb1dbc6546356c767c8bcdee8ce1498f1356
Eskil Hognestad <eskil_hognestad@hotmail.com>
2021-10-11 13:23:40 +0200

    Update component to include prop 'noInit' for removing one line in code previewer, if component does not have the init function

--- src/App/docutils/NpmInformation/index.js
+++ src/App/docutils/NpmInformation/index.js
@@ -5,19 +5,20 @@ import PropTypes from "prop-types";
 import { ComponentPreview } from "@docutils";
 import CodeTags from "../../components/CodeTags";
 
-const NpmInformation = ({ componentName }) => (
+const NpmInformation = ({ componentName, noInit }) => (
     <section>
         <h3>Usage with NPM</h3>
         <p><Link to="/get-started/for-developers/">Usage with NPM</Link> will require you to import it from <CodeTags type="primary" code="@swedbankpay/design-guide" />.</p>
         <ComponentPreview language="javascript" codeFigure dangerousHTML>
             import &#123; {componentName} &#125; from &quot;@swedbankpay/design-guide&quot;; {"\n"}
-            {componentName}.init()
+            {noInit ? "" : `${componentName}.init()`}
         </ComponentPreview>
     </section>
 );
 
 NpmInformation.propTypes = {
-    componentName: PropTypes.string
+    componentName: PropTypes.string,
+    noInit: PropTypes.bool
 };
 
 export default NpmInformation;
