5a1852fd6700d5a6bb9dca4f4daff04aa1fcbd79
Phung Nguyen <44967773+PhungNg@users.noreply.github.com>
2021-10-13 11:02:09 +0200

    Rename Templates to Patterns

--- src/App/Templates/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import { RenderPage } from "../utils";
-
-const Templates = () => (
-    <RenderPage path="/templates" initPath="/templates/templates" />
-);
-
-export default Templates;
