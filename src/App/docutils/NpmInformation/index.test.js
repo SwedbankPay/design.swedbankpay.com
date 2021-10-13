8b623bdcfb28b7589697fd70df4c5792c78b808b
Eskil Hognestad <eskil_hognestad@hotmail.com>
2021-10-13 15:14:05 +0200

    Included some tests for ensuring content is being rendered, and not rendered.

--- src/App/docutils/NpmInformation/index.test.js
+++ src/App/docutils/NpmInformation/index.test.js
@@ -12,23 +12,29 @@ describe("Utilities: NpmInformation", () => {
         const wrapper = shallow(<NpmInformation/>);
 
         expect(wrapper).toMatchSnapshot();
+        expect(wrapper.contains("undefined.init()")).toEqual(true);
     });
 
     it("renders with componentName-prop", () => {
         const wrapper = shallow(<NpmInformation componentName="test"/>);
 
         expect(wrapper).toMatchSnapshot();
+        expect(wrapper.contains("test")).toEqual(true);
+        expect(wrapper.contains("test.init()")).toEqual(true);
     });
 
     it("renders with noInit-prop", () => {
         const wrapper = shallow(<NpmInformation noInit/>);
 
         expect(wrapper).toMatchSnapshot();
+        expect(wrapper.contains("init()")).toEqual(false);
     });
 
     it("renders with noInit- and componentName-prop", () => {
         const wrapper = shallow(<NpmInformation noInit componentName="test"/>);
 
         expect(wrapper).toMatchSnapshot();
+        expect(wrapper.contains("test")).toEqual(true);
+        expect(wrapper.contains("test.init()")).toEqual(false);
     });
 });
