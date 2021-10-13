9045d9a0ad87054b79f3dc14bc11efe942ec69d4
Eskil Hognestad <eskil_hognestad@hotmail.com>
2021-10-13 13:51:50 +0200

    Create tests for testing functions in script. Some tests are needed to get full coverage..

--- src/scripts/main/pagination/index.test.js
+++ src/scripts/main/pagination/index.test.js
@@ -1,5 +1,4 @@
 import { beforeEach, expect, it } from "@jest/globals";
-import { shallow } from "enzyme";
 import React from "react";
 import ReactDOM from "react-dom";
 
@@ -12,22 +11,58 @@ describe("scripts: pagination", () => {
             <li className="arrow-back disabled" aria-label="back">
                 <a href="#"></a>
             </li>
-            <li>
+            <li className="">
                 <a href="#" aria-label="Go to page 1">1</a>
             </li>
-            <li className=" " >
+            <li className="active" >
                 <a href="#" aria-label="Go to page 2">2</a>
             </li>
-            <li className=" " >
+            <li className="" >
                 <a href="#" aria-label="Go to page 3">3</a>
             </li>
-            <li className=" " >
+            <li className="" >
                 <a href="#" aria-label="Go to page 4">4</a>
             </li>
-            <li className=" " >
+            <li className="" >
                 <a href="#" aria-label="Go to page 5">5</a>
             </li>
             <li className="">
+                <a href="#" aria-label="Go to page 6">6</a>
+            </li>
+            <li className="">
+                <a href="#" aria-label="Go to page 7">7</a>
+            </li>
+            <li className="">
+                <a href="#" aria-label="Go to page 8">8</a>
+            </li>
+            <li className="arrow-forward" aria-label="forward">
+                <a href="#"></a>
+            </li>
+        </ul>
+
+    );
+    const TestPagination2 = ({ id }) => (
+
+        <ul className="pagination pagination-simple" id={id}>
+            <li className="arrow-back disabled" aria-label="back">
+                <a href="#"></a>
+            </li>
+            <li className="">
+                <a href="#" aria-label="Go to page 1">1</a>
+            </li>
+            <li className="" >
+                <a href="#" aria-label="Go to page 2">2</a>
+            </li>
+            <li className="" >
+                <a href="#" aria-label="Go to page 3">3</a>
+            </li>
+            <li className="" >
+                <a href="#" aria-label="Go to page 4">4</a>
+            </li>
+            <li className="" >
+                <a href="#" aria-label="Go to page 5">5</a>
+            </li>
+            <li className="active">
                 <a href="#" aria-label="Go to page 6">6</a>
             </li>
             <li className="arrow-forward" aria-label="forward">
@@ -113,13 +148,104 @@ describe("scripts: pagination", () => {
                 expect(renderedPagination.innerHTML).toEqual(expect.stringContaining(arrowBack.innerHTML));
             });
 
-            const clickHandler = jest.fn();
+            const _arrowNavigation = jest.fn();
 
             it("gets click event on arrow forward", () => {
-                arrowForward.addEventListener("click", clickHandler); //
-
-                expect(clickHandler).toHaveBeenCalled();
+                arrowForward.addEventListener("click", _arrowNavigation("forward")); //
+                // expect li[1] classname contains active
+                expect(_arrowNavigation).toHaveBeenCalled();
             });
+        });
+
+        describe("checks if correct index is provided", () => {
+            ReactDOM.render(<TestPagination id="tester"/>, div);
+
+            const renderedPagination = document.getElementById("tester");
+            const pages = renderedPagination.querySelectorAll("li:not(.arrow-back, .arrow-forward)");
+
+            it("getActiveIndex gives correct active index", () => {
+                expect(pagination._getActiveIndex(pages)).toEqual(1);
+
+            });
+        });
+
+        describe("arrow-functions are working properly", () => {
+            ReactDOM.render(<TestPagination2 id="tester"/>, div);
+
+            const renderedPagination = document.getElementById("tester");
+            const pages = renderedPagination.querySelectorAll("li:not(.arrow-back, .arrow-forward)");
+            const arrows = renderedPagination.querySelectorAll("[class*='arrow-']");
+
+            it("arrows is longer than zero", () => {
+                expect(arrows.length).toBeGreaterThan(0);
+            });
+
+            it("has no disabled class in either arrow", () => {
+                pagination._removeDisabledArrowState(arrows);
+                expect(arrows[0].className).not.toContain("disabled");
+                expect(arrows[1].className).not.toContain("disabled");
+            });
+
+            it("forward-arrow has disabled class and back-arrow has not", () => {
+                pages[pages.length - 1].classList.add("active");
+
+                pagination._addDisabledArrowState(arrows, pages);
+                expect(arrows[0].className).not.toContain("disabled");
+                expect(arrows[1].className).toContain("disabled");
+            });
+        });
+
+        describe("paginate function are working properly", () => {
+            ReactDOM.render(<TestPagination id="tester"/>, div);
+
+            const renderedPagination = document.getElementById("tester");
+            const pages = renderedPagination.querySelectorAll("li:not(.arrow-back, .arrow-forward)");
+            const paginatedPages = pagination._paginate(pages);
+            // TODO
+
+            it("pages is containting 8 li-elements and paginatedPages are containing 7 li-elements", () => {
+                expect(pages.length).toEqual(8);
+                expect(paginatedPages.length).toEqual(7);
+            });
+
+            it("paginatedPages contains dotts when number of items > 7", () => {
+                expect(paginatedPages[5].className).toContain("dotts");
+            });
+
+            it("paginatedpages contains last li-element as last child", () => {
+                expect(paginatedPages[6].innerHTML).toEqual("<a href=\"#\" aria-label=\"Go to page 8\">8</a>"); // Last element in paginatedPages List
+            });
+
         });
     });
 });
+
+/** Missing to test event listeners and create more tests after modifying by clicking, on other tests...
+ *
+ * It(returns corrects active index){
+
+    Set initial acitve page
+        - expect..(querySelector(li) sjekke førstesin classlist inneholder 'active')
+
+    Get active index
+        - render pagination, kalt tilfeldig, fram (arrovnav forward) tilbake, getactiveindex, sammenlikne med queryselector
+        Arrow Navigation
+            -
+
+    Add listener
+        -
+
+    Set active page on click
+        - Simulate click.
+        - Klikke på en av li og sjekke active classlist
+
+    Disabled arrow state
+        - active index til 1, navigation(back) forvent arrow to have disabled
+
+    Paginate
+        - teste om det skjer en slicing
+        - sjekke child elments, forvent rekkefølge. child nr x er dotter osv
+        - classlist dotts
+
+ *
+ */
