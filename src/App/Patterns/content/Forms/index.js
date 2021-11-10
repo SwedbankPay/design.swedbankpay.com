e76bcd29ce4c2e5beb997979a2a84cbb94435223
Eskil Hognestad <eskil_hognestad@hotmail.com>
2021-11-10 14:23:36 +0100

    Update styling on label. Removed style from component less and included it in general less for DG documentation

--- src/App/Patterns/content/Forms/index.js
+++ src/App/Patterns/content/Forms/index.js
@@ -48,7 +48,7 @@ const ContentGuidelines = () => (
                 <div className="slab slab-primary d-flex justify-content-center mb-0 py-1 extra-small">24px</div>
                 <InputGroup type="select" label="Input label" placeholder="Select an option" selectOptions={["Option 1", "Option 2"]} optional className="mb-0"/>
                 <div className="slab slab-primary d-flex justify-content-center mb-0 py-1 extra-small">24px</div>
-                <RadioButton groupTitle="Input label" name="guidelines-radio-example" group options={radioOptions} className="mb-0 legend-example"/>
+                <RadioButton groupTitle="Input label" name="guidelines-radio-example" group options={radioOptions} className="mb-0 forms-radio-example"/>
                 <div className="slab slab-primary d-flex justify-content-center mb-0 py-2 extra-small">32px</div>
                 <div className="d-flex">
                     <Button type="primary" label="Submit" className="mr-3"/>
