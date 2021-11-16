f6053a8d9d641fd4933c1a19bb645fdead4f57f8
Phung Nguyen <44967773+PhungNg@users.noreply.github.com>
2021-11-16 12:08:29 +0100

    Update Alert

--- src/App/components/Alert/index.js
+++ src/App/components/Alert/index.js
@@ -11,11 +11,11 @@ const Alert = ({ id, type, icon, close, text, className }) => {
 
     return (
         <div id={id} className={alertClasses}>{"\n"}
-            <i className="material-icons alert-icon mr-3" aria-hidden="true">{icon}</i>
+            <i className="material-icons alert-icon" aria-hidden="true">{icon}</i>
             {text}{"\n"}
             {close ? <>
                 <a href="#" data-alert-close="" onClick={e => e.preventDefault()}>{"\n\t\t"}
-                    <i className="material-icons ml-3" aria-hidden="true">close</i>{"\n\t"}
+                    <i className="material-icons" aria-hidden="true">close</i>{"\n\t"}
                 </a>{"\n"}
             </> : null}
         </div>
