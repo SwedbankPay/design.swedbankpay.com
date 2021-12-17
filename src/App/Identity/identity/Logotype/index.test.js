1626f41e6391826273d4b24d42a47f774bef7342
William Meander <82865399+WMeander@users.noreply.github.com>
2021-12-17 09:52:58 +0100

    clean up

--- src/App/Identity/identity/Logotype/index.test.js
+++ src/App/Identity/identity/Logotype/index.test.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { shallow } from "enzyme";
 
-import Logotype, { Overview, HorizontalLogo, LogoBlack, LogoWhite, SpacingZone, MisuseLogo, LogotypeBackgrounds, Favicon, LogoPayex, LogoWhitePayex } from "./index";
+import Logotype, { Overview, LogoBlack, LogoWhite, SpacingZone, MisuseLogo, LogotypeBackgrounds, Favicon, LogoPayex, LogoWhitePayex } from "./index";
 
 describe("Core: Logotype", () => {
     it("is defined", () => {
@@ -45,18 +45,6 @@ describe("Core: Logotype", () => {
 
         it("renders", () => {
             const wrapper = shallow(<LogoWhitePayex />);
-
-            expect(wrapper).toMatchSnapshot();
-        });
-    });
-
-    describe("HorizontalLogo", () => {
-        it("is defined", () => {
-            expect(HorizontalLogo).toBeDefined();
-        });
-
-        it("renders", () => {
-            const wrapper = shallow(<HorizontalLogo />);
 
             expect(wrapper).toMatchSnapshot();
         });
