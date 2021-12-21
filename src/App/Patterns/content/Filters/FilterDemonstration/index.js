b221513e31023bc6ac96781221be771900e7b2a8
Eskil Hognestad <eskil_hognestad@hotmail.com>
2021-12-21 11:07:16 +0100

    Modify demopage to only include potential filters and table

--- src/App/Patterns/content/Filters/FilterDemonstration/index.js
+++ src/App/Patterns/content/Filters/FilterDemonstration/index.js
@@ -1,104 +1,171 @@
-import React, { useState } from "react";
-import Button from "~/src/App/components/Button";
+import React from "react";
+import ButtonComponent from "@components/Button";
+import InputGroup from "@components/InputGroup";
 
-import "./filterDemonstration.less";
+const payments = [
+    {
+        referenceNumber: 10022,
+        amount: 567,
+        paymentMethod: "Swish",
+        status: "Successful",
+        operation: "Purchase"
+    },
+    {
+        referenceNumber: 10022,
+        amount: 567,
+        paymentMethod: "Swish",
+        status: "Successful",
+        operation: "Purchase"
+    },
+    {
+        referenceNumber: 10022,
+        amount: 567,
+        paymentMethod: "Swish",
+        status: "Successful",
+        operation: "Purchase"
+    },
+    {
+        referenceNumber: 10022,
+        amount: 567,
+        paymentMethod: "Swish",
+        status: "Successful",
+        operation: "Purchase"
+    }
+];
 
-const FilterDemonstration = () => {
-    const [showDemo, setShowDemo] = useState(true); // Change to false
-
-    return (
-        <>
-            <div className={`filter-demonstration ${showDemo ? "show" : "hide"}`}>
-                <section>
-                    <Button label="Close demo" className="filter-demo-close-button" type="secondary" icon="fullscreen"></Button>
-                    <div className="filer-demo-container">
-                        <header className="filer-demo-header">Payments</header>
-                        <div className="filters">
-                            <p className="tempTest">Et filter</p>
-                            <p className="tempTest">Et filter</p>
-                            <p className="tempTest">Et filter</p>
-                            <p className="tempTest">Et filter</p>
-                            <p className="tempTest">Et filter</p>
-                        </div>
-                        <div className="table-demo">
-                            <table className="table">
-                                <thead>
-                                    <tr>
-                                        <th scope="col">#</th>
-                                        <th scope="col">First Name</th>
-                                        <th scope="col">Last Name</th>
-                                        <th scope="col">Location</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                        <th scope="row">1</th>
-                                        <td>Sven</td>
-                                        <td>Svensson</td>
-                                        <td>Visby</td>
-                                    </tr>
-                                    <tr>
-                                        <th scope="row">2</th>
-                                        <td>Sara</td>
-                                        <td>Svensson</td>
-                                        <td>Stockholm</td>
-                                    </tr>
-                                    <tr>
-                                        <th scope="row">3</th>
-                                        <td>Ola</td>
-                                        <td>Nordmann</td>
-                                        <td>Oslo</td>
-                                    </tr>
-                                    <tr>
-                                        <th scope="row">4</th>
-                                        <td>Holger</td>
-                                        <td>Danske</td>
-                                        <td>Copenhagen</td>
-                                    </tr>
-                                    <tr>
-                                        <th scope="row">5</th>
-                                        <td>Matti</td>
-                                        <td>Meikäläinen</td>
-                                        <td>Lahti</td>
-                                    </tr>
-                                    <tr>
-                                        <th scope="row">1</th>
-                                        <td>Sven</td>
-                                        <td>Svensson</td>
-                                        <td>Visby</td>
-                                    </tr>
-                                    <tr>
-                                        <th scope="row">2</th>
-                                        <td>Sara</td>
-                                        <td>Svensson</td>
-                                        <td>Stockholm</td>
-                                    </tr>
-                                    <tr>
-                                        <th scope="row">3</th>
-                                        <td>Ola</td>
-                                        <td>Nordmann</td>
-                                        <td>Oslo</td>
-                                    </tr>
-                                    <tr>
-                                        <th scope="row">4</th>
-                                        <td>Holger</td>
-                                        <td>Danske</td>
-                                        <td>Copenhagen</td>
-                                    </tr>
-                                    <tr>
-                                        <th scope="row">5</th>
-                                        <td>Matti</td>
-                                        <td>Meikäläinen</td>
-                                        <td>Lahti</td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
-                </section>
+const FilterDemonstration = type => (
+    <>
+        <div className="filter-demonstration">
+            <h3 className="mb-4">Payments ({payments.length})</h3>
+            <div className="d-flex flex-row">
+                <InputGroup placeholder="Search reference number " prefixValue="search" prefixType="icon"/>
+                <ButtonComponent type="primary" label="Do something" />
             </div>
-        </>
-    );
-};
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th scope="col">Reference number</th>
+                        <th scope="col">Amount</th>
+                        <th scope="col">Payment method</th>
+                        <th scope="col">Status</th>
+                        <th scope="col">Operation</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Sven</td>
+                        <td>Svensson</td>
+                        <td>Visby</td>
+                        <td>Visby</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Sven</td>
+                        <td>Svensson</td>
+                        <td>Visby</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Sven</td>
+                        <td>Svensson</td>
+                        <td>Visby</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Sven</td>
+                        <td>Svensson</td>
+                        <td>Visby</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Sven</td>
+                        <td>Svensson</td>
+                        <td>Visby</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">2</th>
+                        <td>Sara</td>
+                        <td>Svensson</td>
+                        <td>Stockholm</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">3</th>
+                        <td>Ola</td>
+                        <td>Nordmann</td>
+                        <td>Oslo</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">4</th>
+                        <td>Holger</td>
+                        <td>Danske</td>
+                        <td>Copenhagen</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">5</th>
+                        <td>Matti</td>
+                        <td>Meikäläinen</td>
+                        <td>Lahti</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Sven</td>
+                        <td>Svensson</td>
+                        <td>Visby</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">2</th>
+                        <td>Sara</td>
+                        <td>Svensson</td>
+                        <td>Stockholm</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">3</th>
+                        <td>Ola</td>
+                        <td>Nordmann</td>
+                        <td>Oslo</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">4</th>
+                        <td>Holger</td>
+                        <td>Danske</td>
+                        <td>Copenhagen</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">5</th>
+                        <td>Matti</td>
+                        <td>Meikäläinen</td>
+                        <td>Lahti</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Sven</td>
+                        <td>Svensson</td>
+                        <td>Visby</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">2</th>
+                        <td>Sara</td>
+                        <td>Svensson</td>
+                        <td>Stockholm</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">3</th>
+                        <td>Ola</td>
+                        <td>Nordmann</td>
+                        <td>Oslo</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">4</th>
+                        <td>Holger</td>
+                        <td>Danske</td>
+                        <td>Copenhagen</td>
+                    </tr>
+
+                </tbody>
+            </table>
+        </div>
+    </>
+);
 
 export default FilterDemonstration;
