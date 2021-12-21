2118a3316c856b77fcd2475e50e29c04df208530
Eskil Hognestad <eskil_hognestad@hotmail.com>
2021-12-21 11:10:02 +0100

    Update to include classname modifications. Enable usage of demopage in filters

--- src/App/components/Dialog/index.js
+++ src/App/components/Dialog/index.js
@@ -3,22 +3,22 @@ import PropTypes from "prop-types";
 
 import { Icon } from "@docutils";
 
-const Dialog = ({ diaId, diaHeader, children }) => (
+const Dialog = ({ diaId, diaHeader, children, className }) => (
     <>
-        <div className="dialog" id={diaId} role="dialog" aria-modal="true" aria-labelledby="aria-label-dia" aria-describedby="aria-describe-dia">
+        <div className={`dialog ${className}`} id={diaId} role="dialog" aria-modal="true" aria-labelledby="aria-label-dia" aria-describedby="aria-describe-dia">
             <section>
                 <header className="dialog-header" id="aria-label-dia">
                     <h4>{diaHeader}</h4>{"\n"}
-                    <button type="button" className="dialog-close">{"\n"}
+                    {className ? <button type="button" className="btn btn-secondary" data-dialog-close><i className="material-icons pr-1" aria-hidden="true">fullscreen</i>Close demo</button> : <button type="button" className="dialog-close">{"\n"}
                         <Icon type="close" />{"\n"}
-                    </button>
+                    </button>}
                 </header>
                 <div className="dialog-body" id="aria-describe-dia">
                     {children}
                 </div>
                 <footer className="dialog-footer">{"\n"}
-                    <button className="btn btn-secondary" type="button" data-dialog-close>Cancel</button>{"\n"}
-                    <button className="btn btn-primary" type="button">Delete</button>{"\n"}
+                    {className ? "" : <button className="btn btn-secondary" type="button" data-dialog-close>Cancel</button> }
+                    {className ? "" : <button className="btn btn-primary" type="button">Delete</button>}
                 </footer>
             </section>
         </div>
