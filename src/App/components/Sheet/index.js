84e3f09ad8a154b1f29ff14e0489f6ee609e663d
Eskil Hognestad <eskil_hognestad@hotmail.com>
2022-01-13 16:15:40 +0100

    New styling for sheet component. Desktop is working, including footer and header. Mobileview's animation is badshit crazy.. needs to be worked with

--- src/App/components/Sheet/index.js
+++ src/App/components/Sheet/index.js
@@ -2,21 +2,35 @@ import React from "react";
 import PropTypes from "prop-types";
 
 import { Icon } from "@docutils";
+import Button from "@components/Button";
 
-const Sheet = ({ id, requireAction, children }) => (
+const Sheet = ({ id, requireAction, title, footer, children }) => (
     <div className="sheet" id={id} data-require-action={requireAction ? true : null}>
         <section>{"\n"}
-            <a href="#" className="sheet-close">{"\n\t\t\t"}
-                <Icon type="close" />{"\n\t\t"}
-            </a>
-            {children}
+            <div className="sheet-header">
+                <h2>{title}</h2>
+                <a href="#" className="sheet-close">{"\n\t\t\t"}
+                    <Icon type="close" />{"\n\t\t"}
+                </a>
+            </div>
+            <div className={`sheet-content ${footer ? "has-footer" : null}`}>
+                {children}
+            </div>
+            {footer ?
+                <div className="sheet-footer">
+                    <Button type="primary" label="Primary" />
+                    <Button type="secondary" label="Secondary" />
+                </div> : <></>
+            }
         </section>
     </div>
 );
 
 Sheet.propTypes = {
     id: PropTypes.string,
-    requireAction: PropTypes.bool
+    requireAction: PropTypes.bool,
+    title: PropTypes.string.isRequired,
+    footer: PropTypes.bool
 };
 
 export default Sheet;
