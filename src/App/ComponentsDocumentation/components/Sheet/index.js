e38d8ed3b611352609647d34e577daf42e73c192
Eskil Hognestad <eskil_hognestad@hotmail.com>
2022-01-13 16:14:42 +0100

    Minor changes of component call including title and footer as params

--- src/App/ComponentsDocumentation/components/Sheet/index.js
+++ src/App/ComponentsDocumentation/components/Sheet/index.js
@@ -6,6 +6,7 @@ import CodeTags from "@components/CodeTags";
 
 import { sheet } from "@src/scripts/main";
 import NpmInformation from "@docutils/NpmInformation";
+import ButtonComponent from "@components/Button";
 
 const Example = () => (
     <>
@@ -18,8 +19,7 @@ const Example = () => (
             ways to manipulate the main content. If you have a form then you could include an icon that opens said sheet to display more information about what the expected input is.
         </p>
         <ComponentPreview language="html" showCasePanel codeFigure>
-            <SheetComponent id="demo-sheet">
-                <h2>Sheet component</h2>
+            <SheetComponent id="demo-sheet" title="Sheet Component" footer>
                 <p>This is the sheet component. It can contain anything you want.</p>
                 <p>It can also contain lots of useless text, just like the text in this paragraph. Or it can contain useful info and cheats. For example if you have a math quiz coming up;</p>
                 <ul className="list">
@@ -50,7 +50,26 @@ const Example = () => (
                             <label htmlFor="subscribe-checkbox">Sign up to our math newsletter</label>
                         </div>
                     </div>
-                    <button className="btn btn-primary" type="submit" onClick={e => e.preventDefault()}>Submit</button>
+                    <div className="form-group">
+                        <label htmlFor="full-name">Full name</label>
+                        <div className="input-group">
+                            <input type="text" className="form-control" id="full-name" placeholder="Enter your full name" />
+                        </div>
+                        <div className="help-block">Your full name</div>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="email-address">Email</label>
+                        <div className="input-group">
+                            <input type="text" className="form-control" id="email-address" placeholder="Enter your email" />
+                        </div>
+                        <div className="help-block">Email means electronic mail, not express mail</div>
+                    </div>
+                    <div className="form-group">
+                        <div className="checkbox">
+                            <input type="checkbox" id="subscribe-checkbox" />
+                            <label htmlFor="subscribe-checkbox">Sign up to our math newsletter</label>
+                        </div>
+                    </div>
                 </form>
             </SheetComponent>{"\n"}
             <button className="btn btn-primary" type="button" data-sheet-open="demo-sheet">Open sheet</button>
@@ -65,7 +84,7 @@ const RequireAction = () => (
             To disable the listener responsible for closing the sheet when a user clicks outside, add <CodeTags type="secondary" code="data-require-action" /> to your Sheet.
         </p>
         <ComponentPreview language="html" showCasePanel codeFigure>
-            <SheetComponent id="demo-sheet-1" requireAction>
+            <SheetComponent id="demo-sheet-1" title="Sheet component" requireAction>
                 <h2>Sheet component</h2>
                 <p>
                     This sheet can not be closed by clicking outside its white area.
