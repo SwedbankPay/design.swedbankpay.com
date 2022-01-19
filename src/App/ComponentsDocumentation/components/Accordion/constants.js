411e2a6fad795274ba7c42fb4002f8e822018f15
Eskil Hognestad <eskil_hognestad@hotmail.com>
2022-01-19 12:55:21 +0100

    Include constants.js file for ensuring overall code style. A minor bug in Component preview, need help to understand the problem.

---
+++ src/App/ComponentsDocumentation/components/Accordion/constants.js
@@ -0,0 +1,56 @@
+import React from "react";
+import Accordion from "@components/Accordion";
+import CodeTags from "@components/CodeTags";
+
+const items = [
+    {
+        id: "exp-1",
+        title: "My title",
+        content: "Proident cupidatat irure excepteur id minim dolore mollit amet reprehenderit consectetur aliqua officia Lorem elit. Cupidatat ea tempor ut esse. Aliquip exercitation in nisi reprehenderit anim Lorem. Excepteur nostrud officia irure tempor aute ipsum. Voluptate nisi incididunt proident enim reprehenderit fugiat fugiat magna sit reprehenderit Lorem anim. Aute commodo cillum nisi consectetur amet eiusmod do tempor incididunt anim."
+    },
+    {
+        id: "exp-2",
+        title: "My title",
+        content: "Proident cupidatat irure excepteur id minim dolore mollit amet reprehenderit consectetur aliqua officia Lorem elit. Cupidatat ea tempor ut esse. Aliquip exercitation in nisi reprehenderit anim Lorem. Excepteur nostrud officia irure tempor aute ipsum. Voluptate nisi incididunt proident enim reprehenderit fugiat fugiat magna sit reprehenderit Lorem anim. Aute commodo cillum nisi consectetur amet eiusmod do tempor incididunt anim."
+    },
+    {
+        id: "exp-3",
+        title: "My title",
+        content: "Proident cupidatat irure excepteur id minim dolore mollit amet reprehenderit consectetur aliqua officia Lorem elit. Cupidatat ea tempor ut esse. Aliquip exercitation in nisi reprehenderit anim Lorem. Excepteur nostrud officia irure tempor aute ipsum. Voluptate nisi incididunt proident enim reprehenderit fugiat fugiat magna sit reprehenderit Lorem anim. Aute commodo cillum nisi consectetur amet eiusmod do tempor incididunt anim."
+    }
+];
+
+const AccordionComponentExample = ({ accordionGroup }) => (
+    <>
+        <div className={`w-100 ${accordionGroup ? "accordion-group" : ""}`}>
+            <Accordion items={items}/>
+        </div>
+    </>
+);
+
+export const accordionShowCase = {
+    id: "no-tabs",
+    elements: [
+        {
+            component: <AccordionComponentExample />,
+            options: {
+                checkbox: [
+                    {
+                        title: "Accordion group",
+                        inputs: [
+                            {
+                                id: "checkbox-accordions",
+                                name: "Open one at a time",
+                                value: {
+                                    accordionGroup: true
+                                }
+                            }
+                        ]
+                    }
+                ]
+            },
+            title: "Accordion",
+            description: <p>The accordion component can be either collapsed or expanded. Accordions are by default in the collapsed state with the body content hidden. If a group of accordions are wrapped in a <CodeTags type="primary" code=".accordion-group"/>, only one will be open at a time.</p>
+        }
+    ]
+};
