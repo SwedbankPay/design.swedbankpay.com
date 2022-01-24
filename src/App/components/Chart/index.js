0aff2fd5b68bb2a122dda82119447cb234c643b1
Eskil Hognestad <eskil_hognestad@hotmail.com>
2022-01-24 16:14:05 +0100

    Update component to be functional. Include param of description, used for aria-label. Add role='img' on all charts

--- src/App/components/Chart/index.js
+++ src/App/components/Chart/index.js
@@ -1,26 +1,22 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
 import PropTypes from "prop-types";
-
 import { chart } from "@src/scripts/dashboard";
 
-class Chart extends Component {
-    componentDidMount () {
-        const { id, options, colorPool } = this.props;
-
+const Chart = ({ id, options, colorPool, description }) => {
+    useEffect(() => {
         chart(id, options, colorPool);
-    }
+    }, []);
 
-    render () {
-        return (
-            <canvas id={this.props.id} />
-        );
-    }
-}
+    return (
+        <canvas id={id} role="img" aria-label={description}/>
+    );
+};
+
+export default Chart;
 
 Chart.propTypes = {
     id: PropTypes.string.isRequired,
     options: PropTypes.object.isRequired,
-    colorPool: PropTypes.arrayOf(PropTypes.string)
+    colorPool: PropTypes.arrayOf(PropTypes.string),
+    description: PropTypes.string.isRequired
 };
-
-export default Chart;
