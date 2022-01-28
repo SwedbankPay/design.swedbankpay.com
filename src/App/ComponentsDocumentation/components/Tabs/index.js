9efaa0014d67eddb0bcf043460c1e4d1be0871ed
Eskil Hognestad <eskil_hognestad@hotmail.com>
2022-01-28 11:49:28 +0100

    Update dont-example to be not similar to do-example

--- src/App/ComponentsDocumentation/components/Tabs/index.js
+++ src/App/ComponentsDocumentation/components/Tabs/index.js
@@ -31,7 +31,7 @@ const Overview = () => (
             </div>
             <div className="slab slab-plain slab-border-error px-4">
                 <h3 className="mb-5">Don&apos;t</h3>
-                <TabsComponent id="tabs-scroll-example" ulId="tabs-scroll-example-ul" items={[...tabItems.slice(0, 3)]} scroll/>
+                <TabsComponent id="tabs-scroll-example-dont" ulId="tabs-scroll-example-ul" items={[...tabItems.slice(0, 3)]} scroll/>
                 <div className="slab slab-default p-5"></div>
             </div>
             <p>The tab component should always fill the full container of the content it governs. As seen on the examples above, the length of the underline for tab isn’t determined by the amount of tabs/length of the text. But should instead indicate the content that it anchors to and contains.</p>
