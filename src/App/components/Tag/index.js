e083cdc4b339f7ee700d738b8aa6ca4d0f2f0acb
Eskil Hognestad <eskil_hognestad@hotmail.com>
2022-02-03 12:36:26 +0100

    Update component to also take in className as param

--- src/App/components/Tag/index.js
+++ src/App/components/Tag/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const Tag = ({ code, type, text, removable }) => (
+const Tag = ({ code, type, text, removable, className}) => (
     <>
         {code ?
-            <code className={`tag code ${type ? type : "primary"}`}>{text}</code>
+            <code className={`tag code ${type ? type : "primary"} ${className ? className : ""}`}>{"\n"}{text}</code>
             :
-            <span className={`tag ${type ? type : "primary"} ${removable ? "removable" : ""}`}>{text}
-                {removable ? <button type="button" className="close-button"><i className="material-icons m-auto" aria-label="remove tag">close</i></button> : <></> }
+            <span className={`tag ${type ? type : "primary"} ${removable ? "removable" : ""} ${className ? className : ""}`}>{"\n"}{text}{"\n"}
+                {removable ? <button type="button" className="close-button">{"\n"}<i className="material-icons m-auto" aria-label="remove tag">close</i>{"\n"}</button> : <></> }
             </span>
         }
     </>
@@ -17,7 +17,8 @@ Tag.propTypes = {
     text: PropTypes.string.isRequired,
     type: PropTypes.oneOf(["primary", "secondary", "tertiary"]),
     removable: PropTypes.bool,
-    code: PropTypes.bool
+    code: PropTypes.bool,
+    className: PropTypes.string
 };
 
 export default Tag;
