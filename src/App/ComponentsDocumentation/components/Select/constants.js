c4222bfade8aed35130b65d87be2b95b8a4a2bed
Eskil Hognestad <eskil_hognestad@hotmail.com>
2022-02-08 13:31:05 +0100

    Berryberry is back <3

--- src/App/ComponentsDocumentation/components/Select/constants.js
+++ src/App/ComponentsDocumentation/components/Select/constants.js
@@ -8,7 +8,7 @@ export const selectOverview = {
     id: "no-tabs",
     elements: [
         {
-            component: <InputGroup id="select-showcase-example" type="select" label="Label" placeholder="Select option" selectOptions={["Blueberry", "Blackberry", "Strawberry"]} />,
+            component: <InputGroup id="select-showcase-example" type="select" label="Label" placeholder="Select option" selectOptions={["Blueberry", "Blackberry", "Berryberry"]} />,
             options: {
                 checkbox: [
                     {
