9f0230efedd4a765f68bb2784b88f43d9ab57609
Eskil Hognestad <eskil_hognestad@hotmail.com>
2022-02-15 10:59:10 +0100

    Update code tag type to primary and bullet-list bug

--- src/App/ComponentsDocumentation/components/Sheet/index.js
+++ src/App/ComponentsDocumentation/components/Sheet/index.js
@@ -62,7 +62,7 @@ const SheetExample = () => (
 const ContentGuidelines = () => (
     <>
         <h2>Content guidelines</h2>
-        <div className="row">
+        <div>
             <div className="col-12">
                 <div className="d-flex justify-content-center mt-4 mb-4">
                     <img src={`${basename}img/documentation/sheet/content-guidelines.png`} alt="Sheet component with sticky header and footer" className="img-fluid"/>
@@ -84,7 +84,7 @@ const DeveloperDocumentation = () => (
         <p>Use <CodeTags type="secondary" code="dg.sheet.init()"/> to <Link to="/get-started/for-developers">initialize</Link> all accordions. This will give the sheets open and close functions automatically.</p>
         <p>To open a given sheet, use <CodeTags type="secondary" code="dg.sheet.open(<sheet-id>)"/></p>
         <p>To close a given sheet, use <CodeTags type="secondary" code="dg.sheet.close(<sheet-id>)"/></p>
-        <p>To disable the listener responsible for closing the sheet when a user clicks outside, add <CodeTags type="secondary" code="data-require-action"/> to your sheet.</p>
+        <p>To disable the listener responsible for closing the sheet when a user clicks outside, add <CodeTags type="primary" code="data-require-action"/> to your sheet.</p>
     </>
 );
 
