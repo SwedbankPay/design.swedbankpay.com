9edd9048b2a2e6687a3c77e05c9ab7bca4f28db2
Eskil Hognestad <eskil_hognestad@hotmail.com>
2022-03-22 10:36:48 +0100

    Update tooltip documentation page

--- src/App/ComponentsDocumentation/components/Tooltips/index.js
+++ src/App/ComponentsDocumentation/components/Tooltips/index.js
@@ -13,121 +13,14 @@ const Overview = () => (
         <p>You can use the attribute <CodeTags type="secondary" code="tooltip-position" /> to position the tooltip relative to the containing element, use the values <CodeTags type="secondary" code="top" />, <CodeTags type="secondary" code="right" />, <CodeTags type="secondary" code="bottom" /> or <CodeTags type="secondary" code="left" />. If the attribute is not provided, it will default to top.</p>
 
         <ComponentPreview language="html" showCasePanel codeFigure>
-            <div className="form-group">{"\n"}
-                <label htmlFor="input-showcase-example">Input label{"\n"}
-                    <TooltipComponent id="showcase-tooltip-example" text="Tooltip Top" />{"\n"}
-                </label>{"\n"}
-                <input className="form-control" id="input-showcase-example" aria-describedby="input-showcase-example" />
-            </div>
-        </ComponentPreview>
-
-        <ComponentPreview language="html" showCasePanel codeFigure>
-            <TooltipComponent id="showcase-tooltip-example" text="Tooltip Bottom" icon="help_outline" position="bottom" />
-        </ComponentPreview>
-        {/*
-        <ComponentPreview language="html" showCasePanel codeFigure >
-            <button className="tooltip2 tooltip-right">
-                <i className="material-icons mr-3"
-                    data-tooltip=""
-                    data-tooltip-position="top">{"\n"}
-                    help_outline{"\n"}
-                    <span className="tooltip">{"\n"}
-                        Payment capture is the process by which payments are secured once the payment has been authorized by the customer.{"\n"}
-                    </span>{"\n"}
-                </i>{"\n"}
-            </button>
-        </ComponentPreview>
-        <ComponentPreview language="html" showCasePanel codeFigure removeOuterTag>
-            <div className="button-group">{"\n"}
-                <button
-                    className="btn btn-primary"
-                    type="button"
-                    data-tooltip="This is a tooltip!"
-                    data-tooltip-position="left"
-                >Tooltip to the left
-                </button>{"\n"}
-                <button
-                    className="btn btn-primary"
-                    type="button"
-                    data-tooltip="Payment capture is the process by which payments are secured once the payment has been authorized by the customer."
-                    data-tooltip-position="top"
-                >Tooltip on top
-                </button>{"\n"}
-                <button
-                    className="btn btn-primary"
-                    type="button"
-                    data-tooltip="This is a tooltip!"
-                    data-tooltip-position="bottom"
-                >Tooltip to the bottom
-                </button>{"\n"}
-                <button
-                    className="btn btn-primary"
-                    type="button"
-                    data-tooltip="Payment capture is the process by which payments are secured once the payment has been authorized by the customer!"
-                    data-tooltip-position="right"
-                >Tooltip to the right
-                </button>{"\n"}
-            </div>
-        </ComponentPreview> */}
-    </>
-);
-
-const WithIcons = () => (
-    <>
-        <h2 id="with-icons">With icons</h2>
-        <p>Tooltips can be used with icons to give further information, for instance to inform about CVC</p>
-
-        <ComponentPreview language="html" showCasePanel codeFigure>
-            <div className="form-group">{"\n"}
-                <label htmlFor="cvc">{"\n"}
-                    CVC{"\n"}
-                    <i className="material-icons"
-                        data-tooltip="Payment capture is the process by which payments are secured once the payment has been authorized by the customer."
-                        data-tooltip-position="top"
-                    >{"\n"}
-                        help_outline{"\n"}
-                    </i>{"\n"}
-                </label>{"\n"}
-                <input type="text" className="form-control" id="cvc" style={{ width: "250px" }} />{"\n"}
-            </div>
+            <TooltipComponent className="btn btn-primary" title="Tooltip to the left" text="This is a tooltip!" position="left" id="tooltip-left-example"/>
+            <TooltipComponent className="btn btn-primary" title="Tooltip on top" text="Payment capture is the process by which payments are secured once the payment has been authorized by the customer." position="top" id="tooltip-top-example"/>
+            <TooltipComponent className="btn btn-primary" title="Tooltip to the right" text="This is a tooltip!" position="right" id="tooltip-left-example"/>
+            <TooltipComponent className="btn btn-primary mt-1" title="Tooltip on bottom" text="Payment capture is the process by which payments are secured once the payment has been authorized by the customer." position="bottom" id="tooltip-left-example"/>
         </ComponentPreview>
     </>
 );
 
-const ExtendedUsage = () => (
-    <>
-        <h2 id="extended-usage">Extended usage</h2>
-        <p>
-            The extended usage tooltip is meant to give more control over the tooltip, such as automatic repositioning of the tooltip if its content falls
-            out from the viewport (using <CodeTags type="secondary" code="dg.tooltips.init()" />). Recommended to be used on mobile focused
-            applications. <b>NOTE:</b> Only <CodeTags type="secondary" code="data-tooltip-position" /> with
-            the values <CodeTags type="secondary" code="top" /> and <CodeTags type="secondary" code="bottom" /> are supported.
-        </p>
-        <ComponentPreview language="html" showCasePanel codeFigure removeOuterTag>
-            <div className="d-flex align-items-center">
-                <i className="material-icons mr-3"
-                    data-tooltip=""
-                    data-tooltip-position="top"
-                >{"\n"}
-                    help_outline{"\n"}
-                    <span className="tooltip">{"\n"}
-                        Payment capture is the process by which payments are secured once the payment has been authorized by the customer.{"\n"}
-                    </span>{"\n"}
-                </i>{"\n"}
-                <button
-                    className="btn btn-primary"
-                    type="button"
-                    data-tooltip=""
-                    data-tooltip-position="bottom"
-                >{"\n"}
-                    Tooltip on bottom{"\n"}
-                    <span className="tooltip">Payment capture is the process by which payments are secured once the payment has been authorized by the customer.</span>{"\n"}
-                </button>{"\n"}
-            </div>
-        </ComponentPreview>
-
-    </>
-);
 
 const JavascriptMethods = () => (
     <>
@@ -143,10 +36,8 @@ const Tooltip = () => {
         <DocContainer docToc>
             <p className="lead">Tooltips are small, interactive, textual hints for mainly graphical elements. When using icons for actions you can use a tooltip to give people clarification on its function.</p>
             <Overview />
-            {/* <WithIcons /> */}
-            {/* <ExtendedUsage /> */}
-            {/* <JavascriptMethods /> */}
-            {/* <NpmInformation componentName={"tooltips"}/> */}
+            <JavascriptMethods />
+            <NpmInformation componentName={"tooltips"}/> 
         </DocContainer>
     );
 };
@@ -154,4 +45,4 @@ const Tooltip = () => {
 export default Tooltip;
 
 /* For testing */
-export { Overview, WithIcons, ExtendedUsage, JavascriptMethods };
+export { Overview, JavascriptMethods };
