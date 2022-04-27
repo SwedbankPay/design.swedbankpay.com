d85519afbc069824d1fcc0178a0bfe6fdd4fc23e
Eskil Hognestad <eskil_hognestad@hotmail.com>
2022-04-27 14:19:10 +0200

    Remove unused code

--- src/App/components/FormComponents/Radio.js
+++ src/App/components/FormComponents/Radio.js
@@ -57,7 +57,6 @@ Radio.propTypes = {
     groupTitle: PropTypes.string,
     options: PropTypes.array,
     require: PropTypes.bool,
-    tooltip: PropTypes.bool,
     className: PropTypes.string,
     helpBlock: PropTypes.string,
     expandingHintTitle: PropTypes.string,
