0ba2813c47550fe5ba82cf798b0a1f27777cd418
Eskil Hognestad <eskil_hognestad@hotmail.com>
2022-05-11 09:56:02 +0200

    Rebuilding DisplayColor component, new design. Only started on it

--- src/App/components/DisplayColor/index.js
+++ src/App/components/DisplayColor/index.js
@@ -1,14 +1,36 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const ColorDisplay = ({ hex, rgb, fontColor, border }) => (
-    <div className={`${border ? border : ""}`}>
-        <svg>
-            <rect fill={hex} />
-        </svg>
-        <div className={`pl-3 ${fontColor ? fontColor : ""}`}>
-            <p className="mb-0">{hex}</p>
-            <p>{rgb}</p>
+const ColorDisplay = ({ title, subTitle, hex, rgb, fontColor, border, noWhiteExample, noBlackExample }) => (
+    <div className="d-flex flex-column mb-2">
+        <div className={`color-box ${border ? border : ""}`}>
+            <svg>
+                <rect fill={hex} />
+            </svg>
+        </div>
+        <div className="text-box d-flex flex-column">
+            <p className="font-weight-bold mb-0">{title}</p>
+            <small className="small mb-0">{subTitle}</small>
+            <small className="small mb-0 text-uppercase">{hex}</small>
+            <div className="contrast-box">
+                <div className="contrast-white-container">
+                    <svg className="white-box">
+                        <rect fill={"#FFFFFF"}/>
+                    </svg>
+                    <svg className="second-svg">
+                        <rect fill={hex}/>
+                    </svg>
+                    <i className="material-icons">check_circle</i>
+                </div>
+                <div className="contrast-black-container">
+                    <svg className="first-svg">
+                        <rect fill={"#000000"}/>
+                    </svg>
+                    <svg className="second-svg">
+                        <rect fill={hex}/>
+                    </svg>
+                </div>
+            </div>
         </div>
     </div>
 );
