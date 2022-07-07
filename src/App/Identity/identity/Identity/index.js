a4b11279840d66a752edbf939e4df8e4e249e8f5
Eskil Hognestad <eskil_hognestad@hotmail.com>
2022-07-07 14:02:22 +0200

    Update identity page with opensimplecaring section

--- src/App/Identity/identity/Identity/index.js
+++ src/App/Identity/identity/Identity/index.js
@@ -4,6 +4,7 @@ import overviewList from "@src/App/routes/identity";
 import { DocContainer } from "@docutils";
 
 const brandTitle = process.env.brandTitle;
+const basename = process.env.basename;
 
 const OurBrand = () => (
     <section>
@@ -20,6 +21,15 @@ const OurBrand = () => (
             <i className="material-icons" aria-hidden="true">open_in_new</i>
             <span className="ml-2">Swedbank Brand Manager – read more about our brand</span>
         </a>}
+    </section>
+);
+
+const OpenSimpleCaring = () => (
+    <section>
+        <h2 id="open-simple-caring">Open, simple, caring</h2>
+        <p>Swedbank Pay is a sub-brand to Swedbank and a part of the “Coin family”. That means that Swedbank Pay is built on the same foundation as Swedbank and shares the same brand platform and overall guidelines. Swedbank Pay express its own identity through the logotype, yellow color, images and tonality.</p>
+        <p>To build awareness we display Swedbank Pay’s name and logotype with impact. We express a distinguishing attitude through the logotype, yellow color, images and tonality – always as simple and uncomplicated as possible. Read more about the brand in the <Link to="/identity/identity">Identity overview</Link>.</p>
+        <img src={`${basename}img/documentation/introduction/pyramid.png`} className="img-fluid" alt="Illustration explaining the Swedbank Pay vision, values and purpose. The vision is to enable people, business and society to grow. The values are open, simple and caring. The purpose is to promote a sound and sustainable financial situation for the many households and businesses."/>
     </section>
 );
 
@@ -44,10 +54,11 @@ const Identity = () => (
     <DocContainer>
         <p className="lead">Our identity influences all aspects of the design guide, both in terms of how we approach the design and what we follow in order to create the best possible user experience.</p>
         <OurBrand />
+        {brandTitle === "Swedbank Pay" && <OpenSimpleCaring />}
         <MenuOverview />
     </DocContainer>
 );
 
 export default Identity;
 
-export { OurBrand, MenuOverview };
+export { OurBrand, MenuOverview, OpenSimpleCaring };
