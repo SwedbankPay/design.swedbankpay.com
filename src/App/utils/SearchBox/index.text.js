5a19dba84cec6336ef9705f6d5211a116d36324d
Eskil Hognestad <eskil_hognestad@hotmail.com>
2022-09-01 10:27:59 +0200

    Add tests for SearchBox component

--- src/App/utils/SearchBox/index.text.js
+++ src/App/utils/SearchBox/index.text.js
@@ -3,15 +3,32 @@ import { shallow } from "enzyme";
 
 import SearchBox from "./index";
 
-describe("SearchBox:", () => {
+describe("Utilities; SearchBox", () => {
     it("is defined", () => {
         expect(SearchBox).toBeDefined();
     });
 
-    it("renders", () => {
-        const wrapper = shallow(<SearchBox />);
+    it("renders with no props", () => {
+        const wrapper = shallow(<SearchBox/>);
 
         expect(wrapper).toMatchSnapshot();
     });
 
+    it("renders with mobile prop", () => {
+        const wrapper = shallow(<SearchBox mobile/>);
+
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    it("renders with className prop", () => {
+        const wrapper = shallow(<SearchBox className="test" />);
+
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    it("renders with props mobile and className", () => {
+        const wrapper = shallow(<SearchBox mobile className="test"/>);
+
+        expect(wrapper).toMatchSnapshot();
+    });
 });
