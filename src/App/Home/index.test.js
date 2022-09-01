9ca18e46ac4d0aa556758755433f92f157500b26
Eskil Hognestad <eskil_hognestad@hotmail.com>
2022-09-01 10:27:43 +0200

    Remove unused test

--- src/App/Home/index.test.js
+++ src/App/Home/index.test.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { shallow } from "enzyme";
 
-import Home, { SearchBox } from "./index";
+import Home from "./index";
 
 describe("Home: index", () => {
     it("is defined", () => {
@@ -10,18 +10,6 @@ describe("Home: index", () => {
 
     it("renders", () => {
         const wrapper = shallow(<Home />);
-
-        expect(wrapper).toMatchSnapshot();
-    });
-});
-
-describe("SearchBox", () => {
-    it("is defined", () => {
-        expect(SearchBox).toBeDefined();
-    });
-
-    it("renders", () => {
-        const wrapper = shallow(<SearchBox/>);
 
         expect(wrapper).toMatchSnapshot();
     });
