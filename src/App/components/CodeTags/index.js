62fba452fbbb9898732faa9dad5e9945dc77ddbe
Eskil Hognestad <eskil_hognestad@hotmail.com>
2022-09-12 13:08:11 +0200

    Update codetags to have possibility to be reused as a filtertag.
    Add button for removable filtertag
    Update proptypes

--- src/App/components/CodeTags/index.js
+++ src/App/components/CodeTags/index.js
@@ -1,13 +1,22 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const CodeTags = ({ type, code }) => (
-    <code className={`code-tags code-tags-${type}`}>{code}</code>
+const CodeTags = ({ className, type, code, text, removable }) => (
+    <>
+        {code ?
+            <code className={`code-tags code-tags-${type}`}>{code}</code>
+            :
+            <span className={`tag tag-${type}${removable ? " removable" : ""}${className ? ` ${className}` : ""}`}>{text}{removable && <button type="button" className="close-button"><i className="material-icons m-auto" aria-label="remove tag">close</i></button>}</span>
+        }
+    </>
 );
 
 CodeTags.propTypes = {
     type: PropTypes.string.isRequired,
-    code: PropTypes.string.isRequired
+    code: PropTypes.string,
+    text: PropTypes.string,
+    removable: PropTypes.bool,
+    className: PropTypes.string
 };
 
 export default CodeTags;
