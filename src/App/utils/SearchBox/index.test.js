26b5e3076ac7347d59facde841706f7925eafed2
Eskil Hognestad <eskil_hognestad@hotmail.com>
2022-09-21 12:41:59 +0200

    Update test file to test correct result box behaviour

--- src/App/utils/SearchBox/index.test.js
+++ src/App/utils/SearchBox/index.test.js
@@ -3,6 +3,8 @@ import { shallow, mount } from "enzyme";
 
 import SearchBox, { modify } from "./index";
 
+import { BrowserRouter as Router } from "react-router-dom";
+
 describe("Utilities; SearchBox", () => {
     it("is defined", () => {
         expect(SearchBox).toBeDefined();
@@ -51,16 +53,9 @@ describe("Utilities; SearchBox", () => {
 
 describe("Manipulating input values for input field", () => {
     const wrapper = mount(<SearchBox/>);
-
-    it("does not render the result box before manipulating the search field", () => {
-
-        expect(wrapper.contains("item-list")).toBeFalsy();
-        expect(wrapper).toMatchSnapshot();
-    });
+    const input = wrapper.find("#search-box");
 
     it("button icon changes from search to close after input field manipulation", () => {
-        const input = wrapper.find("#search-box");
-
         expect(input.length).toBe(1);
 
         expect(wrapper.html()).toContain("search-box");
@@ -72,32 +67,97 @@ describe("Manipulating input values for input field", () => {
         expect(wrapper).toMatchSnapshot();
     });
 
-    describe("Testing components methods directly", () => {
-        const result = "test";
+    it("result box is not visible before input is manipulated", () => {
+        const resultBox = wrapper.find("#result-box");
 
-        it("modify method gives <b> tags around parts of searchterm if searchterm is in beginning of result", () => {
+        expect(resultBox.length).toBe(0);
 
-            const searchTerm = "tes";
-
-            expect(modify(result, searchTerm)).toBe("<b>tes</b>t");
-            expect(wrapper).toMatchSnapshot();
-        });
-
-        it("modify method gives <b> tags around parts of searchterm if searchterm is in the end of result", () => {
-
-            const searchTerm = "st";
-
-            expect(modify(result, searchTerm)).toBe("te<b>st</b>");
-            expect(wrapper).toMatchSnapshot();
-        });
-
-        it("modify method returns result if searchterm does not match result and no <b> tags", () => {
-
-            const searchTerm = "foo";
-
-            expect(modify(result, searchTerm)).toBe("test");
-            expect(wrapper).toMatchSnapshot();
-        });
+        expect(wrapper).toMatchSnapshot();
     });
 
+    it("result box is visible after input field change", () => {
+        input.simulate("change", { target: { value: "" } });
+
+        const resultBox = wrapper.find(".result-box");
+
+        expect(resultBox.length).toBe(0);
+
+        input.simulate("change", { target: { value: "test" } });
+
+        const resultBoxAfter = wrapper.find(".result-box");
+
+        expect(resultBoxAfter.length).toBe(1);
+
+        expect(wrapper).toMatchSnapshot();
+    });
+});
+
+describe("Search box results tests", () => {
+    const wrapper = mount(<Router><SearchBox/></Router>);
+    const input = wrapper.find("#search-box");
+
+    it("renders no items if search term has no matches", () => {
+
+        input.simulate("change", { target: { value: "xxxxx" } });
+
+        const liElements = wrapper.find("li");
+
+        expect(liElements.length).toBe(0);
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    it("renders toggelbox after searcing for 'gg'", () => {
+
+        input.simulate("change", { target: { value: "gg" } });
+
+        const liElements = wrapper.find("li");
+
+        expect(liElements.length).toBe(1);
+
+        expect(wrapper.html()).toContain("<span class=\"result\">To<b>gg</b>lebox</span>");
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    it("renders several li elements when search term is 'f", () => {
+        input.simulate("change", { target: { value: "f" } });
+
+        const liElements = wrapper.find("li");
+
+        expect(liElements.length).toBe(5);
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    it("renders more than 10 li elements when search term is 'i'", () => {
+        input.simulate("change", { target: { value: "i" } });
+
+        const liElements = wrapper.find("li");
+
+        expect(liElements.length).toBeGreaterThan(10);
+        expect(wrapper).toMatchSnapshot();
+    });
+});
+
+describe("Testing components modify method directly", () => {
+    const result = "test";
+
+    it("modify method gives <b> tags around parts of searchterm if searchterm is in beginning of result", () => {
+
+        const searchTerm = "tes";
+
+        expect(modify(result, searchTerm)).toBe("<b>tes</b>t");
+    });
+
+    it("modify method gives <b> tags around parts of searchterm if searchterm is in the end of result", () => {
+
+        const searchTerm = "st";
+
+        expect(modify(result, searchTerm)).toBe("te<b>st</b>");
+    });
+
+    it("modify method returns result if searchterm does not match result and no <b> tags", () => {
+
+        const searchTerm = "foo";
+
+        expect(modify(result, searchTerm)).toBe("test");
+    });
 });
