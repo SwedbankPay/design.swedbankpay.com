f6bef364ce07f231a0572c2a0da976340d4e586f
Eskil Hognestad <eskil_hognestad@hotmail.com>
2022-09-28 13:01:16 +0200

    Created dropdown doc page
    test file
    add route
    everything is ready for raphael

---
+++ src/App/ComponentsDocumentation/components/Dropdown/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { ComponentPreview, DocContainer, JavascriptDocs } from "@docutils";
+
+const Overview = () => (
+    <section>
+        <h2 id="overview">Overview</h2>
+        <p>
+            Everything is ready for you now, Raphael 🎉
+        </p>
+        <ComponentPreview language="html" showCasePanel codeFigure>
+            todo
+        </ComponentPreview>
+    </section>
+);
+
+const Dropdown = () => (
+    <DocContainer docToc>
+        <p className="lead">Some informational text about the dropdown component</p>
+        <Overview/>
+    </DocContainer>
+);
+
+export default Dropdown;
