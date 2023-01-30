f6423d204e6e56d11a61baeefbfef01a95f3e303
Eskil Hognestad <eskil_hognestad@hotmail.com>
2023-01-30 14:03:32 +0100

    Change tooltip div to span
    Add isVisible class for one of the examples in the doc

--- src/App/components/Tooltips/index.js
+++ src/App/components/Tooltips/index.js
@@ -2,20 +2,21 @@ import React from "react";
 import PropTypes from "prop-types";
 import classnames from "classnames";
 
-const Tooltip = ({ title, text, className, position, id, icon }) => {
+const Tooltip = ({ title, text, className, position, id, icon, isVisible }) => {
     const tooltipClasses = classnames(
-        "tooltip",
+        "tooltip btn btn-secondary",
         position && `tooltip-${position}`,
-        className && `${className}`
+        className && `${className}`,
+        isVisible && "isVisible"
     );
 
     return (
         <button aria-describedby={icon ? id : null} className={tooltipClasses}>{"\n"}
             {title}
             {icon && <i className="material-icons-outlined">{icon}</i>}{"\n"}
-            <div role="tooltip" id={id}>
+            <span role="tooltip" id={id}>
                 {text}
-            </div>
+            </span>
         </button>
     );
 };
