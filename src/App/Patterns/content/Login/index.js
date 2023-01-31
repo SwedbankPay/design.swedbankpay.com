c2b4e7ba6497e77118593e955def552ac2e6c8f5
Raphael Ferrand <goldenraphti@users.noreply.github.com>
2023-01-31 16:14:11 +0100

    SWED-2060 revert linting login

--- src/App/Patterns/content/Login/index.js
+++ src/App/Patterns/content/Login/index.js
@@ -14,29 +14,14 @@ const Overview = () => (
     <section>
         <h2 id="overview">Overview</h2>
 
-        <img
-            src={`${basename}img/documentation/patterns/login-pattern-example.png`}
-            className="d-none d-md-block w-100 border-left border-right border-bottom"
-            alt="A login screen with the login functionality to the left and an image to the right showing a woman using a laptop."
-        />
+        <img src={`${basename}img/documentation/patterns/login-pattern-example.png`} className="d-none d-md-block w-100 border-left border-right border-bottom" alt="A login screen with the login functionality to the left and an image to the right showing a woman using a laptop." />
         <div className="container-fluid d-block d-md-none border-left border-right border-bottom border-top-brand py-3">
-            <LoginForm
-                passwordId="login-password-example"
-                emailId="login-email-example"
-                checkboxId="login-checkbox-example"
-                additionalText
-                helpDesk
-                logo
-                alternativeLogins
-                title="h1"
-                productLabel="Merchant Admin"
-            />
+            <LoginForm passwordId="login-password-example" emailId="login-email-example" checkboxId="login-checkbox-example" additionalText helpDesk logo alternativeLogins title="h1" productLabel="Merchant Admin" />
         </div>
         <div className="border-left border-right p-3">
             <div className="m-3">
                 <h3>Login</h3>
-        The login page is customizable and components can be removed or added to
-        fit a specific products needs.
+                The login page is customizable and components can be removed or added to fit a specific products needs.
             </div>
         </div>
         <EditableComponentPreview language="html" codeFigure>
@@ -44,25 +29,10 @@ const Overview = () => (
                 <div className="row">
                     <div className="col-lg-5 col-12 d-flex p-0">
                         <div className="col-sm-9 col-12 m-auto">
-                            <LoginForm
-                                passwordId="password-input-example"
-                                emailId="email-input-example"
-                                checkboxId="checkbox-example"
-                                additionalText
-                                helpDesk
-                                logo
-                                alternativeLogins
-                                title="h1"
-                                productLabel="Merchant Admin"
-                            />
+                            <LoginForm passwordId="password-input-example" emailId="email-input-example" checkboxId="checkbox-example" additionalText helpDesk logo alternativeLogins title="h1" productLabel="Merchant Admin" />
                         </div>
-                    </div>
-                    {"\n"}
-                    <img
-                        src={`${basename}img/documentation/patterns/login-img-example.png`}
-                        alt="Image for login screen"
-                        className="d-none d-lg-block col-7 px-0"
-                    />
+                    </div>{"\n"}
+                    <img src={`${basename}img/documentation/patterns/login-img-example.png`} alt="Image for login screen" className="d-none d-lg-block col-7 px-0" />
                 </div>
             </div>
         </EditableComponentPreview>
@@ -75,46 +45,29 @@ const ContentGuidelines = () => (
         <div className="showcase-panel p-0 mb-4">
             <div className="border-top-brand py-5">
                 <div className="col-sm-8 col-12 m-auto">
-                    <LoginForm
-                        placeholder="Placeholder text"
+                    <LoginForm placeholder="Placeholder text"
                         title="h1"
                         checkboxId="guideline-checkbox-example"
                         passwordId="guideline-password-example"
-                        passwordLabel="Required input field"
-                        emailId="guideline-email-example"
+                        passwordLabel="Required input field" emailId="guideline-email-example"
                         emailLabel="Required input field"
                         alternativeLogins
                         additionalText="Additional help text"
                         productLabel="Product Label"
-                        logo
-                    />
+                        logo />
                 </div>
             </div>
         </div>
         <ul className="list list-bullet">
-            {guidelineList.map((text, i) => (
-                <li key={i}>{text}</li>
-            ))}
+            {guidelineList.map((text, i) => <li key={i}>{text}</li>)}
         </ul>
         <section>
             <h3>Image</h3>
-            <img
-                src={`${basename}img/documentation/patterns/login-img-collection-example.png`}
-                alt="Selection of images showing people in different settings using digital devices."
-                className="w-100 mb-3"
-            />
-            <p>
-        The login form is always accompanied by a image on the right side on
-        desktop devices, the image is hidden on smaller devices. Above are an
-        example of some images that would be fitting to be used as login images,
-        the most important thing to look out for is telling a story, especially
-        when it comes to the product. “Who is this product for?” If it is an
-        product for consumers outside of our organization, try to relate the
-        image to that segment of the user. For more guidance on how to design
-        with images and find our image library, refer to the{" "}
-                <Link to="/identity/imagery">Imagery section</Link>.
+            <img src={`${basename}img/documentation/patterns/login-img-collection-example.png`} alt="Selection of images showing people in different settings using digital devices." className="w-100 mb-3" />
+            <p>The login form is always accompanied by a image on the right side on desktop devices, the image is hidden on smaller devices. Above are an example of some images that would be fitting to be used as login images, the most important thing to look out for is telling a story, especially when it comes to the product. “Who is this product for?” If it is an product for consumers outside of our organization, try to relate the image to that segment of the user. For more guidance on how to design with images and find our image library, refer to the <Link to="/identity/imagery">Imagery section</Link>.
             </p>
         </section>
+
     </section>
 );
 
@@ -122,59 +75,27 @@ const DeveloperDocumentation = () => (
     <section>
         <h2 id="developer-documentation">Developer documentation</h2>
         <h3>Errors and validation</h3>
-        <p>
-      Not being able to log in to an application is very frustrating for the
-      user, therefore error messages should be used to help the user understand
-      what went wrong and how to solve the problem.
-        </p>
+        <p>Not being able to log in to an application is very frustrating for the user, therefore error messages should be used to help the user understand what went wrong and how to solve the problem.</p>
 
         <section>
             <h4>Client-side validation</h4>
-            <p>
-        Use client-side validation to validate as much data as possible inline
-        before the user submits the form. Common inline errors include empty
-        required fields, invalid characters or incorrect input format.{" "}
-            </p>
-            <EditableComponentPreview
-                language="html"
-                codeFigure
-                showCasePanel
-                previewMinHeight={250}
-            >
+            <p>Use client-side validation to validate as much data as possible inline before the user submits the form. Common inline errors include empty required fields, invalid characters or incorrect input format. </p>
+            <EditableComponentPreview language="html" codeFigure showCasePanel>
                 <div className="col-sm-6 col-12 m-auto px-0 pt-3">
-                    <InputGroup
-                        type="email"
-                        id="login-client-side"
-                        validationState="error"
-                        label="Email"
-                        errorMessage="Email is required"
-                    />
+                    <InputGroup type="email" id="login-client-side" validationState="error" label="Email" errorMessage="Email is required" />
                 </div>
             </EditableComponentPreview>
         </section>
         <section>
             <h4>Server-side validation</h4>
-            <p>
-        Use server side validation when the user submits the form. Display a
-        local error alert after the page has reloaded if an error has occurred.
-        Incorrect email or password are the most common servers errors.{" "}
-            </p>
-            <EditableComponentPreview
-                language="html"
-                codeFigure
-                showCasePanel
-                previewMinHeight={600}
-            >
+            <p>Use server side validation when the user submits the form. Display a local error alert after the page has reloaded if an error has occurred. Incorrect email or password are the most common servers errors. </p>
+            <EditableComponentPreview language="html" codeFigure showCasePanel>
                 <div className="col-sm-6 col-12 m-auto px-0 py-3">
-                    <LoginForm
-                        error
-                        defaultPassword
-                        defaultEmail
+                    <LoginForm error defaultPassword defaultEmail
                         passwordId="validation-password-example"
                         emailId="validation-email-example"
                         checkboxId="checkbox-example"
-                        title="h2"
-                    />
+                        title="h2" />
                 </div>
             </EditableComponentPreview>
         </section>
@@ -183,10 +104,7 @@ const DeveloperDocumentation = () => (
 
 const Login = () => (
     <DocContainer>
-        <p className="lead">
-      The login page allow users to gain access to an application by entering
-      login credentials to a form.
-        </p>
+        <p className="lead">The login page allow users to gain access to an application by entering login credentials to a form.</p>
         <Overview />
         <ContentGuidelines />
         <DeveloperDocumentation />
