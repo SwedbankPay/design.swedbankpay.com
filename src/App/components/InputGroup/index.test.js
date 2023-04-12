0bd21b24e22ae01c926f6f83b8bde5d369fb5578
Eskil Hognestad <eskil_hognestad@hotmail.com>
2023-04-12 14:24:03 +0200

    Remove obsolete validation test

--- src/App/components/InputGroup/index.test.js
+++ src/App/components/InputGroup/index.test.js
@@ -26,19 +26,6 @@ describe("Component: InputGroup -", () => {
         expect(componentForSnap.toJSON()).toMatchSnapshot();
     });
 
-    it.skip("throws an error if validationState prop has an invalid value", () => {
-        const testValues = ["success", "error", "", "invalid", "test"];
-
-        console.error = jest.fn();
-        testValues.forEach(testValue => {
-
-            const componentForSnap = renderer.create(<InputGroup type="typetest" validationState={testValue} />);
-
-            expect(componentForSnap.toJSON()).toMatchSnapshot();
-        });
-        expect(console.error).toHaveBeenCalledTimes(3);
-    });
-
     it("throws an error if addOnType prop has an invalid value", () => {
         const testValues = ["button", "icon", "", "invalid", "test", "text"];
 
