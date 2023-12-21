6141ce6ada11e00445edc25a1c2e8c7c47d8bb73
Raphael Ferrand <goldenraphti@users.noreply.github.com>
2023-12-22 00:23:11 +0100

    SWED-2276 add basic custom icon - update iconography documentation

--- src/App/Identity/identity/Iconography/constants.js
+++ src/App/Identity/identity/Iconography/constants.js
@@ -1,3 +1,11 @@
+export const atlasIcons = [
+	"calendar-dots",
+	"image-gallery",
+	"home",
+	"shopping-cart",
+	"trash",
+];
+
 export const materialIcons = [
 	"insert_invitation",
 	"cake",
@@ -132,3 +140,69 @@ export const flags = [
 ];
 
 export const flagSizes = ["tiny", "small", "medium", "large", "huge"];
+
+export const shapes = [
+	{
+		title: "Chevron down",
+		code: "chevron-down",
+	},
+	{
+		title: "Chevron up",
+		code: "chevron-up",
+	},
+	{
+		title: "Chevron left",
+		code: "chevron-left",
+	},
+	{
+		title: "Chevron right",
+		code: "chevron-right",
+	},
+	{
+		title: "Check circle filled",
+		code: "check-circle-filled",
+	},
+	{
+		title: "Close",
+		code: "close",
+	},
+	{
+		title: "Error circle filled",
+		code: "error-circle-filled",
+	},
+	{
+		title: "Error triangle filled",
+		code: "error-triangle-filled",
+	},
+	{
+		title: "Warning triangle filled",
+		code: "warning-triangle-filled",
+	},
+	{
+		title: "Info circle filled",
+		code: "info-circle-filled",
+	},
+	{
+		title: "Minus substract",
+		code: "minus-substract",
+	},
+	{
+		title: "Plus add",
+		code: "plus-add",
+	},
+];
+
+export const commonIconSizes = [
+	{
+		title: "small",
+		code: "small",
+	},
+	{
+		title: '"" (default)',
+		code: "",
+	},
+	{
+		title: "large",
+		code: "large",
+	},
+];
