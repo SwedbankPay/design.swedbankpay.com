a7c0b7f96308a44b478e59071c290b7daa25e232
Raphael Ferrand <goldenraphti@users.noreply.github.com>
2024-01-15 19:06:15 +0100

    SWED-2379 fix links icon migration & pure CSS

--- src/App/ComponentsDocumentation/components/Links/constants.js
+++ src/App/ComponentsDocumentation/components/Links/constants.js
@@ -216,7 +216,7 @@ export const overviewLinks = {
 							{
 								name: "Back",
 								value: {
-									leftIcon: "swepay-icon-chevron-left small",
+									leftIcon: "swepay-icon-chevron-left",
 								},
 							},
 							{
